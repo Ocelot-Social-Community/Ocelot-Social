b93489fe14f018ab2470a9fbea73cc5936b7e780
roschaefer <git@roschaefer.de>
2020-06-08 15:19:12 +0200

    Remove all humanconnection dockerhub repos
    I'm experiencing non-determinstic behaviour when I configure a repsitory
    which does not exist on dockerhub or is not part of the schoolsinmotion
    organization. Therefore I'm going to use a tag `bare` for the base
    images.

---
+++ webapp/constants/emails.js
@@ -0,0 +1,4 @@
+export default {
+  SUPPORT: 'support-schools-in-motion@roschaefer.de',
+  MODERATION: 'moderation-schools-in-motion@roschaefer.de',
+}
---
+++ webapp/constants/links.js
@@ -0,0 +1,7 @@
+export default {
+  ORGANIZATION: 'https://www.youth-and-arts.nrw/',
+  DONATE: 'https://www.youth-and-arts.nrw/de/kontakt',
+  IMPRINT: 'https://www.youth-and-arts.nrw/de/impressum',
+  DATA_PRIVACY: 'https://www.youth-and-arts.nrw/de/datenschutzerklaerung',
+  FAQ: 'https://www.youth-and-arts.nrw/de/kontakt',
+}
