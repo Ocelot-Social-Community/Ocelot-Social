68c5f0dd85eb3e77b6cb3795bbd93e506ceb65d8
roschaefer <git@roschaefer.de>
2020-06-15 15:15:55 +0200

    Fix lint

--- webapp/constants/links.js
+++ webapp/constants/links.js
@@ -2,5 +2,5 @@ export default {
   ORGANIZATION: 'https://human-connection.org/',
   DONATE: 'https://human-connection.org/spenden/',
   FAQ: 'https://faq.human-connection.org/',
-  SUPPORT: 'https://human-connection.org/support'
+  SUPPORT: 'https://human-connection.org/support',
 }
