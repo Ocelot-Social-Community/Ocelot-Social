360f545fb70904e18d8477b1583fd6d133cc701a
Wolfgang Huß <wolle.huss@pjannto.com>
2020-10-28 10:36:14 +0100

    Change docker account
    - humanconnection  —>  ocelotsocialnetwork

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -8,22 +8,22 @@ apps=(nitro-web nitro-backend neo4j maintenance)
 tags=($major $major.$minor $major.$minor.$patch)
 
 # These three docker images have already been built by now:
-# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT --target production -t humanconnection/nitro-backend:latest $ROOT_DIR/backend
-# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT --target production -t humanconnection/nitro-web:latest $ROOT_DIR/webapp
-# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT -t humanconnection/neo4j:latest $ROOT_DIR/neo4j
-docker build -t humanconnection/maintenance:latest $ROOT_DIR/webapp/ -f $ROOT_DIR/webapp/Dockerfile.maintenance
+# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT --target production -t ocelotsocialnetwork/nitro-backend:latest $ROOT_DIR/backend
+# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT --target production -t ocelotsocialnetwork/nitro-web:latest $ROOT_DIR/webapp
+# docker build --build-arg BUILD_COMMIT=$BUILD_COMMIT -t ocelotsocialnetwork/neo4j:latest $ROOT_DIR/neo4j
+docker build -t ocelotsocialnetwork/maintenance:latest $ROOT_DIR/webapp/ -f $ROOT_DIR/webapp/Dockerfile.maintenance
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 for app in "${apps[@]}"
 do
-  SOURCE="humanconnection/${app}:latest"
+  SOURCE="ocelotsocialnetwork/${app}:latest"
   echo "docker push $SOURCE"
   docker push $SOURCE
 
   for tag in "${tags[@]}"
   do
-    TARGET="humanconnection/${app}:${tag}"
+    TARGET="ocelotsocialnetwork/${app}:${tag}"
     if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $TARGET >/dev/null; then
       echo "docker image ${TARGET} already present, skipping ..."
     else
