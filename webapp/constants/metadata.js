4ad6629c9ac95be6ae0536241855d26960babcf9
Wolfgang Huß <wolle.huss@pjannto.com>
2020-12-22 17:09:25 +0100

    Add cookie entry to 'metadata.js'

--- webapp/constants/metadata.js
+++ webapp/constants/metadata.js
@@ -2,6 +2,7 @@ export default {
   APPLICATION_NAME: 'fyphe_O',
   APPLICATION_SHORT_NAME: 'fyphe',
   APPLICATION_DESCRIPTION: 'The Schools in Motion Network',
+  COOKIE_NAME: 'ocelot-social-token',
   ORGANIZATION_NAME: 'Ensible e.V.',
   ORGANIZATION_JURISDICTION: 'Köln',
 }
