7c5e5d971e0f0d4c70fb10be041515a4616dde83
Moriz Wahl <moriz.wahl@gmx.de>
2021-02-22 19:07:44 +0100

    cypher to track user interactions

--- backend/src/middleware/userInteractions.js
+++ backend/src/middleware/userInteractions.js
@@ -1,8 +1,38 @@
+const createRelatedCypher = (relation) => `
+MATCH (user:User { id: $currentUser})
+MATCH (post:Post { id: $postId})<-[r:${relation}]-(u:User)
+WHERE NOT u.disabled AND NOT u.deleted
+WITH user, post, count(DISTINCT u) AS count
+MERGE (user)-[relation:${relation} { }]->(post)
+ON CREATE
+SET relation.count = 1,
+relation.createdAt = toString(datetime()),
+post.clickCount =  count + 1
+ON MATCH
+SET relation.count = relation.count + 1,
+relation.updatedAt = toString(datetime()),
+post.clickCount = count
+RETURN user, post, relation
+`
 
+const setPostCounter = async (postId, relation, context) => {
+  const { user: currentUser } = context
+  const session = context.driver.session()
+  try {
+    await session.writeTransaction((txc) => {
+      return txc.run(
+        createRelatedCypher(relation),
+        { currentUser, postId },
+      )
+    })
+  } finally {
+    session.close()
+  }  
+}
 
 const userClickedPost = async (resolve, root, args, context, info) => {
   if (args.id) {
-    console.log('post clicked--', args.id)
+    await setPostCounter(args.id, 'CLICKED', context)
   }
   return resolve(root, args, context, info)
 }
