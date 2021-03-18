e54763cddf97ab25094db799fbc2fe7d6ba68982
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-03-18 12:34:28 +0100

    backend changes for INVITE_REGISTRATION config

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -82,7 +82,8 @@ const options = {
   SUPPORT_URL: links.SUPPORT,
   APPLICATION_NAME: metadata.APPLICATION_NAME,
   ORGANIZATION_URL: links.ORGANIZATION,
-  PUBLIC_REGISTRATION: env.PUBLIC_REGISTRATION === 'true',
+  PUBLIC_REGISTRATION: env.PUBLIC_REGISTRATION === 'true' || false,
+  INVITE_REGISTRATION: env.INVITE_REGISTRATION !== 'false', //default = true
 }
 
 // Check if all required configs are present
