7c794012e23aebfb5785f196d7025f525831a5b3
Wolfgang Huß <wolle.huss@pjannto.com>
2021-03-24 11:51:48 +0100

    Design invite button

--- webapp/components/InviteButton/InviteButton.vue
+++ webapp/components/InviteButton/InviteButton.vue
@@ -6,15 +6,32 @@
     <template #popover>
       <div class="invite-button-menu-popover">
         <div v-if="inviteCode && inviteCode.code">
-          <ds-text align="center">{{ $t('invite-codes.your-code') }}</ds-text>
-          <base-button class="invite-code" v-if="canCopy" ghost @click="copyInviteCode">
-            <ds-text bold>
-              {{ inviteCode.code }}
-            </ds-text>
-          </base-button>
-          <base-button v-if="canCopy" icon="copy" ghost @click="copyInviteLink">
-            {{ $t('invite-codes.copy-link') }}
-          </base-button>
+          <!-- Wolle <ds-text align="center">{{ $t('invite-codes.your-code') }}</ds-text> -->
+          <!-- <ds-text align="center" bold>Copy to clipboard</ds-text> -->
+          <!-- <p class="description">{{ $t(`invite-codes.your-code`) }}</p> -->
+          <p class="description">{{ $t('invite-codes.your-code') }}</p>
+          <base-card class="code-card" wideContent>
+            <!-- Wolle <ds-text bold>As code</ds-text> -->
+            <base-button
+              v-if="canCopy"
+              class="invite-code"
+              icon="copy"
+              ghost
+              @click="copyInviteLink"
+            >
+              <ds-text bold>
+                <!-- Wolle {{ inviteCode.code }} -->
+                {{ $t('invite-codes.copy-code') }}
+                {{ inviteCode.code }}
+              </ds-text>
+            </base-button>
+            <!-- Wolle <ds-text bold>As link</ds-text> -->
+            <!-- <base-button v-if="canCopy" icon="copy" ghost @click="copyInviteLink">
+              <!-- Wolle {{ $t('invite-codes.copy-link') }} -->
+              <!-- {{ $t('invite-codes.copy-link') }}
+              {{ inviteLink | truncateStr(30) }}
+            </base-button> -->
+          </base-card>
         </div>
         <div v-else>
           <ds-text>{{ $t('invite-codes.not-available') }}</ds-text>
@@ -27,10 +44,12 @@
 <script>
 import Dropdown from '~/components/Dropdown'
 import gql from 'graphql-tag'
+import BaseCard from '../_new/generic/BaseCard/BaseCard.vue'
 
 export default {
   components: {
     Dropdown,
+    BaseCard,
   },
   props: {
     placement: { type: String, default: 'top-end' },
@@ -44,18 +63,23 @@ export default {
   created() {
     this.canCopy = !!navigator.clipboard
   },
-  methods: {
-    async copyInviteCode() {
-      await navigator.clipboard.writeText(this.inviteCode.code)
-      this.$toast.success(this.$t('invite-codes.copy-success'))
-    },
-    async copyInviteLink() {
-      await navigator.clipboard.writeText(
+  computed: {
+    inviteLink() {
+      return (
         'https://' +
-          window.location.hostname +
-          '/registration?method=invite-code&inviteCode=' +
-          this.inviteCode.code,
+        window.location.hostname +
+        '/registration?method=invite-code&inviteCode=' +
+        this.inviteCode.code
       )
+    },
+  },
+  methods: {
+    // Wolle async copyInviteCode() {
+    //   await navigator.clipboard.writeText(this.inviteCode.code)
+    //   this.$toast.success(this.$t('invite-codes.copy-success'))
+    // },
+    async copyInviteLink() {
+      await navigator.clipboard.writeText(this.inviteLink)
       this.$toast.success(this.$t('invite-codes.copy-success'))
     },
   },
@@ -79,17 +103,26 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scope>
 .invite-button {
   color: $color-secondary;
 }
 
 .invite-button-menu-popover {
-  text-align: center;
+  // Wolle text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .description {
+    margin-top: $space-x-small;
+    margin-bottom: $space-x-small;
+  }
+  .code-card {
+    margin-bottom: $space-x-small;
+  }
 }
+
 .invite-code {
   left: 50%;
 }
