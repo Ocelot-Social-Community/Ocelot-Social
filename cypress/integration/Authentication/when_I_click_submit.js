2ad12630115b25c453f64aefb5eb5f0445be8825
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-10 12:36:53 +0200

    wait 500ms when logging in to increase stability

--- cypress/integration/Authentication/when_I_click_submit.js
+++ cypress/integration/Authentication/when_I_click_submit.js
@@ -2,5 +2,6 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 
 When("I click submit", () => {
   cy.get("button[name=submit]")
-    .click();
+    .click()
+    .wait(500); // This is to ensure we reach the feed
 });
