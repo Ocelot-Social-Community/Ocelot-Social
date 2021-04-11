61d4107e7bf381c08fe5ea318517376c98d5e304
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-11 16:10:44 +0200

    Search.feature

---
+++ cypress/integration/Search/I_search_for_{string}.js
@@ -0,0 +1,7 @@
+import { When } from "cypress-cucumber-preprocessor/steps";
+
+When("I search for {string}", value => {
+  cy.get(".searchable-input .ds-select input")
+    .focus()
+    .type(value);
+});
