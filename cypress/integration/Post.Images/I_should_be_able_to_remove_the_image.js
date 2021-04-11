aa2fffb947bcc575ad242b77def9785da0100728
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-11 15:25:51 +0200

    fix some flakyness

--- cypress/integration/Post.Images/I_should_be_able_to_remove_the_image.js
+++
@@ -1,6 +0,0 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
-
-Then('I should be able to remove the image', () => {
-  cy.get('.delete-image-button' /* .dz-message > .base-button .crop-cancel*/)
-    .click()
-})
