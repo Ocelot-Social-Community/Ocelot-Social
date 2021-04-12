64a83a7a63ba8e265c929c9ed006c1a0a815b0af
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-12 19:07:43 +0200

    cleanup search

--- cypress/integration/Search/I_search_for_{string}.js
+++
@@ -1,7 +0,0 @@
-import { When } from "cypress-cucumber-preprocessor/steps";
-
-When("I search for {string}", value => {
-  cy.get(".searchable-input .ds-select input")
-    .focus()
-    .type(value);
-});
