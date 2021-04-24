38a6fbbed28d75804036a92e24bcd0770c50bfa3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-24 12:54:09 +0200

    coverage PageFooter.spec.js

---
+++ webapp/components/PageFooter/PageFooter.spec.js
@@ -0,0 +1,40 @@
+import { config, mount } from '@vue/test-utils'
+import PageFooter from './PageFooter.vue'
+import links from '~/constants/links.js'
+
+const localVue = global.localVue
+
+config.stubs['nuxt-link'] = '<span class="nuxt-link"><slot /></span>'
+
+describe('PageFooter.vue', () => {
+  let mocks
+
+  beforeEach(() => {
+    mocks = {
+      $t: jest.fn(),
+      $env: {
+        VERSION: 'v1.0.0'
+      },
+      links
+    }
+  })
+
+  describe('mount', () => {
+    let wrapper
+    const Wrapper = () => {
+      return mount(PageFooter, { mocks, localVue })
+    }
+
+    beforeEach(() => {
+      wrapper = Wrapper()
+    })
+
+    it('renders three links', () => {
+      expect(wrapper.findAll('a')).toHaveLength(3)
+    })
+
+    it('renders four nuxt-links', () => {
+      expect(wrapper.findAll('.nuxt-link')).toHaveLength(4)
+    })
+  })
+})
