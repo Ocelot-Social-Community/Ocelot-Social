37c04849dd8a981c230bda268dafa4f448d595f0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-26 00:35:09 +0200

    coverage default.spec.js

---
+++ webapp/layouts/default.spec.js
@@ -0,0 +1,45 @@
+import Vuex from 'vuex'
+import { config, shallowMount } from '@vue/test-utils'
+import Default from './default.vue'
+
+const localVue = global.localVue
+
+config.stubs['nuxt'] = '<span><slot /></span>'
+config.stubs['client-only'] = '<span><slot /></span>'
+config.stubs['nuxt-link'] = '<span><slot /></span>'
+
+describe('default.vue', () => {
+  let wrapper
+  let mocks
+  let store
+
+  beforeEach(() => {
+    mocks = {
+      $t: jest.fn(),
+      $env: {
+        INVITE_REGISTRATION: true
+      },
+    }
+    store = new Vuex.Store({
+      getters: {},
+    })
+  })
+
+  describe('shallow mount', () => {
+    const Wrapper = () => {
+      return shallowMount(Default, {
+        store,
+        mocks,
+        localVue,
+      })
+    }
+
+    beforeEach(() => {
+      wrapper = Wrapper()
+    })
+
+    it('renders', () => {
+      expect(wrapper.is('.layout-default')).toBe(true)
+    })
+  })
+})
