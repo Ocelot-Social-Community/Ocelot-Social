43c1c268ca6cf022b349b90056426d3a2c7e31d3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2021-04-25 16:33:35 +0200

    coverage post/edit/_id.spec.js

---
+++ webapp/pages/post/edit/_id.spec.js
@@ -0,0 +1,30 @@
+import { config, mount } from '@vue/test-utils'
+import _id from './_id.vue'
+
+const localVue = global.localVue
+// config.stubs['nuxt-child'] = '<span class="nuxt-child"><slot /></span>'
+
+describe('post/_id.vue', () => {
+  let wrapper
+  let mocks
+
+  beforeEach(() => {
+    mocks = {
+      $t: jest.fn(),
+    }
+  })
+
+  describe('mount', () => {
+    const Wrapper = () => {
+      return mount(_id, { mocks, localVue })
+    }
+
+    beforeEach(() => {
+      wrapper = Wrapper()
+    })
+
+    it('renders', () => {
+      expect(wrapper.findAll('.contribution-form')).toHaveLength(1)
+    })
+  })
+})
