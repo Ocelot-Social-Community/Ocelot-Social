7b09df848ba8c7706cd13a58e87149106151d497
Wolfgang Huß <wolle.huss@pjannto.com>
2021-06-03 07:36:55 +0200

    Extend and change links

--- branding/constants/links.js
+++ branding/constants/links.js
@@ -1,7 +1,12 @@
 export default {
   ORGANIZATION: 'https://ocelot.social',
-  DONATE: 'https://ocelot-social.herokuapp.com/donations',
-  IMPRINT: 'https://ocelot-social.herokuapp.com/imprint',
-  DATA_PRIVACY: 'https://ocelot-social.herokuapp.com/imprint',
-  FAQ: 'https://ocelot-social.herokuapp.com/#kontakt',
+  SUPPORT: 'https://ocelot.social',
+
+  // on null or empty string internal imprint is used, see 'webapp/locales/html/'
+  DONATE: 'https://ocelot-social.herokuapp.com/donations', // we use 'ocelot-social.herokuapp.com' at the moment, because redirections of 'ocelot.social' subpages are not working correctly
+  IMPRINT: 'https://ocelot-social.herokuapp.com/imprint', // we use 'ocelot-social.herokuapp.com' at the moment, because redirections of 'ocelot.social' subpages are not working correctly
+  TERMS_AND_CONDITIONS: null,
+  CODE_OF_CONDUCT: null,
+  DATA_PRIVACY: null,
+  FAQ: 'https://ocelot.social',
 }
