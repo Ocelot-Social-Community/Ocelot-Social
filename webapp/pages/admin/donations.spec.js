cb16de13d9df7d5d812619416dcf271125902709
Moriz Wahl <moriz.wahl@gmx.de>
2021-07-12 12:20:08 +0200

    suggested solutions

--- webapp/pages/admin/donations.spec.js
+++ webapp/pages/admin/donations.spec.js
@@ -8,24 +8,38 @@ describe('donations.vue', () => {
   let wrapper
   let mocks
 
+  const donationsQueryMock = jest.fn()
+  const donationsUpdateMock = jest.fn()
+  const donationsMutaionMock = jest.fn()
+  donationsMutaionMock.mockResolvedValue({
+    then: jest.fn(),
+    catch: jest.fn(),
+  })
+  
   beforeEach(() => {
     mocks = {
       $t: jest.fn((string) => string),
       $apollo: {
-        // queries: {
-          // Donations: {
-        //     refetch: jest.fn(),
-        //     // fetchMore: jest.fn().mockResolvedValue([
-        //     //   {
-        //     //     id: 'p23',
-        //     //     name: 'It is a post',
-        //     //     author: {
-        //     //       id: 'u1',
-        //     //     },
-        //     //   },
-        //     // ]),
-          // },
-        // },
+        Donations: {
+          query: donationsQueryMock,
+          update: donationsUpdateMock,
+        },
+        mutate: donationsMutaionMock,
+        queries: {
+          Donations: {
+            query: donationsQueryMock,
+            refetch: jest.fn(),
+            fetchMore: jest.fn().mockResolvedValue([
+              {
+                id: 'p23',
+                name: 'It is a post',
+                author: {
+                  id: 'u1',
+                },
+              },
+            ]),
+          },
+        },
         // query: jest.fn().mockResolvedValue({
         //   data: {
         //     Donations: 1,
@@ -100,17 +114,18 @@ describe('donations.vue', () => {
       })
 
       it.skip('on donations-goal and enter value XXX', async () => {
-        wrapper.find('#showDonations').trigger('click') // set to true
         // wrapper.find('[data-test="donations-goal"]').setValue('20000')
         wrapper.find('#donations-goal').setValue('20000')
         await wrapper.vm.$nextTick()
+        console.log(wrapper.find('#donations-goal').element.value)
         expect(wrapper.vm.formData.goal).toBe('20000')
       })
     })
 
     describe('apollo', () => {
       it.skip('query is called', () => {
-        // expect(mocks.$apollo.queries.Donations.refetch).toHaveBeenCalledTimes(1)
+        expect(donationsQueryMock).toHaveBeenCalledTimes(1)
+        expect(mocks.$apollo.queries.Donations.refetch).toHaveBeenCalledTimes(1)
         // expect(mocks.$apollo.Donations.query().exists()).toBeTruthy()
         console.log('mocks.$apollo: ', mocks.$apollo)
       })
@@ -124,27 +139,36 @@ describe('donations.vue', () => {
       })
 
       describe('submit', () => {
-        it.skip('calls mutation with default values once', async () => {
+        beforeEach(() => {
+          jest.clearAllMocks()
+        })
+        
+        it('calls mutation with default values once', () => {
           // TODO: Makes the warning: "UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'error' of undefined"
           wrapper.find('.donations-info-button').trigger('submit')
-          await mocks.$apollo.mutate
-          // await flushPromises()
-          expect(mocks.$apollo.mutate).toHaveBeenCalledWith(expect.objectContaining({variables: { showDonations: false, goal: 15000, progress: 0 }}))
-          expect(mocks.$apollo.mutate).toHaveBeenCalledTimes(1)
+          //await flushPromises()
+          expect(donationsMutaionMock).toHaveBeenCalledWith(
+            expect.objectContaining({
+              variables: { showDonations: false, goal: 15000, progress: 0 },
+            }),
+          )
         })
 
-        it.skip('calls mutation with input values once', async () => {
+        it('calls mutation with input values once', async () => {
           wrapper.find('#showDonations').trigger('click') // set to true
-          await wrapper.vm.$nextTick()
-          // wrapper.find('[data-test="donations-goal"]').setValue('20000')
+          //wrapper.find('[data-test="donations-goal"]').setValue('20000')
           wrapper.find('#donations-goal').setValue('20000')
           await wrapper.vm.$nextTick()
           // expect(wrapper.vm.formData.goal).toBe('20000')
           wrapper.find('.donations-info-button').trigger('submit')
-          await mocks.$apollo.mutate
+          await wrapper.vm.$nextTick()
+          //await mocks.$apollo.mutate
           await flushPromises()
-          expect(mocks.$apollo.mutate).toHaveBeenCalledWith(expect.objectContaining({variables: { showDonations: true, goal: 15000, progress: 0 }}))
-          expect(mocks.$apollo.mutate).toHaveBeenCalledTimes(1)
+          expect(mocks.$apollo.mutate).toHaveBeenCalledWith(
+            expect.objectContaining({
+              variables: { showDonations: true, goal: 20000, progress: 0 },
+            }),
+          )
         })
 
         it.skip('calls mutation with corrected values once', async () => {
@@ -158,7 +182,9 @@ describe('donations.vue', () => {
         })
 
         it.skip('default values are displayed', async () => {
-          mocks.$apollo.mutate = jest.fn().mockResolvedValue({ data: { UpdateDonations: { showDonations: true, goal: 10, progress: 20 } } })
+          mocks.$apollo.mutate = jest.fn().mockResolvedValue({
+            data: { UpdateDonations: { showDonations: true, goal: 10, progress: 20 } },
+          })
           wrapper.find('.donations-info-button').trigger('submit')
           await mocks.$apollo.mutate
           await Vue.nextTick()
