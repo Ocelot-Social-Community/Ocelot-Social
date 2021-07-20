2f045553d41799b66b2dfd4f69d49fad619f83a2
Wolfgang Huß <wolle.huss@pjannto.com>
2021-07-20 11:34:30 +0200

    Implement HTML internal support and organization pages

--- branding/constants/links.js
+++ branding/constants/links.js
@@ -1,10 +1,11 @@
-// this file is duplicated in `backend/src/config/links.js` and `webapp/constants/links.js` and replaced on rebranding
+// this file is duplicated in `backend/src/config/links.js` and `webapp/constants/links.js` and replaced on rebranding by https://github.com/Ocelot-Social-Community/Ocelot-Social-Deploy-Rebranding/tree/master/branding/constants/
 export default {
+  LANDING_PAGE: '/login', // examples: '/login', '/registration', '/organization'
+  SUPPORT: 'https://ocelot.social', // example for internal support page: 'https://staging.ocelot.social/support'
+  
+  // on null or empty strings internal pages are used, see 'webapp/locales/html/'
+  //   you can find and store templates at https://github.com/Ocelot-Social-Community/Ocelot-Social-Deploy-Rebranding/tree/master/branding/templates/
   ORGANIZATION: 'https://ocelot.social',
-  SUPPORT: 'https://ocelot.social',
-
-  // on null or empty strings internal imprint is used, see 'webapp/locales/html/'
-  //   you can find and store templates at 'master/branding/templates/'
   DONATE: 'https://ocelot-social.herokuapp.com/donations', // we use 'ocelot-social.herokuapp.com' at the moment, because redirections of 'ocelot.social' subpages are not working correctly
   IMPRINT: 'https://ocelot-social.herokuapp.com/imprint', // we use 'ocelot-social.herokuapp.com' at the moment, because redirections of 'ocelot.social' subpages are not working correctly
   TERMS_AND_CONDITIONS: null,
