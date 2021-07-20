688af4b8c151c142b8fdd66edaa54aa46da6c264
Wolfgang Huß <wolle.huss@pjannto.com>
2021-07-20 11:49:26 +0200

    Refine comments

--- webapp/constants/links.js
+++ webapp/constants/links.js
@@ -1,8 +1,8 @@
 // this file is duplicated in `backend/src/config/links.js` and `webapp/constants/links.js` and replaced on rebranding by https://github.com/Ocelot-Social-Community/Ocelot-Social-Deploy-Rebranding/tree/master/branding/constants/
 export default {
-  LANDING_PAGE: '/login', // examples: '/login', '/registration', '/organization'
-  SUPPORT: 'https://ocelot.social', // example for internal support page: 'https://staging.ocelot.social/support'
-  
+  LANDING_PAGE: '/login', // examples: '/login', '/registration', '/organization', or external 'https://ocelot.social'
+  SUPPORT: 'https://ocelot.social', // example for internal support page: 'https://staging.ocelot.social/support'. set a full URL please, because it is used in e-mails as well!
+
   // on null or empty strings internal pages are used, see 'webapp/locales/html/'
   //   you can find and store templates at https://github.com/Ocelot-Social-Community/Ocelot-Social-Deploy-Rebranding/tree/master/branding/templates/
   ORGANIZATION: 'https://ocelot.social',
