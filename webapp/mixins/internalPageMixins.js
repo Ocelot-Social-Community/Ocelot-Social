e3ea3e1733bb876677e5885fe98bd22aeb7d3bae
Wolfgang Huß <wolle.huss@pjannto.com>
2021-08-05 16:54:20 +0200

    Fix error 'window is undefined' that appears sometimes
    - Avoid possible errors, because of 'window' is only defined on browser side but not in NodeJS on client side. Check for 'typeof window' is neccessary, because if it's not defined at all you can't check for 'window !== undefined' without the same error 'window is undefined'.

--- webapp/mixins/internalPageMixins.js
+++ webapp/mixins/internalPageMixins.js
@@ -11,7 +11,10 @@ export function internalPageMixins(pageParams) {
     },
     created() {
       if (!this.pageParams.isInternalPage) {
-        window.location.href = this.pageParams.externalLink
+        // to avoid possible errors, because 'window' is only defined on browser side but not in NodeJS on client side. check for 'typeof window' is neccessary, because if it's not defined at all you can't check for 'window !== undefined' without the same error 'window is undefined'
+        if (typeof window !== 'undefined') {
+          window.location.href = this.pageParams.externalLink
+        }
       }
     },
   }
