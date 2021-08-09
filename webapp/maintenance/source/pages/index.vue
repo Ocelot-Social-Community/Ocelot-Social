b37c05ef14fb4145dfe2d73dab64ce71a36675f4
Wolfgang Huß <wolle.huss@pjannto.com>
2021-08-09 14:19:04 +0200

    Add hint to 'page-params-link' on maintenance page

--- webapp/maintenance/source/pages/index.vue
+++ webapp/maintenance/source/pages/index.vue
@@ -8,9 +8,11 @@
         <ds-flex>
           <ds-flex-item :width="{ base: '100%', sm: 1, md: 1 }">
             <ds-space>
-              <!-- this link works only for external pages -->
               <!-- QUESTION: could we have internal page or even all internal pages here as well with PageParamsLink by having the footer underneath? -->
-              <!--   I tried this out, but only could get the nginx page displayed. I guees the there were nuxt errors, because the nuxt config file 'webapp/maintenance/source/nuxt.config.maintenance.js' would have to be refactored for that as well -->
+              <!--   I tried this out, but only could get the nginx page displayed. I guees the there were nuxt errors, because the nuxt config file 'webapp/maintenance/source/nuxt.config.maintenance.js' would have to be refactored for that as well and may be the missing folder `components/_new/generic/` plays a role, see https://github.com/Ocelot-Social-Community/Ocelot-Social/pull/4619 -->
+              <!-- <page-params-link :pageParams="links.ORGANIZATION">
+                <logo type="maintenance" />
+              </page-params-link> -->
               <!-- BUT: not the logo and not even the a-tag is working at the moment -->
               <!-- <a
                 :href="emails.ORGANIZATION_LINK"
@@ -18,7 +20,6 @@
                 target="_blank"
               > -->
               <img class="image" alt="Under maintenance" src="/img/custom/logo-squared.svg" />
-              <!-- <logo type="maintenance" /> -->
               <!-- </a> -->
             </ds-space>
           </ds-flex-item>
