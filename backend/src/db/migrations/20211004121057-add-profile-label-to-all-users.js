72ee0fe479d47b97cdab59dd72ed130c0e82175a
Wolfgang Huß <wolle.huss@pjannto.com>
2021-10-04 12:32:46 +0200

    Migrate db by setting additional label 'Profile' to all 'User' nodes

---
+++ backend/src/db/migrations/20211004121057-add-profile-label-to-all-users.js
@@ -0,0 +1,59 @@
+import { getDriver } from '../../db/neo4j'
+
+export const description = ''
+
+export async function up(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    // Implement your migration here.
+    await transaction.run(
+      `
+        MATCH (user:User)
+        SET user:Profile
+        RETURN user {.*}
+      `,
+    )
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
+
+export async function down(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    // Implement your migration here.
+    await transaction.run(
+      `
+        MATCH (user:Profile)
+        REMOVE user:Profile
+        RETURN user {.*}
+      `,
+    )
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
