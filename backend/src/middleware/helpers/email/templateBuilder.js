0acd7f6501b14b48d44ace0b7cd7b8daed234c12
Wolfgang Huß <wolle.huss@pjannto.com>
2021-11-22 12:48:56 +0100

    Remove 'wolle' from e-mail, because to many wolles in the code

--- backend/src/middleware/helpers/email/templateBuilder.js
+++ backend/src/middleware/helpers/email/templateBuilder.js
@@ -21,7 +21,7 @@ const englishHint = 'English version below!'
 
 export const signupTemplate = ({ email, variables: { nonce, inviteCode = null } }) => {
   const subject = `Willkommen, Bienvenue, Welcome to ${CONFIG.APPLICATION_NAME}!`
-  // dev format example: http://localhost:3000/registration?method=invite-mail&email=wolle.huss%40pjannto.com&nonce=64853
+  // dev format example: http://localhost:3000/registration?method=invite-mail&email=huss%40pjannto.com&nonce=64853
   const actionUrl = new URL('/registration', CONFIG.CLIENT_URI)
   actionUrl.searchParams.set('email', email)
   actionUrl.searchParams.set('nonce', nonce)
