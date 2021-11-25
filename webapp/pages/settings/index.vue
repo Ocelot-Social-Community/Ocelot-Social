51c2d5c3f3120c4bf851fd393cc7d3da4be795db
Wolfgang Huß <wolle.huss@pjannto.com>
2021-11-25 16:21:34 +0100

    Implement ProfileCoreData.vue and use it on settings index page, first step

--- webapp/pages/settings/index.vue
+++ webapp/pages/settings/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <base-card>
+  <base-card class="settings-index-page">
     <h2 class="title">{{ $t('settings.data.name') }}</h2>
     <ds-form v-model="form" :schema="formSchema" @submit="submit">
       <template #default="{ errors }">
-        <ds-input
+        <!-- <ds-input
           id="name"
           model="name"
           icon="user"
           :label="$t('settings.data.labelName')"
           :placeholder="$t('settings.data.namePlaceholder')"
-        />
-        <ds-input id="slug" model="slug" icon="at" :label="$t('settings.data.labelSlug')" />
+        /> -->
+        <!-- <ds-input id="slug" model="slug" icon="at" :label="$t('settings.data.labelSlug')" /> -->
         <!-- eslint-disable vue/use-v-on-exact -->
-        <ds-select
+        <!-- <ds-select
           id="city"
           model="locationName"
           icon="map-marker"
@@ -21,16 +21,17 @@
           :placeholder="$t('settings.data.labelCity')"
           :loading="loadingGeo"
           @input.native="handleCityInput"
-        />
+        /> -->
         <!-- eslint-enable vue/use-v-on-exact -->
-        <ds-input
+        <!-- <ds-input
           id="about"
           model="about"
           type="textarea"
           rows="3"
           :label="$t('settings.data.labelBio')"
           :placeholder="$t('settings.data.labelBio')"
-        />
+        /> -->
+        <profile-core-data :currentUser="currentUser" />
         <base-button icon="check" :disabled="errors" type="submit" :loading="loadingData" filled>
           {{ $t('actions.save') }}
         </base-button>
@@ -44,10 +45,15 @@ import { mapGetters, mapMutations } from 'vuex'
 import UniqueSlugForm from '~/components/utils/UniqueSlugForm'
 import { updateUserMutation } from '~/graphql/User'
 import { queryLocations } from '~/graphql/location'
+import ProfileCoreData from '~/components/_new/features/ProfileCoreData/ProfileCoreData'
+import { formSchemaProfileCoreData } from '~/components/_new/features/ProfileCoreData/ProfileCoreData'
 
 let timeout
 
 export default {
+  components: {
+    ProfileCoreData,
+  },
   data() {
     return {
       cities: [],
@@ -61,13 +67,16 @@ export default {
       currentUser: 'auth/user',
     }),
     formSchema() {
-      const uniqueSlugForm = UniqueSlugForm({
-        apollo: this.$apollo,
-        currentUser: this.currentUser,
-        translate: this.$t,
-      })
+      // Wolle const uniqueSlugForm = UniqueSlugForm({
+      //   apollo: this.$apollo,
+      //   currentUser: this.currentUser,
+      //   translate: this.$t,
+      // })
+      // return {
+      //   ...uniqueSlugForm.formSchema,
+      // }
       return {
-        ...uniqueSlugForm.formSchema,
+        ...formSchemaProfileCoreData(this),
       }
     },
     form: {
@@ -117,43 +126,51 @@ export default {
         this.loadingData = false
       }
     },
-    handleCityInput(value) {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => this.requestGeoData(value), 500)
-    },
-    processLocationsResult(places) {
-      if (!places.length) {
-        return []
-      }
-      const result = []
-      places.forEach((place) => {
-        result.push({
-          label: place.place_name,
-          value: place.place_name,
-          id: place.id,
-        })
-      })
+    // Wolle handleCityInput(value) {
+    //   clearTimeout(timeout)
+    //   timeout = setTimeout(() => this.requestGeoData(value), 500)
+    // },
+    // processLocationsResult(places) {
+    //   if (!places.length) {
+    //     return []
+    //   }
+    //   const result = []
+    //   places.forEach((place) => {
+    //     result.push({
+    //       label: place.place_name,
+    //       value: place.place_name,
+    //       id: place.id,
+    //     })
+    //   })
 
-      return result
-    },
-    async requestGeoData(e) {
-      const value = e.target ? e.target.value.trim() : ''
-      if (value === '') {
-        this.cities = []
-        return
-      }
-      this.loadingGeo = true
+    //   return result
+    // },
+    // async requestGeoData(e) {
+    //   const value = e.target ? e.target.value.trim() : ''
+    //   if (value === '') {
+    //     this.cities = []
+    //     return
+    //   }
+    //   this.loadingGeo = true
 
-      const place = encodeURIComponent(value)
-      const lang = this.$i18n.locale()
+    //   const place = encodeURIComponent(value)
+    //   const lang = this.$i18n.locale()
 
-      const {
-        data: { queryLocations: res },
-      } = await this.$apollo.query({ query: queryLocations(), variables: { place, lang } })
+    //   const {
+    //     data: { queryLocations: res },
+    //   } = await this.$apollo.query({ query: queryLocations(), variables: { place, lang } })
 
-      this.cities = this.processLocationsResult(res)
-      this.loadingGeo = false
-    },
+    //   this.cities = this.processLocationsResult(res)
+    //   this.loadingGeo = false
+    // },
   },
 }
 </script>
+
+<style lang="scss">
+.settings-index-page {
+  .base-button {
+    margin-top: $space-small;
+  }
+}
+</style>
