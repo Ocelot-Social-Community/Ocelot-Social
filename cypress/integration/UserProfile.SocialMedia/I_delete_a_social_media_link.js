16fa5e2cecd6f2f056ec0d62a254ed37b7800831
Wolfgang Huß <wolle.huss@pjannto.com>
2022-06-22 10:53:43 +0200

    Remove comment

--- cypress/integration/UserProfile.SocialMedia/I_delete_a_social_media_link.js
+++ cypress/integration/UserProfile.SocialMedia/I_delete_a_social_media_link.js
@@ -3,4 +3,5 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 When('I delete a social media link', () => {
   cy.get(".base-button[title='Delete']")
     .click()
+  // Wolle: add delition confirmation modal click
 })
