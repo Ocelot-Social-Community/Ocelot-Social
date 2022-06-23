343e9e8c260a298d244f9ad6733be8cf7ff15e5c
ogerly <fridolin@tutanota.com>
2022-06-23 21:30:25 +0200

    add maetamask data list

--- webapp/pages/settings/wallet.vue
+++ webapp/pages/settings/wallet.vue
@@ -2,38 +2,48 @@
   <base-card>
     <h2 class="title">{{ $t('settings.wallet.walletConnect') }}</h2>
     <div>
-        <h3>MetaMask</h3>
-        <hr />
-        {{metamaskData}}
-         <br />
-        <vue-metamask ref="metamask" :initConnect="false"></vue-metamask>
-        
-        
-        <base-button @click="connect">connect</base-button>
+      <h3>MetaMask</h3>
+      <hr />
+      <div v-for="value in data" :key="value">{{value}}</div>
+      <br />
+      <vue-metamask ref="metamask" :initConnect="false"  @onComplete="onComplete"></vue-metamask>
+
+      <base-button @click="connect">connect</base-button>
     </div>
-      
-     
   </base-card>
 </template>
 
 <script>
-import VueMetamask from 'vue-metamask';
+import VueMetamask from 'vue-metamask'
 
 export default {
-    name: 'settings-wallet',
-    components: {
-        VueMetamask,
-    },
-    data(){
-        return {
-            msg: "This is demo net work",
-            metamaskData: 'datas',
-        }
-    },
-    methods:{
-        connect() {
-                this.$refs.metamask.init();
-            }
+  name: 'settings-wallet',
+  components: {
+    VueMetamask,
+  },
+  data() {
+    return {
+      msg: 'This is demo net work',
+      data: {
+        message: null,
+        metaMaskAddress: null,
+        netID: null,
+        type: null,
+      },
     }
+  },
+  methods: {
+    connect() {
+      this.$refs.metamask.init()
+      // console.log('connect:', this.$refs.metamask.init())
+    },
+    onComplete(data) {
+        console.log('data:', data)
+        this.data.message = data['message']
+        this.data.metaMaskAddress = data['metaMaskAddress']
+        this.data.netID = data['netID']
+        this.data.type = data['type']
+    }
+  },
 }
 </script>
