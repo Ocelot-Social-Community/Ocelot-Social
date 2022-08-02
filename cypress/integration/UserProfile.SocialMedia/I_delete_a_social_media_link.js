8f336a0d13315d2761714a19fd88e232700b1e63
Wolfgang Huß <wolle.huss@pjannto.com>
2022-08-02 08:30:14 +0200

    Add todo comment to Cypress test

--- cypress/integration/UserProfile.SocialMedia/I_delete_a_social_media_link.js
+++ cypress/integration/UserProfile.SocialMedia/I_delete_a_social_media_link.js
@@ -3,4 +3,5 @@ import { When } from "cypress-cucumber-preprocessor/steps";
 When('I delete a social media link', () => {
   cy.get(".base-button[title='Delete']")
     .click()
+  // TODO: add delition confirmation modal click, see issue
 })
