b3c179011a05d30fab24bd925c0f56c5796e0a0d
Wolfgang Huß <wolle.huss@pjannto.com>
2022-08-22 11:45:51 +0200

    Add URL comment for sleep

--- backend/src/helpers/jest.js
+++ backend/src/helpers/jest.js
@@ -10,6 +10,7 @@ export function gql(strings) {
 
 // sometime we have to wait to check a db state by having a look into the db in a certain moment
 // or we wait a bit to check if we missed to set an await somewhere
+// see: https://www.sitepoint.com/delay-sleep-pause-wait/
 export function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
