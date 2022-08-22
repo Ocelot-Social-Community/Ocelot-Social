c1f1408f6c7670f9305643b7440e5c9af0b17beb
Moriz Wahl <moriz.wahl@gmx.de>
2022-08-22 19:17:50 +0200

    add yunite categories

---
+++ backend/src/constants/categories.js
@@ -0,0 +1,98 @@
+export const categories = [
+  {
+    icon: 'users',
+    name: 'networking',
+    description: 'Kooperation, Aktionsbündnisse, Solidarität, Hilfe',
+  },
+  {
+    icon: 'home',
+    name: 'home',
+    description: 'Bauen, Lebensgemeinschaften, Tiny Houses, Gemüsegarten',
+  },
+  {
+    icon: 'lightbulb',
+    name: 'energy',
+    description: 'Öl, Gas, Kohle, Wind, Wasserkraft, Biogas, Atomenergie, ...',
+  },
+  {
+    icon: 'smile',
+    name: 'psyche',
+    description: 'Seele, Gefühle, Glück',
+  },
+  {
+    icon: 'movement',
+    name: 'body-and-excercise',
+    description: 'Sport, Yoga, Massage, Tanzen, Entspannung',
+  },
+  {
+    icon: 'balance-scale',
+    name: 'law',
+    description: 'Menschenrechte, Gesetze, Verordnungen',
+  },
+  {
+    icon: 'money',
+    name: 'finance',
+    description: 'Geld, Finanzsystem, Alternativwährungen, ...',
+  },
+  {
+    icon: 'child',
+    name: 'children',
+    description: 'Familie, Pädagogik, Schule, Prägung',
+  },
+  {
+    icon: 'suitcase',
+    name: 'mobility',
+    description: 'Reise, Verkehr, Elektromobilität',
+  },
+  {
+    icon: 'shopping-cart',
+    name: 'economy',
+    description: 'Handel, Konsum, Marketing, Lebensmittel, Lieferketten, ...',
+  },
+  {
+    icon: 'angellist',
+    name: 'peace',
+    description: 'Krieg, Militär, soziale Verteidigung, Waffen, Cyberattacken',
+  },
+  {
+    icon: 'university',
+    name: 'politics',
+    description: 'Demokratie, Mitbestimmung, Wahlen, Korruption, Parteien',
+  },
+  {
+    icon: 'tree',
+    name: 'nature',
+    description: 'Tiere, Pflanzen, Landwirtschaft, Ökologie, Artenvielfalt',
+  },
+  {
+    icon: 'graduation-cap',
+    name: 'science',
+    description: 'Bildung, Hochschule, Publikationen, ...',
+  },
+  {
+    icon: 'medkit',
+    name: 'health',
+    description: 'Medizin, Ernährung, WHO, Impfungen, Schadstoffe, ...',
+  },
+  {
+    icon: 'desktop',
+    name: 'IT-and-media',
+    description:
+      'Nachrichten, Manipulation, Datenschutz, Überwachung, Datenkraken, AI, Software, Apps',
+  },
+  {
+    icon: 'heart-o',
+    name: 'spirituality',
+    description: 'Religion, Werte, Ethik',
+  },
+  {
+    icon: 'music',
+    name: 'culture',
+    description: 'Kunst, Theater, Musik, Fotografie, Film',
+  },
+  {
+    icon: 'ellipsis-h',
+    name: 'miscellaneous',
+    description: '',
+  },
+]
