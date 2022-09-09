362aae3c5b519d26c774ffede8274c75de490403
ogerly <fridolin@tutanota.com>
2022-09-09 11:10:11 +0200

    add router in group settings

--- webapp/pages/group/edit/_id.vue
+++ webapp/pages/group/edit/_id.vue
@@ -3,68 +3,39 @@
     <ds-page-title heading="Group Setting"></ds-page-title>
     <ds-flex gutter="small">
       <ds-flex-item :width="{ base: '100%', md: '200px' }">
-        <ds-list>
-          <ds-list-item>
-            <div @click="menu = 'default'">
-              <ds-text :color="menu === 'default' ? 'primary' : ''" Group data>Group Data</ds-text>
-            </div>
-          </ds-list-item>
-          <ds-list-item>
-            <div @click="menu = 'members'">
-              <ds-text :color="menu === 'members' ? 'primary' : ''" Group data>Members</ds-text>
-            </div>
-          </ds-list-item>
-          <ds-list-item>
-            <div @click="menu = 'socialMedia'">
-              <ds-text :color="menu === 'socialMedia' ? 'primary' : ''" Group data>
-                Social Media
-              </ds-text>
-            </div>
-          </ds-list-item>
-          <ds-list-item>
-            <div @click="menu = 'links'">
-              <ds-text :color="menu === 'links' ? 'primary' : ''" Group data>Links</ds-text>
-            </div>
-          </ds-list-item>
-        </ds-list>
+        <ds-menu :routes="routes" :is-exact="() => true" />
       </ds-flex-item>
       <ds-flex-item :width="{ base: '100%', md: 1 }">
-        <group-form
-          v-show="menu === 'default'"
-          @updateGroup="updateGroup"
-          :group="group"
-          :update="true"
-        />
-        <group-member v-show="menu === 'members'" />
-        <div v-show="menu === 'socialMedia'">Social Media</div>
-        <group-link v-show="menu === 'links'" />
+        <transition name="slide-up" appear>
+          <nuxt-child :group="group" :update="true" @updateGroup="updateGroup" />
+        </transition>
       </ds-flex-item>
     </ds-flex>
+   
   </div>
 </template>
 
 <script>
-import GroupForm from '~/components/Group/GroupForm'
-import GroupMember from '~/components/Group/GroupMember'
-import GroupLink from '~/components/Group/GroupLink'
 import { groupQuery, updateGroupMutation } from '~/graphql/groups.js'
 import { mapGetters } from 'vuex'
 
 export default {
-  components: {
-    GroupForm,
-    GroupMember,
-    GroupLink,
-  },
-  data() {
-    return {
-      menu: 'default',
-    }
-  },
+
   computed: {
     ...mapGetters({
       user: 'auth/user',
     }),
+    routes() {
+      return [
+      {
+            name: 'formular',
+            path: `/group/edit/${this.group.id}`
+          },
+          {
+            name: 'members',
+            path: `/group/edit/${this.group.id}/members`
+          },
+    ]},
   },
   async asyncData(context) {
     const {
