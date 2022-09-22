4d480d3130abc443340417d3497b4c6ef7a07789
Moriz Wahl <moriz.wahl@gmx.de>
2022-09-22 08:16:06 +0200

    add post query

--- backend/src/db/graphql/posts.js
+++ backend/src/db/graphql/posts.js
@@ -3,14 +3,42 @@ import gql from 'graphql-tag'
 // ------ mutations
 
 export const createPostMutation = gql`
-  mutation ($id: ID, $title: String!, $slug: String, $content: String!, $categoryIds: [ID]!) {
-    CreatePost(id: $id, title: $title, slug: $slug, content: $content, categoryIds: $categoryIds) {
+  mutation (
+    $id: ID
+    $title: String!
+    $slug: String
+    $content: String!
+    $categoryIds: [ID]
+    $groupId: ID
+  ) {
+    CreatePost(
+      id: $id
+      title: $title
+      slug: $slug
+      content: $content
+      categoryIds: $categoryIds
+      groupId: $groupId
+    ) {
       id
       slug
+      title
+      content
     }
   }
 `
 
 // ------ queries
+
+export const postQuery = () => {
+  return gql`
+    query Post($id: ID!) {
+      Post(id: $id) {
+        id
+        title
+        content
+      }
+    }
+  `
+}
 
 // fill queries in here
