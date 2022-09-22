870fe89a80ca031782629a60aff3ccfbe134797c
ogerly <fridolin@tutanota.com>
2022-09-22 19:49:24 +0200

    refactor rebranding

---
+++ branding/constants/headerMenu.js
@@ -0,0 +1,12 @@
+export default {
+  MENU: [
+    // {
+    //   name: 'Beiträge',
+    //   path: '/#',
+    // },
+    // {
+    //   name: 'Über Yunite',
+    //   url: 'https://yunite.org',
+    // },
+  ],
+}
