abcfe259a09d51b26076ce0252807db04119d447
Moriz Wahl <moriz.wahl@gmx.de>
2022-10-03 22:41:25 +0200

    query and mutations are functions

--- backend/src/db/graphql/posts.js
+++ backend/src/db/graphql/posts.js
@@ -2,30 +2,32 @@ import gql from 'graphql-tag'
 
 // ------ mutations
 
-export const createPostMutation = gql`
-  mutation (
-    $id: ID
-    $title: String!
-    $slug: String
-    $content: String!
-    $categoryIds: [ID]
-    $groupId: ID
-  ) {
-    CreatePost(
-      id: $id
-      title: $title
-      slug: $slug
-      content: $content
-      categoryIds: $categoryIds
-      groupId: $groupId
+export const createPostMutation = () => {
+  return gql`
+    mutation (
+      $id: ID
+      $title: String!
+      $slug: String
+      $content: String!
+      $categoryIds: [ID]
+      $groupId: ID
     ) {
-      id
-      slug
-      title
-      content
+      CreatePost(
+        id: $id
+        title: $title
+        slug: $slug
+        content: $content
+        categoryIds: $categoryIds
+        groupId: $groupId
+      ) {
+        id
+        slug
+        title
+        content
+      }
     }
-  }
-`
+  `
+}
 
 // ------ queries
 
