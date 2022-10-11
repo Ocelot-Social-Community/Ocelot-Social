7cb1663be75810cbebc490106503b33483e2392f
Moriz Wahl <moriz.wahl@gmx.de>
2022-10-11 14:35:27 +0200

    change from role eq pending to usual, admin, owner includes role

--- backend/src/schema/resolvers/groups.js
+++ backend/src/schema/resolvers/groups.js
@@ -287,16 +287,16 @@ export default {
       const session = context.driver.session()
       const writeTxResultPromise = session.writeTransaction(async (transaction) => {
         let postRestrictionCypher = ''
-        if (roleInGroup === 'pending') {
-          postRestrictionCypher = `
-            WITH group, member, membership
-            FOREACH (post IN [(p:Post)-[:IN]->(group) | p] |
-              MERGE (member)-[:CANNOT_SEE]->(post))`
-        } else {
+        if (['usual', 'admin', 'owner'].includes(roleInGroup)) {
           postRestrictionCypher = `
             WITH group, member, membership
             FOREACH (restriction IN [(member)-[r:CANNOT_SEE]->(:Post)-[:IN]->(group) | r] |
               DELETE restriction)`
+        } else {
+          postRestrictionCypher = `
+            WITH group, member, membership
+            FOREACH (post IN [(p:Post)-[:IN]->(group) | p] |
+              MERGE (member)-[:CANNOT_SEE]->(post))`
         }
 
         const joinGroupCypher = `
