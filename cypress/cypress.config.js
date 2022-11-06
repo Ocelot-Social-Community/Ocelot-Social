096b69a205c602d9ce71bd126c0904c26f5708f2
mahula <lenzmath@posteo.de>
2022-11-06 16:40:13 +0100

    Cypress: move plugins configuration to new config file

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,5 +1,44 @@
 const { defineConfig } = require("cypress");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 
+const dotenv = require('dotenv');
+
+// Import backend .env (smart)?
+const { parsed } = dotenv.config({ path: require.resolve('../backend/.env') });
+
+// Test persistent(between commands) store
+const testStore = {}
+
+async function setupNodeEvents(on, config) {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  config.env.NEO4J_URI = parsed.NEO4J_URI
+  config.env.NEO4J_USERNAME = parsed.NEO4J_USERNAME
+  config.env.NEO4J_PASSWORD = parsed.NEO4J_PASSWORD
+  config.env.JWT_SECRET = parsed.JWT_SECRET
+
+  on("task", {
+    pushValue({ name, value }) {
+      testStore[name] = value
+      return true
+    },
+    getValue(name) {
+      console.log("getValue",name,testStore)
+      return testStore[name]
+    },
+  });
+
+  on("after:run", (results) => {
+    if (results) {
+      console.log(results.status);
+    }
+  });
+
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
