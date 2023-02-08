58c9c8455770648d84783fbc10ab38eb4c40d768
Moriz Wahl <moriz.wahl@gmx.de>
2023-02-08 11:51:14 +0100

    min length of group description to 50

--- backend/src/constants/groups.js
+++ backend/src/constants/groups.js
@@ -1,3 +1,3 @@
 // this file is duplicated in `backend/src/constants/group.js` and `webapp/constants/group.js`
-export const DESCRIPTION_WITHOUT_HTML_LENGTH_MIN = 100 // with removed HTML tags
+export const DESCRIPTION_WITHOUT_HTML_LENGTH_MIN = 50 // with removed HTML tags
 export const DESCRIPTION_EXCERPT_HTML_LENGTH = 250 // with removed HTML tags
