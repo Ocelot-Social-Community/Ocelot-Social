8e8565810cfd85a694fcd2f64d02b5b36ea749f3
ogerly <fridolin@tutanota.com>
2023-02-09 13:39:30 +0100

    fix lint

--- webapp/components/HeaderMenu/HeaderMenu.vue
+++ webapp/components/HeaderMenu/HeaderMenu.vue
@@ -314,7 +314,7 @@ export default {
           this.hideNavbar = false
         } else {
           this.hideNavbar = true
-        } 
+        }
       }
       this.prevScrollpos = currentScrollPos
     },
