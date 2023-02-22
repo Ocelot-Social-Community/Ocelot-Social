e5b43cd1e263691d9a75776ef9e9d8d238fd6b2a
Moriz Wahl <moriz.wahl@gmx.de>
2023-02-22 21:34:45 +0100

    log node ids

--- backend/src/db/factories.js
+++ backend/src/db/factories.js
@@ -100,6 +100,7 @@ Factory.define('user')
       neode.create('EmailAddress', { email: options.email }),
       options.avatar,
     ])
+    console.log(user.id(), email.id(), avatar.id())
     await Promise.all([user.relateTo(email, 'primaryEmail'), email.relateTo(user, 'belongsTo')])
     if (avatar) await user.relateTo(await avatar, 'avatar')
     return user
