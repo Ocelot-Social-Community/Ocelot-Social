42a370ffc86c60316deb225fb1d91f9cf3ee7a35
Markus <markus@infinity.labs.ooo>
2023-02-27 10:42:20 +0100

    set showDonations flag to false as default to avoid blinking

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -149,7 +149,7 @@ export default {
       hideByScroll: false,
       revScrollpos: 0,
       showFilter: false,
-      showDonations: true,
+      showDonations: false,
       goal: 15000,
       progress: 7000,
       posts: [],
