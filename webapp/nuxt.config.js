60e4ff28a70bd59eaf50bbeac872436d4fbe5f57
Wolfgang Huß <wolle.huss@pjannto.com>
2023-03-14 07:39:25 +0100

    Remove comment why 'viewport' has to be changed

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -62,7 +62,6 @@ export default {
       },
       {
         name: 'viewport',
-        // content: 'width=device-width, initial-scale=1', // "width=device-width" does not work correctly for Safari on iOS, see https://stackoverflow.com/a/37136777
         content: 'initial-scale=1',
       },
       {
