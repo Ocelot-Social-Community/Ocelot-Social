67d15db533b2a52900794730c82fbcffed7e04cb
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-15 12:25:41 +0100

    cluster upgrade to specific version

--- deployment/scripts/cluster.upgrade.sh
+++ deployment/scripts/cluster.upgrade.sh
@@ -7,7 +7,13 @@ SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 # configuration
 CONFIGURATION=${CONFIGURATION:-"example"}
 KUBECONFIG=${KUBECONFIG:-${SCRIPT_DIR}/../configurations/${CONFIGURATION}/kubeconfig.yaml}
-VALUES=${SCRIPT_DIR}/../configurations/${CONFIGURATION}/kubernetes/values.yaml
+VALUES=${SCRIPT_DIR}/../configurations/${CONFIGURATION}/kubernetes/values.
+DOCKERHUB_OCELOT_TAG=${DOCKERHUB_OCELOT_TAG:-"latest"}
 
 # upgrade with helm
-helm --kubeconfig=${KUBECONFIG} upgrade ocelot --values ${VALUES} ${SCRIPT_DIR}/../src/kubernetes/ --debug --timeout 10m
+helm --kubeconfig=${KUBECONFIG} upgrade ocelot \
+  --values ${VALUES} \
+  --set appVersion="${DOCKERHUB_OCELOT_TAG}"
+  ${SCRIPT_DIR}/../src/kubernetes/ \
+  --debug \
+  --timeout 10m
