b3f5bf4e6a7e4d0a923de80cbb00b1093db38c3b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-20 14:06:10 +0100

    new migration to create search indexes

---
+++ backend/src/db/migrations/20230320130345-fulltext-search-indexes.js
@@ -0,0 +1,54 @@
+import { getDriver } from '../../db/neo4j'
+
+export const description = ''
+
+export async function up(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    // Drop all indexes because due to legacy code they might be set already
+    await transaction.run(`CALL db.index.fulltext.drop("user_fulltext_search")`)
+    await transaction.run(`CALL db.index.fulltext.drop("post_fulltext_search")`)
+    await transaction.run(`CALL db.index.fulltext.drop("tag_fulltext_search")`)
+    // Create indexes
+    await transaction.run(`CALL db.index.fulltext.createNodeIndex("user_fulltext_search",["User"],["name", "slug"])`)
+    await transaction.run(`CALL db.index.fulltext.createNodeIndex("post_fulltext_search",["Post"],["title", "content"])`)
+    await transaction.run(`CALL db.index.fulltext.createNodeIndex("tag_fulltext_search",["Tag"],["id"])`)
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
+
+export async function down(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    await transaction.run(`CALL db.index.fulltext.drop("user_fulltext_search")`)
+    await transaction.run(`CALL db.index.fulltext.drop("post_fulltext_search")`)
+    await transaction.run(`CALL db.index.fulltext.drop("tag_fulltext_search")`)
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
