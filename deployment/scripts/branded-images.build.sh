3a4f7b40b68205f43503c614fb9eae3f41a8c476
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-03-21 10:57:05 +0100

    supply github run number via json payload, tag branded images with ocelot build number

--- deployment/scripts/branded-images.build.sh
+++ deployment/scripts/branded-images.build.sh
@@ -19,15 +19,20 @@ echo "Using CONFIGURATION=${CONFIGURATION}"
 # configuration
 DOCKERHUB_ORGANISATION=${DOCKERHUB_ORGANISATION:-"ocelotsocialnetwork"}
 OCELOT_VERSION=${OCELOT_VERSION:-$(node -p -e "require('${SCRIPT_DIR}/../../package.json').version")}
+OCELOT_GITHUB_RUN_NUMBER=${OCELOT_GITHUB_RUN_NUMBER:-master}
+OCELOT_VERSION_BUILD=${OCELOT_VERSION_BUILD:-${OCELOT_VERSION}-${OCELOT_GITHUB_RUN_NUMBER}}
 BRANDED_VERSION=${BRANDED_VERSION:-${GITHUB_RUN_NUMBER:-"local"}}
 BUILD_DATE=${BUILD_DATE:-$(date -u +'%Y-%m-%dT%H:%M:%SZ')}
-BUILD_VERSION=${BRANDED_VERSION}-ocelot.social${OCELOT_VERSION}
+BUILD_VERSION_BASE=${BRANDED_VERSION}-ocelot.social${OCELOT_VERSION}
+BUILD_VERSION=${BRANDED_VERSION}-ocelot.social${OCELOT_VERSION_BUILD}
 BUILD_COMMIT=${GITHUB_SHA:-"0000000"}
 
 # backend
 docker build --target branded \
     -t "${DOCKERHUB_ORGANISATION}/backend-branded:latest" \
     -t "${DOCKERHUB_ORGANISATION}/backend-branded:${OCELOT_VERSION}" \
+    -t "${DOCKERHUB_ORGANISATION}/backend-branded:${OCELOT_VERSION_BUILD}" \
+    -t "${DOCKERHUB_ORGANISATION}/backend-branded:${BUILD_VERSION_BASE}" \
     -t "${DOCKERHUB_ORGANISATION}/backend-branded:${BUILD_VERSION}" \
     -f "${SCRIPT_DIR}/../src/docker/backend.Dockerfile" \
     --build-arg "CONFIGURATION=${CONFIGURATION}" \
@@ -39,6 +44,8 @@ docker build --target branded \
 docker build --target branded \
     -t "${DOCKERHUB_ORGANISATION}/webapp-branded:latest" \
     -t "${DOCKERHUB_ORGANISATION}/webapp-branded:${OCELOT_VERSION}" \
+    -t "${DOCKERHUB_ORGANISATION}/webapp-branded:${OCELOT_VERSION_BUILD}" \
+    -t "${DOCKERHUB_ORGANISATION}/webapp-branded:${BUILD_VERSION_BASE}" \
     -t "${DOCKERHUB_ORGANISATION}/webapp-branded:${BUILD_VERSION}" \
     -f "${SCRIPT_DIR}/../src/docker/webapp.Dockerfile" \
     --build-arg "CONFIGURATION=${CONFIGURATION}" \
@@ -50,6 +57,8 @@ docker build --target branded \
 docker build --target branded \
     -t "${DOCKERHUB_ORGANISATION}/maintenance-branded:latest" \
     -t "${DOCKERHUB_ORGANISATION}/maintenance-branded:${OCELOT_VERSION}" \
+    -t "${DOCKERHUB_ORGANISATION}/maintenance-branded:${OCELOT_VERSION_BUILD}" \
+    -t "${DOCKERHUB_ORGANISATION}/maintenance-branded:${BUILD_VERSION_BASE}" \
     -t "${DOCKERHUB_ORGANISATION}/maintenance-branded:${BUILD_VERSION}" \
     -f "${SCRIPT_DIR}/../src/docker/maintenance.Dockerfile" \
     --build-arg "CONFIGURATION=${CONFIGURATION}" \
