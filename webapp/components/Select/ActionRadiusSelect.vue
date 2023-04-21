c951a900ad3f165349c03777cc9e1d0b81ef0a3c
mahula <lenzmath@posteo.de>
2023-04-21 12:03:34 +0200

    add action radius select component

---
+++ webapp/components/Select/ActionRadiusSelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <select
+    class="select ds-input appearance--auto"
+    name="actionRadius"
+    :value="value"
+    @change="onActionRadiusChange"
+  >
+    <option
+      v-for="actionRadius in actionRadiusOptions"
+      :key="actionRadius"
+      :value="actionRadius"
+    >
+      {{ $t(`group.actionRadii.${actionRadius}`) }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'ActionRadiusSelect',
+  props: ['value'],
+  data() {
+    const actionRadius = ''
+    return {
+      actionRadiusOptions: ['regional', 'national', 'continental', 'global'],
+    }
+  },
+  methods: {
+    onActionRadiusChange(event) {
+      this.$emit('change', event.target.value)
+    }
+  }
+}
+</script>
