3244f3f86d1e8c09e0fd49f43c49f0a3aa8b85ab
Moriz Wahl <moriz.wahl@gmx.de>
2023-04-25 12:35:46 +0200

    feat(webapp): group categories on posts

--- webapp/components/CategoriesSelect/CategoriesSelect.vue
+++ webapp/components/CategoriesSelect/CategoriesSelect.vue
@@ -10,9 +10,9 @@
       :icon="category.icon"
       size="small"
       v-tooltip="{
-        content: $t(`contribution.category.description.${category.slug}`),
-        placement: 'bottom-start',
-      }"
+             content: $t(`contribution.category.description.${category.slug}`),
+             placement: 'bottom-start',
+             }"
     >
       {{ $t(`contribution.category.name.${category.slug}`) }}
     </base-button>
@@ -20,75 +20,82 @@
 </template>
 
 <script>
-import CategoryQuery from '~/graphql/CategoryQuery'
-import { CATEGORIES_MAX } from '~/constants/categories.js'
-import xor from 'lodash/xor'
-import SortCategories from '~/mixins/sortCategoriesMixin.js'
+ import CategoryQuery from '~/graphql/CategoryQuery'
+ import { CATEGORIES_MAX } from '~/constants/categories.js'
+ import xor from 'lodash/xor'
+ import SortCategories from '~/mixins/sortCategoriesMixin.js'
 
-export default {
-  inject: {
-    $parentForm: {
-      default: null,
-    },
-  },
-  mixins: [SortCategories],
-  props: {
-    existingCategoryIds: { type: Array, default: () => [] },
-    model: { type: String, required: true },
-  },
-  data() {
-    return {
-      categories: null,
-      selectedMax: CATEGORIES_MAX,
-      selectedCategoryIds: this.existingCategoryIds,
-    }
-  },
-  computed: {
-    selectedCount() {
-      return this.selectedCategoryIds.length
-    },
-    reachedMaximum() {
-      return this.selectedCount >= this.selectedMax
-    },
-  },
-  methods: {
-    toggleCategory(id) {
-      this.selectedCategoryIds = xor(this.selectedCategoryIds, [id])
-      if (this.$parentForm) {
-        this.$parentForm.update(this.model, this.selectedCategoryIds)
-      }
-    },
-    isActive(id) {
-      return this.selectedCategoryIds.includes(id)
-    },
-    isDisabled(id) {
-      return !!(this.reachedMaximum && !this.isActive(id))
-    },
-    categoryButtonsId(categoryId) {
-      return `category-buttons-${categoryId}`
-    },
-  },
-  apollo: {
-    Category: {
-      query() {
-        return CategoryQuery()
-      },
-      result({ data: { Category } }) {
-        this.categories = this.sortCategories(Category)
-      },
-    },
-  },
-}
+ export default {
+   inject: {
+     $parentForm: {
+       default: null,
+     },
+   },
+   mixins: [SortCategories],
+   props: {
+     existingCategoryIds: { type: Array, default: () => [] },
+     model: { type: String, required: true },
+   },
+   data() {
+     return {
+       categories: null,
+       selectedMax: CATEGORIES_MAX,
+       selectedCategoryIds: this.existingCategoryIds,
+     }
+   },
+   watch: {
+     existingCategoryIds() {
+       console.log('existingCategoryIds', this.selectedCategoryIds)
+       if (!this.selectedCategoryIds.length)
+         this.selectedCategoryIds = this.existingCategoryIds
+     },
+   },
+   computed: {
+     selectedCount() {
+       return this.selectedCategoryIds.length
+     },
+     reachedMaximum() {
+       return this.selectedCount >= this.selectedMax
+     },
+   },
+   methods: {
+     toggleCategory(id) {
+       this.selectedCategoryIds = xor(this.selectedCategoryIds, [id])
+       if (this.$parentForm) {
+         this.$parentForm.update(this.model, this.selectedCategoryIds)
+       }
+     },
+     isActive(id) {
+       return this.selectedCategoryIds.includes(id)
+     },
+     isDisabled(id) {
+       return !!(this.reachedMaximum && !this.isActive(id))
+     },
+     categoryButtonsId(categoryId) {
+       return `category-buttons-${categoryId}`
+     },
+   },
+   apollo: {
+     Category: {
+       query() {
+         return CategoryQuery()
+       },
+       result({ data: { Category } }) {
+         this.categories = this.sortCategories(Category)
+       },
+     },
+   },
+ }
 </script>
 
 <style lang="scss">
-.categories-select {
-  display: flex;
-  flex-wrap: wrap;
+ .categories-select {
+   display: flex;
+   flex-wrap: wrap;
 
-  > .base-button {
-    margin-right: $space-xx-small;
-    margin-bottom: $space-xx-small;
-  }
-}
+   > .base-button {
+     margin-right: $space-xx-small;
+     margin-bottom: $space-xx-small;
+   }
+ }
 </style>
