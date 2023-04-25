43ed69803639da47a093edcdc049016108124b69
mahula <lenzmath@posteo.de>
2023-04-25 17:31:34 +0200

    add missing option to action radius select

--- webapp/components/Select/ActionRadiusSelect.vue
+++ webapp/components/Select/ActionRadiusSelect.vue
@@ -21,7 +21,7 @@ export default {
   },
   data() {
     return {
-      actionRadiusOptions: ['regional', 'national', 'continental', 'global'],
+      actionRadiusOptions: ['regional', 'national', 'continental', 'global', 'interplanetary'],
     }
   },
   methods: {
