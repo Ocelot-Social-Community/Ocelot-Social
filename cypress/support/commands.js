18288efa1bba4819e4aaaf3abb5890929264ee3e
mahula <lenzmath@posteo.de>
2023-05-26 10:47:35 +0200

    wait for network to be idle in cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -51,7 +51,7 @@ Cypress.Commands.add(
   (graphQLClient, mutation, variables, response) => {
     return new Cypress.Promise(async (resolve, reject) => {
       const response = await graphQLClient.request(mutation, variables)
-      console.log('Cypress command mutate:', data)
+      console.log('Cypress command mutate:', response)
       graphQLClient.request(mutation, variables).then(() => resolve(graphQLClient))
 
     })
