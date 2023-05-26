7528d32dfce11581b3a4f18b19841d4ae9d99252
mahula <lenzmath@posteo.de>
2023-05-26 12:53:18 +0200

    add network interception to cypress test

--- cypress/integration/Moderation.ReportContent/somebody_reported_the_following_posts.js
+++ cypress/integration/Moderation.ReportContent/somebody_reported_the_following_posts.js
@@ -2,10 +2,12 @@ import { Given } from "cypress-cucumber-preprocessor/steps";
 import 'cypress-network-idle';
 
 Given('somebody reported the following posts:', table => {
+  const reportIdRegex = /^[0-9a-zA-Z]{8}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{12}$/
   cy.intercept({
     method: 'POST',
     url: '/',
     hostname: 'localhost',
+    port: 4000,
   }).as('postToLocalhoast')
 
   table.hashes().forEach(({ submitterEmail, resourceId, reasonCategory, reasonDescription }) => {
@@ -36,6 +38,7 @@ Given('somebody reported the following posts:', table => {
         cy.wrap(interception.response.statusCode).should('eq', 200)
         cy.wrap(interception.response.body)
           .should('have.nested.property', 'data.fileReport.reportId')
+          .and('match', reportIdRegex)
       })
       console.log('Cypress waited for fileReport request')
       cy.waitForNetworkIdle(2000)
