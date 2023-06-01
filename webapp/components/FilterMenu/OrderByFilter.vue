0de47d5163e6a24fc1328f6628a60a5f0718254b
Wolfgang Huß <wolle.huss@pjannto.com>
2023-06-01 17:18:24 +0200

    Fix typo of 'buttonLabel'

--- webapp/components/FilterMenu/OrderByFilter.vue
+++ webapp/components/FilterMenu/OrderByFilter.vue
@@ -4,7 +4,7 @@
       <li class="item">
         <labeled-button
           icon="sort-amount-asc"
-          :label="buttonLable('desc')"
+          :label="buttonLabel('desc')"
           :filled="orderBy === orderedDesc"
           :title="buttonTitle('desc')"
           @click="setOrder(orderedDesc)"
@@ -14,7 +14,7 @@
       <li class="item">
         <labeled-button
           icon="sort-amount-desc"
-          :label="buttonLable('asc')"
+          :label="buttonLabel('asc')"
           :filled="orderBy === orderedAsc"
           :title="buttonTitle('asc')"
           @click="setOrder(orderedAsc)"
@@ -51,12 +51,16 @@ export default {
     },
   },
   methods: {
-    buttonLable(buttonType) {
+    buttonLabel(buttonType) {
       switch (buttonType) {
         case 'asc':
-          return this.orderedByCreationDate ? this.$t('filter-menu.order.oldest.label') : this.$t('filter-menu.order.last.label')
+          return this.orderedByCreationDate
+            ? this.$t('filter-menu.order.oldest.label')
+            : this.$t('filter-menu.order.last.label')
         case 'desc':
-          return this.orderedByCreationDate ? this.$t('filter-menu.order.newest.label') : this.$t('filter-menu.order.next.label')
+          return this.orderedByCreationDate
+            ? this.$t('filter-menu.order.newest.label')
+            : this.$t('filter-menu.order.next.label')
         default:
           return ''
       }
@@ -64,9 +68,13 @@ export default {
     buttonTitle(buttonType) {
       switch (buttonType) {
         case 'asc':
-          return this.orderedByCreationDate ? this.$t('filter-menu.order.oldest.hint') : this.$t('filter-menu.order.last.hint')
+          return this.orderedByCreationDate
+            ? this.$t('filter-menu.order.oldest.hint')
+            : this.$t('filter-menu.order.last.hint')
         case 'desc':
-          return this.orderedByCreationDate ? this.$t('filter-menu.order.newest.hint') : this.$t('filter-menu.order.next.hint')
+          return this.orderedByCreationDate
+            ? this.$t('filter-menu.order.newest.hint')
+            : this.$t('filter-menu.order.next.hint')
         default:
           return ''
       }
