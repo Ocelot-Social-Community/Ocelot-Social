20ad70362ae23bfa6865fce9ade8b42b8909ce78
mahula <lenzmath@posteo.de>
2023-06-03 06:00:53 +0200

    fix social media test step

--- cypress/support/step_definitions/UserProfile.SocialMedia/I_delete_the_social_media_link_{string}.js
+++ cypress/support/step_definitions/UserProfile.SocialMedia/I_delete_the_social_media_link_{string}.js
@@ -8,5 +8,5 @@ When('I delete the social media link {string}', (link) => {
   cy.get('[data-test="confirm-button"]')
     .click()
   cy.get('.ds-list-item-content > a')
-    .contains(link).should('not.exist')
+    .should('not.exist')
 })
