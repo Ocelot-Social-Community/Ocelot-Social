aca8e4fbb26e74819b7b7962f7bb2beb71078f07
elweyn <heine.hannes@gmail.com>
2023-06-06 13:27:05 +0200

    Create an input checkbox component.

---
+++ webapp/components/InputCheckbox/InputCheckbox.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      v-model="currentValue"
+      :name="name"
+    />
+    {{ label }}
+  </div>
+</template>
+<script>
+export default {
+  inject: {
+    $parentForm: {
+      default: null,
+    },
+  },
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    label: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    }
+  },
+  watch: {
+    currentValue() {
+    //   this.$emit('input', this.currentValue)
+      this.$parentForm.update('formData.isOnlineEvent', this.currentValue)
+    },
+  },
+}
+</script>
