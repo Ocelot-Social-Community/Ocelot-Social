31e1efd356cb35c03f82c7845e62e6bccf489942
elweyn <heine.hannes@gmail.com>
2023-06-08 19:45:58 +0200

    Remove cypress changes.

--- cypress/support/step_definitions/Notification.Mention/mention_{string}_in_the_text.js
+++ cypress/support/step_definitions/Notification.Mention/mention_{string}_in_the_text.js
@@ -3,6 +3,7 @@ import { When } from "@badeball/cypress-cucumber-preprocessor";
 When("mention {string} in the text", mention => {
   cy.get(".ProseMirror")
     .type(" @");
-  cy.contains(".suggestion-list__item", mention)
+  cy.get(".suggestion-list__item")
+    .contains(mention)
     .click();
 });
