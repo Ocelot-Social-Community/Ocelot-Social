3f7f3c265105112eb9a9167c497ca91776b48952
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-06-13 14:22:25 +0200

    "fix" decoder

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -2,7 +2,7 @@
 // https://stackoverflow.com/questions/68468203/why-am-i-getting-textencoder-is-not-defined-in-jest
 import { TextEncoder, TextDecoder } from 'util'
 global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+global.TextDecoder = TextDecoder as any
 
 // Metascraper takes longer nowadays, double time
 jest.setTimeout(10000)
