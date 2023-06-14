aecc50fceb6e1dd8c073906cbbbd9343e0f0b7c6
Moriz Wahl <moriz.wahl@gmx.de>
2023-06-14 12:09:52 +0200

    simple create room mutation

---
+++ backend/src/schema/resolvers/rooms.ts
@@ -0,0 +1,34 @@
+export default {
+  Mutation: {
+    CreateRoom: async (_parent, params, context, _resolveInfo) => {
+      const { userId } = params
+      const { user: { id: currentUserId } } = context
+      const session = context.driver.session()
+      const writeTxResultPromise = session.writeTransaction(async (transaction) => {
+        const createRoomCypher = `
+          MATCH (currentUser:User { id: $currentUserId })
+          MATCH (user:User { id: $userId })
+          MERGE (currentUser)-[:CHATS_IN]->(room:Room)<-[:CHATS_IN]-(user)
+          SET room.createdAt = toString(datetime())
+          RETURN room
+        `
+        const createRommTxResponse = await await transaction.run(
+          createRoomCypher,
+          { userId, currentUserId }
+        )
+        const [room] = await createRommTxResponse.records.map((record) =>
+                                                              record.get('room'),
+                                                             )
+        return room
+      })
+      try {
+        const room = await writeTxResultPromise
+        return room
+      } catch (error) {
+        throw new Error(error)
+      } finally {
+        session.close()
+      }      
+    }
+  }
+}
