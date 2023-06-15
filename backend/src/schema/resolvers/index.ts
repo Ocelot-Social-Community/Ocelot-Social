3ca6e4d6cfe49fc8b15441325ecdd28c7d5ec40e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-06-15 13:58:26 +0200

    comment to explain the bug

--- backend/src/schema/resolvers/index.ts
+++ backend/src/schema/resolvers/index.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import { fileLoader, mergeResolvers } from 'merge-graphql-schemas'
 
+// the files must be correctly evaluated in built and dev state - therefore accept both js & ts files
 const resolversArray = fileLoader(path.join(__dirname, './!(*.spec).(ts|js)'))
 export default mergeResolvers(resolversArray)
