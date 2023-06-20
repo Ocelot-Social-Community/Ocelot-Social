56816f0d982c553027db4d06a28f53b39073db54
Wolfgang Huß <wolle.huss@pjannto.com>
2023-06-20 14:22:26 +0200

    Simplify 'forceTargetBlank' in call of 'page-params-link'

--- webapp/components/Registration/RegistrationSlideCreate.vue
+++ webapp/components/Registration/RegistrationSlideCreate.vue
@@ -89,11 +89,11 @@
           <label for="checkbox0">
             {{ $t('components.registration.create-user-account.termsAndCondsEtcConfirmed') }}
             <br />
-            <page-params-link :pageParams="links.TERMS_AND_CONDITIONS" :forceTargetBlank="true">
+            <page-params-link :pageParams="links.TERMS_AND_CONDITIONS" forceTargetBlank>
               {{ $t('site.termsAndConditions') }}
             </page-params-link>
             <br />
-            <page-params-link :pageParams="links.DATA_PRIVACY" :forceTargetBlank="true">
+            <page-params-link :pageParams="links.DATA_PRIVACY" forceTargetBlank>
               {{ $t('site.data-privacy') }}
             </page-params-link>
           </label>
