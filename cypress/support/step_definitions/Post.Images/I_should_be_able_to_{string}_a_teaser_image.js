242619d21ba25e3b439d49f2a6d6c23acf3bd952
mahula <lenzmath@posteo.de>
2023-07-11 11:19:47 +0200

    remove unnecessary cypress code from step definitions

--- cypress/support/step_definitions/Post.Images/I_should_be_able_to_{string}_a_teaser_image.js
+++ cypress/support/step_definitions/Post.Images/I_should_be_able_to_{string}_a_teaser_image.js
@@ -1,24 +1,24 @@
 import { Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Then("I should be able to {string} a teaser image", condition => {
+  let postTeaserImage = ""
+
   switch(condition){
     case "change":
+      postTeaserImage = "humanconnection.png"
       cy.get(".delete-image-button")
         .click()
-      cy.fixture("humanconnection.png").as("postTeaserImage").then(function() {
-        cy.get("#postdropzone").selectFile(
-          { contents: this.postTeaserImage, fileName: "humanconnection.png", mimeType: "image/png" },
-          { action: "drag-drop" }
-        ).wait(750);
-      })
+      cy.get("#postdropzone").selectFile(
+        { contents: `cypress/fixtures/${postTeaserImage}`, fileName: postTeaserImage, mimeType: "image/png" },
+        { action: "drag-drop" }
+      ).wait(750);
       break;
     case "add":
-      cy.fixture("onourjourney.png").as("postTeaserImage").then(function() {
-        cy.get("#postdropzone").selectFile(
-          { contents: this.postTeaserImage, fileName: "onourjourney.png", mimeType: "image/png" },
-          { action: "drag-drop" }
-        ).wait(750);
-      })
+      postTeaserImage = "onourjourney.png"
+      cy.get("#postdropzone").selectFile(
+        { contents: `cypress/fixtures/${this.postTeaserImage}`, fileName: postTeaserImage, mimeType: "image/png" },
+        { action: "drag-drop" }
+      ).wait(750);
       break;
     case "remove":
       cy.get(".delete-image-button")
--- cypress/support/step_definitions/UserProfile.Avatar/I_should_be_able_to_change_my_profile_picture.js
+++ cypress/support/step_definitions/UserProfile.Avatar/I_should_be_able_to_change_my_profile_picture.js
@@ -2,13 +2,11 @@ import { Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Then("I should be able to change my profile picture", () => {
   const avatarUpload = "onourjourney.png";
-  
-  cy.fixture(avatarUpload, "base64").then(fileContent => {
-    cy.get("#customdropzone").selectFile(
-      { contents: `cypress/fixtures/${avatarUpload}`, fileName: avatarUpload, mimeType: "image/png" },
-      { action: "drag-drop" }
-    );
-  });
+
+  cy.get("#customdropzone").selectFile(
+    { contents: `cypress/fixtures/${avatarUpload}`, fileName: avatarUpload, mimeType: "image/png" },
+    { action: "drag-drop" }
+  );
   cy.get(".profile-page-avatar img")
     .should("have.attr", "src")
     .and("contains", "onourjourney");
