604e1d8465bc6054eebc1f16b6778c6d34507cb9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-07-20 13:44:23 +0200

    subscription roomCountUpdated security fix

--- webapp/graphql/Rooms.js
+++ webapp/graphql/Rooms.js
@@ -52,8 +52,8 @@ export const unreadRoomsQuery = () => {
 
 export const roomCountUpdated = () => {
   return gql`
-    subscription roomCountUpdated($userId: ID!) {
-      roomCountUpdated(userId: $userId)
+    subscription roomCountUpdated {
+      roomCountUpdated
     }
   `
 }
