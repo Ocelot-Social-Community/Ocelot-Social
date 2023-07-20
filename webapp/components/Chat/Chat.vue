f7c381efd820e11f872544516df3c35ec0c6e850
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-07-20 13:44:04 +0200

    subscription chatMessageAdded security fix

--- webapp/components/Chat/Chat.vue
+++ webapp/components/Chat/Chat.vue
@@ -195,9 +195,6 @@ export default {
     // Subscriptions
     const observer = this.$apollo.subscribe({
       query: chatMessageAdded(),
-      variables: {
-        userId: this.currentUser.id,
-      },
     })
 
     observer.subscribe({
