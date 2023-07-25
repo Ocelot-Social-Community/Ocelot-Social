56170df98e0b6c114bc0f946556b9c6ab7733d68
Wolfgang Huß <wolle.huss@pjannto.com>
2023-07-25 16:35:12 +0200

    Test 'backend/src/middleware/notifications/mentions/extractMentionedUsers.ts'

--- backend/src/middleware/notifications/mentions/extractMentionedUsers.spec.ts
+++ backend/src/middleware/notifications/mentions/extractMentionedUsers.spec.ts
@@ -8,6 +8,8 @@ const contentWithPlainLinks =
   '<p>Something inspirational about <a class="mention" href="/profile/u2" target="_blank">@bob-der-baumeister</a> and <a href="/profile/u3" target="_blank">@jenny-rostock</a>.</p>'
 const contentWithDuplicateIds =
   'One more mention to <a data-mention-id="you" class="mention" href="/profile/you"> @al-capone </a> and again: <a data-mention-id="you" class="mention" href="/profile/you"> @al-capone </a> and again <a data-mention-id="you" class="mention" href="/profile/you"> @al-capone </a>'
+const contentWithAllMention =
+  'Mention to all <a data-mention-id="all" class="mention" href="/profile/you">@all</a> and to <a data-mention-id="u2" class="mention" href="/profile/u2">@bob-der-baumeister</a> and to <a data-mention-id="u3" class="mention" href="/profile/u3">@jenny-rostock </a>'
 
 describe('extractMentionedUsers', () => {
   describe('content undefined', () => {
@@ -20,11 +22,15 @@ describe('extractMentionedUsers', () => {
     expect(extractMentionedUsers(contentWithPlainLinks)).toEqual([])
   })
 
+  it('extracts @all mention and others', () => {
+    expect(extractMentionedUsers(contentWithAllMention)).toEqual(['all', 'u2', 'u3'])
+  })
+
   it('removes duplicates', () => {
     expect(extractMentionedUsers(contentWithDuplicateIds)).toEqual(['you'])
   })
 
-  describe('given a link with .mention class and `data-mention-id` attribute ', () => {
+  describe('given a link with .mention class and `data-mention-id` attribute', () => {
     it('extracts ids', () => {
       expect(extractMentionedUsers(contentWithMentions)).toEqual(['u3'])
     })
