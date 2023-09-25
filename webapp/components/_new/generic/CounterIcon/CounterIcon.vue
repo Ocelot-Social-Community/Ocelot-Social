d07fa4be0ea68c8164771c7931821f5a2d317f89
Tarun Samanta <tarunsamanta77@gmail.com>
2023-09-26 02:39:47 +0530

    Update CounterIcon.vue

--- webapp/components/_new/generic/CounterIcon/CounterIcon.vue
+++ webapp/components/_new/generic/CounterIcon/CounterIcon.vue
@@ -1,4 +1,4 @@
-<template v-if="cappedCount>0">
+<template v-show="cappedCount > 0">
   <span class="counter-icon">
     <base-icon :name="icon" />
     <span :class="counterClass">{{ cappedCount }}</span>
