e3e634d848bd84c6afd6fa2114eabbe136365c65
Wolfgang Huß <wolle.huss@pjannto.com>
2023-11-02 12:35:42 +0100

    Add deployment script 'clusters.cron-backups.sh'

---
+++ deployment/scripts/clusters.cron-backups.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# base setup
+SCRIPT_PATH=$(realpath $0)
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+
+# check BACKUP_CONFIGURATIONS
+if [ -z ${BACKUP_CONFIGURATIONS} ]; then
+  echo "You must provide a `BACKUP_CONFIGURATIONS` via environment variable"
+  exit 1
+fi
+
+# convert configurations to array
+CONFIGURATIONS_ARRAY=($BACKUP_CONFIGURATIONS)
+
+# display the clusters
+echo "Backup the clusters:"
+for i in "${CONFIGURATIONS_ARRAY[@]}"
+do
+  echo $i
+done
+echo "Cancel by ^C. You have 20 seconds"
+# wait for the admin to react
+sleep 20
+
+# save old CONFIGURATION for later reset
+SAVE_CONFIGURATION=$CONFIGURATION
+
+for i in "${CONFIGURATIONS_ARRAY[@]}"
+do
+  CONFIGURATION=$i
+  # individual cluster backup
+  ${SCRIPT_DIR}/cluster.backup.sh
+done
+
+# reset CONFIGURATION to old
+CONFIGURATION=$SAVE_CONFIGURATION
+echo "Reset to CONFIGURATION=${CONFIGURATION}"
