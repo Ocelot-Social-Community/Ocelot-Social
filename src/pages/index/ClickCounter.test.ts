e6f34c0a813179519292fb180e86b968dc6df36f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-16 12:43:56 +0100

    vitest eslint plugin

--- src/pages/index/ClickCounter.test.ts
+++ src/pages/index/ClickCounter.test.ts
@@ -3,11 +3,11 @@ import { describe, it, expect } from 'vitest'
 
 import ClickCounter from './ClickCounter.vue'
 
-describe('ClickCounter', () => {
+describe('clickCounter', () => {
   const wrapper = mount(ClickCounter)
 
   it('renders Button with a Counter of 0', () => {
-    expect(wrapper.find('v-btn').exists()).toBe(true)
+    expect(wrapper.find('v-btn').exists()).toBeTruthy()
     expect(wrapper.text()).toBe("$t('counter') 0")
   })
 })
