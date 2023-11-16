a8276912a55cfab04eef47d2173a79556e20c11d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-16 14:02:11 +0100

    fixed i18n in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 import vue from '@vitejs/plugin-vue'
 import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
@@ -6,6 +7,9 @@ const config: UserConfig = {
   plugins: [
     vue(),
     process.env.STORYBOOK !== 'true' && vike(), // SSR only when storybook is not running
+    vueI18n({
+      ssr: true,
+    }),
   ],
   ssr: { noExternal: ['vuetify'] },
 }
