b846f6273ac18aab8190f1a10e50c8feae794bc7
mahula <lenzmath@posteo.de>
2023-11-16 22:42:43 +0100

    add basic custom booter layout

---
+++ .vuepress/layouts/Layout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="my-footer">CC BY busFaktor() e.V. & Authors</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.my-footer {
+  text-align: center;
+  height: 50px;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+}
+</style>
