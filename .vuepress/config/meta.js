0e5520bf3f851d6b6f0741a5a00b9ac6db401165
mahula <lenzmath@posteo.de>
2023-11-17 19:07:39 +0100

    move meta config in separate file

---
+++ .vuepress/config/meta.js
@@ -0,0 +1,8 @@
+export default {
+  base: '/',
+  title: 'Ocelot-Social Documentation',
+  description: 'Ocelot-Social Documentation',
+  head: [
+    ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}],
+  ],
+}
