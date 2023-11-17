5c28726813e22ecc63c07a2c046f8d4336597ce6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-17 18:54:33 +0100

    proper coverage check

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,15 @@ export default mergeConfig(
       globals: true,
       environment: 'happy-dom',
       setupFiles: ['tests/mock.$t.ts', 'tests/plugin.vuetify.ts'],
+      coverage: {
+        all: true,
+        include: ['src/**/*.{js,jsx,ts,tsx,vue}'],
+        lines: 5.81,
+        functions: 0,
+        branches: 11.11,
+        statements: 5.81,
+        // 100: true,
+      },
     },
   }),
 )
