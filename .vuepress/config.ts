1dcf26a1cc62f372d5c3e1a08848907e0b5b865e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-17 18:26:05 +0100

    also lint .vuepress folder

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -3,5 +3,5 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   title: 'IT4C Frontend Boilerplate Documentation',
   description: 'IT4C Frontend Boilerplate Documentation',
-  dest: 'build/docs'
+  dest: 'build/docs',
 })
