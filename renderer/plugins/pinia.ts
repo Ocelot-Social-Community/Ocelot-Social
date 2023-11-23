5f529ebebed11d208ffd0b9dd5b46b6c749f705a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-23 04:58:08 +0100

    pinia store for counter

--- renderer/plugins/pinia.ts
+++ renderer/plugins/pinia.ts
@@ -1,3 +1,6 @@
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-export default createPinia()
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+export default pinia
