d0038cffb6b0fa08d2cf41a62fd78d76cb501110
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-23 04:57:20 +0100

    .env

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+const META = {
+  DEFAULT_TITLE: import.meta.env.PUBLIC_ENV__META__DEFAULT_TITLE ?? 'IT4C',
+  DEFAULT_DESCRIPTION:
+    import.meta.env.PUBLIC_ENV__META__DEFAULT_DESCRIPTION ?? 'IT4C Frontend Boilerplate',
+}
+
+export { META }
