15c492d7cf44645e9101a8944a1ac6994c38071c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-30 09:56:53 +0100

    vuepress base

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,5 +4,5 @@ export default defineUserConfig({
   title: 'IT4C Frontend Boilerplate Documentation',
   description: 'IT4C Frontend Boilerplate Documentation',
   dest: 'build/docs',
-  base: '/boilerplate-frontend/',
+  base: process.env.VUEPRESS_BASE ?? '/',
 })
