f59a679d3b6d8047e34d11008a851e57dda070bb
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-01 15:06:03 +0100

    fix type error vuepress base

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,5 +4,5 @@ export default defineUserConfig({
   title: 'IT4C Frontend Boilerplate Documentation',
   description: 'IT4C Frontend Boilerplate Documentation',
   dest: 'build/docs',
-  base: process.env.VUEPRESS_BASE ?? '/',
+  base: `/${process.env.VUEPRESS_BASE}/` ?? '/',
 })
