7d4a75a94c31b00c1af40cd271c3e82b5293341c
Wolfgang Huß <wolle.huss@pjannto.com>
2023-12-01 15:02:25 +0100

    Refine checking of definition of variables

--- deployment/scripts/cluster.backup.sh
+++ deployment/scripts/cluster.backup.sh
@@ -9,7 +9,7 @@ SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
 # check CONFIGURATION
-if [[ -z ${CONFIGURATION} ]]; then
+if [[ -z "$CONFIGURATION" ]] || [[ $CONFIGURATION == "" ]]; then
   echo "!!! You must provide a CONFIGURATION via environment variable !!!"
   exit 1
 fi
