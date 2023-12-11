7e5a7f02f676c8f00d023ddb223ccab880254db5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-11 17:08:35 +0100

    add tests working form dreammall

---
+++ src/pages/_error.page.test.ts
@@ -0,0 +1,48 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+
+import ErrorPage from './_error.page.vue'
+
+describe('ErrorPage', () => {
+  let wrapper
+  const Wrapper = () => {
+    return mount(ErrorPage)
+  }
+
+  beforeEach(() => {
+    wrapper = Wrapper()
+  })
+
+  describe('no is404 property set', () => {
+    it('renders error 500', () => {
+      expect(wrapper.find('h1').text()).toEqual("$t('error.500.h1')")
+      expect(wrapper.find('p').text()).toEqual("$t('error.500.text')")
+    })
+  })
+
+  describe('is404 property is false', () => {
+    beforeEach(async () => {
+      await wrapper.setProps({
+        is404: false,
+      })
+    })
+
+    it('renders error 500', () => {
+      expect(wrapper.find('h1').text()).toEqual("$t('error.500.h1')")
+      expect(wrapper.find('p').text()).toEqual("$t('error.500.text')")
+    })
+  })
+
+  describe('is404 property is true', () => {
+    beforeEach(async () => {
+      await wrapper.setProps({
+        is404: true,
+      })
+    })
+
+    it('renders error 400', () => {
+      expect(wrapper.find('h1').text()).toEqual("$t('error.404.h1')")
+      expect(wrapper.find('p').text()).toEqual("$t('error.404.text')")
+    })
+  })
+})
