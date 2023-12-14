c431b621ec4a1d00dac4bb52f83976beae69ca66
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-12-14 01:18:29 +0100

    lint fixes

--- server/index.ts
+++ server/index.ts
@@ -63,7 +63,8 @@ async function startServer() {
         next()
       } else {
         const { body, statusCode, headers, earlyHints } = httpResponse
-        if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+        if (res.writeEarlyHints)
+          res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
         headers.forEach(([name, value]) => res.setHeader(name, value))
         res.status(statusCode)
         // For HTTP streams use httpResponse.pipe() instead, see https://vike.dev/stream
