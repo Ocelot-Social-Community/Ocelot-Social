5e30de11d0dfe68121be61a6ffcf2110420d5ff3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-02-08 13:27:07 +0100

    hackfix for image not displaying

--- src/components/menu/LogoAvatar.vue
+++ src/components/menu/LogoAvatar.vue
@@ -5,3 +5,10 @@
 <script lang="ts" setup>
 import Logo from '#assets/it4c-logo2-clean-bg_alpha-128x128.png'
 </script>
+
+<style lang="scss">
+/* stylelint-disable-next-line selector-class-pattern */
+.v-img__img {
+  display: block !important;
+}
+</style>
