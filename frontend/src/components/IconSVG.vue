d65d574e1d4202fa567c78870a6491ed1be2b548
Wolfgang Huß <wolle.huss@pjannto.com>
2024-03-19 08:06:17 +0100

    Add ocelot SVGs to assets

---
+++ frontend/src/components/IconSVG.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+// import icons, { iconNames } from '#assets/icons'
+// import icons from '#assets/icons/svgs/angle-down.svg'
+
+// const iconsX = { icons }
+</script>
+
+<template>
+  <!-- <v-icon aria-label="My Account" role="img" aria-hidden="false"> $icons['angle-down'] </v-icon> -->
+  <v-icon aria-label="My Account" role="img" aria-hidden="false" icon="$vuetify"></v-icon>
+</template>
