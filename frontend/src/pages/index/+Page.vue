790e5497be6f524dff4aca01e49c102111d5e4ae
Wolfgang Huß <wolle.huss@pjannto.com>
2024-04-23 14:14:39 +0200

    Adjust home page

--- frontend/src/pages/index/+Page.vue
+++ frontend/src/pages/index/+Page.vue
@@ -7,26 +7,13 @@
     <br />
     <p>{{ $t('home.text3') }}</p>
     <br />
-    <p>{{ $t('home.text4') }} <v-icon icon="mdi-heart" color="red" /></p>
+    <p>{{ $t('home.text4') }} <v-icon icon="mdi-heart" color="error" /></p>
     <br />
     <p>{{ $t('home.greet1') }}</p>
     <p>{{ $t('home.greet2') }}</p>
-    <v-icon icon="mdi-heart" color="primary" />
-    <v-icon icon="mdi-heart" color="primary-darken-1" />
-    <v-icon icon="mdi-heart" color="secondary" />
-    <v-icon icon="mdi-heart" color="error" />
-    <v-icon icon="mdi-heart" :color="$vuetify.theme.themes.dark.colors.primary" />
-    <!-- <v-icon icon="mdi-heart" :color="$vuetify.theme.themes.dark.variables['theme-kbd']" /> -->
-    <v-icon icon="mdi-heart" />
   </DefaultLayout>
 </template>
 
 <script lang="ts" setup>
 import DefaultLayout from '#layouts/DefaultLayout.vue'
 </script>
-
-<style lang="scss">
-.icon-color {
-  color: var(--v-theme-primary);
-}
-</style>
