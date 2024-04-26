9fccaa57bb6070a637b7c89392596a61f7d41514
Wolfgang Huß <wolle.huss@pjannto.com>
2024-04-26 10:56:46 +0200

    Draft a script to have options for the 'cluster.install.sh', second approach, works

--- deployment/scripts/cluster.install.test.sh
+++ deployment/scripts/cluster.install.test.sh
@@ -17,31 +17,62 @@ VALUES=${SCRIPT_DIR}/../configurations/${CONFIGURATION}/kubernetes/values.yaml
 DOCKERHUB_OCELOT_TAG=${DOCKERHUB_OCELOT_TAG:-"latest"}
 
 
-# see https://kodekloud.com/blog/bash-getopts/
-while getopts -l "certmanager,ocelot,nginx" -- "chon" option; do
-  case ${option} in
-    h) # display help
-      echo
-      echo "Syntax: scriptTemplate [-c|h|o|n]"
-      echo "options:"
-      echo "c     certmanager"
-      echo "h     help"
-      echo "o     ocelot"
-      echo "n     nginx"
-      echo
-      ;;
-    c | certmanager) # certmanager
-      echo "certmanager"
-      ;;
-    o) # ocelot
-      echo "ocelot"
-      ;;
-    n) # nginx
-      echo "nginx"
-      ;;
-    *) # invalid option
-      echo "Invalid option: -${OPTARG}."
-      exit 1
-      ;;
-   esac
-done
+# show help message
+display_help() {
+  echo "Install ocelot.social and/or optional infrastructure pods"
+  echo "Usage: script.sh [options]"
+  echo ""
+  echo "Options:"
+  echo "  -h, --help           Displays this help message"
+  echo "  -c, --certmanager    Option -c: Certificate manager"
+  echo "  -n, --nginx          Option -n: nginx"
+  echo "  -o, --ocelot         Option -o: ocelot.social"
+}
+
+# standard message if no option has been specified
+display_error() {
+  echo "Error: An option is required."
+  echo "Use -h or --help for more information."
+}
+
+# main function
+main() {
+  # checking the options with while and getopts
+  while getopts ":hcn:o:" opt; do
+    case ${opt} in
+      h | help)
+        display_help
+        exit 0
+        ;;
+      c | certmanager)
+        echo "Install certmanager …"
+        # Fügen Sie hier den entsprechenden Befehl für -c hinzu
+        echo "certmanager SUCCESSfully installed!"
+        ;;
+      n | nginx)
+        echo "Install nginx …"
+        # Fügen Sie hier den entsprechenden Befehl für -n hinzu
+        echo "nginx SUCCESSfully installed!"
+        ;;
+      o | ocelot)
+        echo "Install ocelot.social …"
+        # Fügen Sie hier den entsprechenden Befehl für -o hinzu
+        echo "ocelot.social SUCCESSfully installed!"
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        display_help
+        exit 1
+        ;;
+    esac
+  done
+
+  # check whether an option has been specified
+  if [ $OPTIND -eq 1 ]; then
+    display_error
+    exit 1
+  fi
+}
+
+# call main function
+main "$@"
