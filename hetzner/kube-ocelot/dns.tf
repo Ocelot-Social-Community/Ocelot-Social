83aa3de314bcbff8758c3e3e3649a7961f7b49a4
Robert Schäfer <git@roschaefer.de>
2024-10-12 21:03:50 +0200

    setup dns records automatically

--- hetzner/kube-ocelot/dns.tf
+++ hetzner/kube-ocelot/dns.tf
@@ -8,20 +8,23 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
+resource "cloudflare_zone" "roschaeferde" {
+  account_id = "96f99f4d8611e917dbc3f12ecde6b1e0"
+  zone       = "roschaefer.de"
+}
+
 resource "cloudflare_record" "terraform_managed_resource_f74d9e9ce7f4efa02204fac4f5296647" {
-  content = "128.140.24.181"
+  content = module.kube-hetzner.ingress_public_ipv4
   name    = "ocelot-social"
   proxied = false
-  ttl     = 1
   type    = "A"
-  zone_id = "075910ecd8f8366d1f1bc7cfd3c8835c"
+  zone_id = cloudflare_zone.roschaeferde.id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_aad453c5a627017d66250b3513322148" {
-  content = "2a01:4f8:c01e:534::1"
+  content = module.kube-hetzner.ingress_public_ipv6
   name    = "ocelot-social"
   proxied = false
-  ttl     = 1
   type    = "AAAA"
-  zone_id = "075910ecd8f8366d1f1bc7cfd3c8835c"
+  zone_id = cloudflare_zone.roschaeferde.id
 }
