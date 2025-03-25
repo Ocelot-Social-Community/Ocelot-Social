405f6bc0c6c9c3ec4c88a038fe2ece6b139f06bb
Moriz Wahl <moriz.wahl@gmx.de>
2025-03-25 17:01:34 +0100

    fix unit tests

--- backend/src/middleware/slugifyMiddleware.spec.ts
+++ backend/src/middleware/slugifyMiddleware.spec.ts
@@ -21,6 +21,9 @@ const { server } = createServer({
       driver,
       neode,
       user: authenticatedUser,
+      cypherParams: {
+        currentUserId: authenticatedUser ? authenticatedUser.id : null,
+      },
     }
   },
 })
