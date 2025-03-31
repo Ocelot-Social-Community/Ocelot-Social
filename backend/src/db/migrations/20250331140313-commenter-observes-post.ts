c69dc9ec9a40544183b004ab84437ae032505660
Moriz Wahl <moriz.wahl@gmx.de>
2025-03-31 14:15:52 +0200

    migrate commenting users to observe commented posts

---
+++ backend/src/db/migrations/20250331140313-commenter-observes-post.ts
@@ -0,0 +1,61 @@
+import { getDriver } from '../../db/neo4j'
+
+export const description = `
+All users commenting a post observe the post.
+`
+
+export async function up(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    // Implement your migration here.
+    await transaction.run(`
+      MATCH (commenter:User)-[:COMMENTS]->(post:Post)
+      MERGE (commenter)-[obs:OBSERVES]->(post)
+      ON CREATE SET
+        obs.active = true,
+        obs.createdAt = toString(datetime()),
+        obs.updatedAt = toString(datetime())
+      RETURN post
+    `)
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
+
+export async function down(next) {
+  const driver = getDriver()
+  const session = driver.session()
+  const transaction = session.beginTransaction()
+
+  try {
+    // Implement your migration here.
+    await transaction.run(`
+      MATCH (u:User)-[obs:OBSERVES]->(p:Post)<-[:COMMENTS]-(u)
+      DELETE obs
+      RETURN p
+    `)
+    await transaction.commit()
+    next()
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error)
+    await transaction.rollback()
+    // eslint-disable-next-line no-console
+    console.log('rolled back')
+    throw new Error(error)
+  } finally {
+    session.close()
+  }
+}
