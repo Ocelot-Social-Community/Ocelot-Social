57c22b9dab1750cdd6e496cd305147f3c91cf2e8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-04-07 01:48:52 +0200

    eslint jest

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -201,7 +201,7 @@ module.exports = {
         'jest/prefer-to-have-length': 'error',
         'jest/valid-expect': 'error',
         '@typescript-eslint/unbound-method': 'off',
-        // 'jest/unbound-method': 'error',
+        'jest/unbound-method': 'error',
       },
     },
   ],
