fe7bab46753991da7cb918ab19a75bc3337b67b0
Moriz Wahl <moriz.wahl@gmx.de>
2025-04-09 10:36:56 +0200

    posts and comments created by the factory set the observe relation (#8344)

--- backend/src/db/factories.ts
+++ backend/src/db/factories.ts
@@ -175,6 +175,7 @@ Factory.define('post')
     ])
     await Promise.all([
       post.relateTo(author, 'author'),
+      post.relateTo(author, 'observes'),
       // Promise.all(categories.map((c) => c.relateTo(post, 'post'))),
       Promise.all(tags.map((t) => t.relateTo(post, 'post'))),
     ])
@@ -210,7 +211,11 @@ Factory.define('comment')
       options.author,
       options.post,
     ])
-    await Promise.all([comment.relateTo(author, 'author'), comment.relateTo(post, 'post')])
+    await Promise.all([
+      comment.relateTo(author, 'author'),
+      comment.relateTo(post, 'post'),
+      post.relateTo(author, 'observes'),
+    ])
     return comment
   })
 
