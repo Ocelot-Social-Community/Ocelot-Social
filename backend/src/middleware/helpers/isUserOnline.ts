ab6fe37c3e1c3c18b19f1d200245860e3b7a154a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-04-10 18:38:51 +0200

    refactor(backend): new chat message notification email (#8357)
    * new chat message notification email
    
    - new mail features name in subject and text
    
    * fix english version
    
    * typo
    
    * fix typos
    
    * adjust tests

--- backend/src/middleware/helpers/isUserOnline.ts
+++ backend/src/middleware/helpers/isUserOnline.ts
@@ -1,9 +1,9 @@
 export const isUserOnline = (user) => {
   // Is Recipient considered online
-  const lastActive = new Date(user.properties.lastActiveAt).getTime()
-  const awaySince = new Date(user.properties.awaySince).getTime()
+  const lastActive = new Date(user.lastActiveAt).getTime()
+  const awaySince = new Date(user.awaySince).getTime()
   const now = new Date().getTime()
-  const status = user.properties.lastOnlineStatus
+  const status = user.lastOnlineStatus
   if (
     // online & last active less than 1.5min -> online
     (status === 'online' && now - lastActive < 90000) ||
