2362404d032217dde2db04e440ddae14c8e597fc
Maximilian Harz <maxharz@gmail.com>
2025-04-14 12:27:48 +0200

    Add fail-safety

--- webapp/pages/settings/scroll-to-content.js
+++ webapp/pages/settings/scroll-to-content.js
@@ -1,9 +1,13 @@
 export default {
   mounted() {
-    if (document.documentElement.clientWidth < 600) {
-      document.getElementById('settings-content').scrollIntoView({
-        behavior: 'smooth',
-      })
-    }
+    if (document.documentElement.clientWidth >= 600) return
+
+    const content = document.getElementById('settings-content')
+
+    if (!content) return
+
+    content.scrollIntoView({
+      behavior: 'smooth',
+    })
   },
 }
