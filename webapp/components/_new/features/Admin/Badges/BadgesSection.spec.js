f57b67bb5e5ea5709fa82b3969b38ae26d49e4c3
Maximilian Harz <maxharz@gmail.com>
2025-04-17 16:48:41 +0200

    Add BadgeSection test

---
+++ webapp/components/_new/features/Admin/Badges/BadgesSection.spec.js
@@ -0,0 +1,60 @@
+import { render, fireEvent, screen } from '@testing-library/vue'
+import BadgesSection from './BadgesSection.vue'
+
+const localVue = global.localVue
+
+const badge1 = {
+  id: 'badge1',
+  icon: 'icon1',
+  type: 'type1',
+  description: 'description1',
+  isActive: true,
+}
+const badge2 = {
+  id: 'badge2',
+  icon: 'icon2',
+  type: 'type1',
+  description: 'description2',
+  isActive: false,
+}
+
+describe('Admin/BadgesSection', () => {
+  let wrapper
+  let mocks
+
+  beforeEach(() => {
+    mocks = {
+      $t: jest.fn((v) => v),
+      $apollo: {
+        mutate: jest.fn(),
+      },
+      $toast: {
+        success: jest.fn(),
+        error: jest.fn(),
+      },
+    }
+  })
+  const Wrapper = () => {
+    return render(BadgesSection, {
+      mocks,
+      localVue,
+      propsData: {
+        badges: [badge1, badge2],
+      },
+    })
+  }
+
+  beforeEach(() => {
+    wrapper = Wrapper()
+  })
+
+  it('renders', () => {
+    expect(wrapper.baseElement).toMatchSnapshot()
+  })
+
+  it('emits toggleButton', async () => {
+    const button = screen.getByAltText(badge1.description)
+    await fireEvent.click(button)
+    expect(wrapper.emitted().toggleBadge[0][0]).toEqual(badge1)
+  })
+})
