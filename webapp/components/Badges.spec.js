2da6da5f71c552eb379a586dc372ad7fe8472109
Maximilian Harz <maxharz@gmail.com>
2025-04-24 17:54:38 +0200

    add Badges.vue tests

--- webapp/components/Badges.spec.js
+++ webapp/components/Badges.spec.js
@@ -1,29 +1,123 @@
-import { shallowMount } from '@vue/test-utils'
+import { render, screen, fireEvent } from '@testing-library/vue'
 import Badges from './Badges.vue'
 
+const localVue = global.localVue
+
 describe('Badges.vue', () => {
-  let propsData
+  let mocks
 
   beforeEach(() => {
-    propsData = {}
+    mocks = {
+      $t: jest.fn((s) => s),
+    }
   })
 
-  describe('shallowMount', () => {
-    const Wrapper = () => {
-      return shallowMount(Badges, { propsData })
-    }
+  const Wrapper = (propsData) => {
+    return render(Badges, {
+      propsData,
+      mocks,
+      localVue,
+    })
+  }
 
-    it('has class "hc-badges"', () => {
-      expect(Wrapper().find('.hc-badges').exists()).toBe(true)
+  describe('without badges', () => {
+    it('renders in presentation mode', () => {
+      const wrapper = Wrapper({ badges: [], scale: 1.2, selectionMode: false })
+      expect(wrapper.container).toMatchSnapshot()
     })
 
-    describe('given a badge', () => {
+    it('renders in selection mode', () => {
+      const wrapper = Wrapper({ badges: [], scale: 1.2, selectionMode: true })
+      expect(wrapper.container).toMatchSnapshot()
+    })
+  })
+
+  describe('with badges', () => {
+    const badges = [
+      {
+        id: '1',
+        icon: '/path/to/some/icon',
+        isDefault: false,
+        description: 'Some description',
+      },
+      {
+        id: '2',
+        icon: '/path/to/another/icon',
+        isDefault: true,
+        description: 'Another description',
+      },
+      {
+        id: '3',
+        icon: '/path/to/third/icon',
+        isDefault: false,
+        description: 'Third description',
+      },
+    ]
+
+    describe('in presentation mode', () => {
+      let wrapper
+
       beforeEach(() => {
-        propsData.badges = [{ id: '1', icon: '/path/to/some/icon' }]
+        wrapper = Wrapper({ badges, scale: 1.2, selectionMode: false })
       })
 
-      it('proxies badge icon, which is just a URL without metadata', () => {
-        expect(Wrapper().find('img[src="/api/path/to/some/icon"]').exists()).toBe(true)
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      it('clicking on second badge does nothing', async () => {
+        const badge = screen.getByTitle(badges[1].description)
+        await fireEvent.click(badge)
+        expect(wrapper.emitted()).toEqual({})
+      })
+    })
+
+    describe('in selection mode', () => {
+      let wrapper
+
+      beforeEach(() => {
+        wrapper = Wrapper({ badges, scale: 1.2, selectionMode: true })
+      })
+
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      it('clicking on first badge does nothing', async () => {
+        const badge = screen.getByTitle(badges[0].description)
+        await fireEvent.click(badge)
+        expect(wrapper.emitted()).toEqual({})
+      })
+
+      describe('clicking on second badge', () => {
+        beforeEach(async () => {
+          const badge = screen.getByTitle(badges[1].description)
+          await fireEvent.click(badge)
+        })
+
+        it('selects badge', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+
+        it('emits badge-selected with index', async () => {
+          expect(wrapper.emitted()['badge-selected']).toEqual([[1]])
+        })
+      })
+
+      describe('clicking twice on second badge', () => {
+        beforeEach(async () => {
+          const badge = screen.getByTitle(badges[1].description)
+          await fireEvent.click(badge)
+          await fireEvent.click(badge)
+        })
+
+        it('deselects badge', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+
+        it('emits badge-selected with null', async () => {
+          expect(wrapper.emitted()['badge-selected']).toEqual([[1], [null]])
+        })
       })
     })
   })
