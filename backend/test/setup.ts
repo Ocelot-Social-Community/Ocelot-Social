2369d13ca2c702a2786d028466401cba6d285a83
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-04-24 22:39:32 +0200

    lint everything, disable some setup steps for jest (#8423)

--- backend/test/setup.ts
+++ backend/test/setup.ts
@@ -1,8 +1,10 @@
 // Polyfill missing encoders in jsdom
 // https://stackoverflow.com/questions/68468203/why-am-i-getting-textencoder-is-not-defined-in-jest
-import { TextEncoder, TextDecoder } from 'util'
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder as any
+// import { TextEncoder, TextDecoder } from 'util'
+
+// global.TextEncoder = TextEncoder
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+// global.TextDecoder = TextDecoder as any
 
 // Metascraper takes longer nowadays, double time
-jest.setTimeout(10000)
+// jest.setTimeout(10000)
