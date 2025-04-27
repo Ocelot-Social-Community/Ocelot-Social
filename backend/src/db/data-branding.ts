769ef90d43fb124ee4e489768d2b5fd56fc2825f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-04-27 14:08:02 +0200

    lint fixes

--- backend/src/db/data-branding.ts
+++ backend/src/db/data-branding.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { readdir } from 'node:fs/promises'
 import path from 'node:path'
