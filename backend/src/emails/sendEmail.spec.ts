4a0f93493f9f8370428c50272d70d9c19956c8c8
Moriz Wahl <moriz.wahl@gmx.de>
2025-04-30 01:49:34 +0200

    add missing snapshot for post in group

--- backend/src/emails/sendEmail.spec.ts
+++ backend/src/emails/sendEmail.spec.ts
@@ -33,6 +33,31 @@ describe('sendEmail', () => {
       ).resolves.toMatchSnapshot()
     })
 
+    it('post_in_group template', async () => {
+      await expect(
+        sendMail({
+          reason: 'post_in_group',
+          email: 'user@example.org',
+          to: {
+            name: 'Jenny Rostock',
+            id: 'u1',
+            slug: 'jenny-rostock',
+            locale,
+          },
+          from: {
+            id: 'p1',
+            slug: 'new-post',
+            title: 'New Post',
+            author: {
+              id: 'u2',
+              name: 'Peter Lustig',
+              slug: 'peter-lustig',
+            },
+          },
+        }),
+      ).resolves.toMatchSnapshot()
+    })
+
     it('mentioned_in_post template', async () => {
       await expect(
         sendMail({
@@ -222,6 +247,31 @@ describe('sendEmail', () => {
       await expect(
         sendMail({
           reason: 'followed_user_posted',
+          email: 'user@example.org',
+          to: {
+            name: 'Jenny Rostock',
+            id: 'u1',
+            slug: 'jenny-rostock',
+            locale,
+          },
+          from: {
+            id: 'p1',
+            slug: 'new-post',
+            title: 'New Post',
+            author: {
+              id: 'u2',
+              name: 'Peter Lustig',
+              slug: 'peter-lustig',
+            },
+          },
+        }),
+      ).resolves.toMatchSnapshot()
+    })
+
+    it('post_in_group template', async () => {
+      await expect(
+        sendMail({
+          reason: 'post_in_group',
           email: 'user@example.org',
           to: {
             name: 'Jenny Rostock',
