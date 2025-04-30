7ab736a28dc202c6eb662945e7530f21e6d9da0e
Maximilian Harz <maxharz@gmail.com>
2025-04-30 20:12:57 +0200

    Improve tests for UserTeaser, WIP

--- webapp/components/UserTeaser/UserTeaser.spec.js
+++ webapp/components/UserTeaser/UserTeaser.spec.js
@@ -1,113 +1,227 @@
-import { mount, RouterLinkStub } from '@vue/test-utils'
+import { render, screen, fireEvent } from '@testing-library/vue'
+import { RouterLinkStub } from '@vue/test-utils'
 import UserTeaser from './UserTeaser.vue'
 import Vuex from 'vuex'
 
 const localVue = global.localVue
-const filter = jest.fn((str) => str)
 
-localVue.filter('truncate', filter)
+/*
+to test:
+  - user is deleted
+  - user is disabled
+  - user is anonymous
+  - user is a moderator
+  - user is a moderator and the user is deleted
+  - user is a moderator and the user is disabled
+  - linkToProfile on/off
+  - showAvatar on/off
+  - showPopover on/off
+  - hovering name on desktop
+  - clicking name on mobile
+  - hovering avatar on desktop
+  - clicking avatar on mobile
+  - hovering slug on desktop
+  - clicking slug on mobile
+*/
+
+// Mock Math.random, used in Dropdown
+Object.assign(Math, {
+  random: () => 0,
+})
+
+const userTilda = {
+  name: 'Tilda Swinton',
+  slug: 'tilda-swinton',
+  id: 'user1',
+  badgeVerification: {
+    id: 'bv1',
+    icon: '/icons/verified',
+    description: 'Verified',
+    isDefault: false,
+  },
+  badgeTrophiesSelected: [
+    {
+      id: 'trophy1',
+      icon: '/icons/trophy1',
+      description: 'Trophy 1',
+      isDefault: false,
+    },
+    {
+      id: 'trophy2',
+      icon: '/icons/trophy2',
+      description: 'Trophy 2',
+      isDefault: false,
+    },
+    {
+      id: 'empty',
+      icon: '/icons/empty',
+      description: 'Empty',
+      isDefault: true,
+    },
+  ],
+}
 
 describe('UserTeaser', () => {
-  let propsData
-  let mocks
-  let stubs
-  let getters
-
-  beforeEach(() => {
-    propsData = {}
-
-    mocks = {
-      $t: jest.fn(),
-    }
-    stubs = {
-      NuxtLink: RouterLinkStub,
-    }
-    getters = {
-      'auth/user': () => {
-        return {}
+  const Wrapper = ({
+    isModerator = false,
+    withLinkToProfile = true,
+    onTouchScreen = false,
+    user = userTilda,
+  }) => {
+    const mockIsTouchDevice = onTouchScreen
+    jest.mock('../utils/isTouchDevice', () => ({
+      isTouchDevice: jest.fn(() => mockIsTouchDevice),
+    }))
+    const store = new Vuex.Store({
+      getters: {
+        'auth/user': () => {
+          return {}
+        },
+        'auth/isModerator': () => isModerator,
       },
-      'auth/isModerator': () => false,
-    }
+    })
+    return render(UserTeaser, {
+      localVue,
+      store,
+      propsData: {
+        user,
+        linkToProfile: withLinkToProfile,
+      },
+      stubs: {
+        NuxtLink: RouterLinkStub,
+        'user-teaser-popover': true,
+        'v-popover': true,
+        'client-only': true,
+      },
+      mocks: {
+        $t: jest.fn((t) => t),
+      },
+    })
+  }
+
+  it('renders anonymous user', () => {
+    const wrapper = Wrapper({ user: null })
+    expect(wrapper.container).toMatchSnapshot()
   })
 
-  describe('mount', () => {
-    const Wrapper = () => {
-      const store = new Vuex.Store({
-        getters,
+  describe('given an user', () => {
+    describe('without linkToProfile, on touch screen', () => {
+      let wrapper
+      beforeEach(() => {
+        wrapper = Wrapper({ withLinkToProfile: false, onTouchScreen: true })
       })
-      return mount(UserTeaser, { store, propsData, mocks, stubs, localVue })
-    }
 
-    it('renders anonymous user', () => {
-      const wrapper = Wrapper()
-      expect(wrapper.text()).toBe('')
-      expect(mocks.$t).toHaveBeenCalledWith('profile.userAnonym')
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      describe('when clicking the user name', () => {
+        beforeEach(async () => {
+          const userName = screen.getByText('Tilda Swinton')
+          await fireEvent.click(userName)
+        })
+
+        it('renders the popover', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+      })
     })
 
-    describe('given an user', () => {
+    describe('with linkToProfile, on touch screen', () => {
+      let wrapper
       beforeEach(() => {
-        propsData.user = {
-          name: 'Tilda Swinton',
-          slug: 'tilda-swinton',
-        }
+        wrapper = Wrapper({ withLinkToProfile: true, onTouchScreen: true })
       })
 
-      it('renders user name', () => {
-        const wrapper = Wrapper()
-        expect(mocks.$t).not.toHaveBeenCalledWith('profile.userAnonym')
-        expect(wrapper.text()).toMatch('Tilda Swinton')
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
       })
 
-      describe('user is deleted', () => {
-        beforeEach(() => {
-          propsData.user.deleted = true
+      describe('when clicking the user name', () => {
+        beforeEach(async () => {
+          const userName = screen.getByText('Tilda Swinton')
+          await fireEvent.click(userName)
         })
 
+        it('renders the popover', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+      })
+    })
+
+    describe('without linkToProfile, on desktop', () => {
+      let wrapper
+      beforeEach(() => {
+        wrapper = Wrapper({ withLinkToProfile: false, onTouchScreen: false })
+      })
+
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      describe('when hovering the user name', () => {
+        beforeEach(async () => {
+          const userName = screen.getByText('Tilda Swinton')
+          await fireEvent.mouseOver(userName)
+          await new Promise((resolve) => setTimeout(resolve, 2000))
+        })
+
+        it('renders the popover', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+      })
+    })
+
+    describe('with linkToProfile, on desktop', () => {
+      let wrapper
+      beforeEach(() => {
+        wrapper = Wrapper({ withLinkToProfile: true, onTouchScreen: false })
+      })
+
+      it('renders', () => {
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      describe('when hovering the user name', () => {
+        beforeEach(async () => {
+          const userName = screen.getByText('Tilda Swinton')
+          await fireEvent.mouseOver(userName)
+          await new Promise((resolve) => setTimeout(resolve, 2000))
+        })
+
+        it('renders the popover', () => {
+          expect(wrapper.container).toMatchSnapshot()
+        })
+      })
+    })
+
+    describe('user is deleted', () => {
+      it('renders anonymous user', () => {
+        const wrapper = Wrapper({ user: { ...userTilda, deleted: true } })
+        expect(wrapper.container).toMatchSnapshot()
+      })
+
+      describe('even if the current user is a moderator', () => {
         it('renders anonymous user', () => {
-          const wrapper = Wrapper()
-          expect(wrapper.text()).not.toMatch('Tilda Swinton')
-          expect(mocks.$t).toHaveBeenCalledWith('profile.userAnonym')
-        })
-
-        describe('even if the current user is a moderator', () => {
-          beforeEach(() => {
-            getters['auth/isModerator'] = () => true
+          const wrapper = Wrapper({
+            user: { ...userTilda, deleted: true },
+            isModerator: true,
           })
-
-          it('renders anonymous user', () => {
-            const wrapper = Wrapper()
-            expect(wrapper.text()).not.toMatch('Tilda Swinton')
-            expect(mocks.$t).toHaveBeenCalledWith('profile.userAnonym')
-          })
+          expect(wrapper.container).toMatchSnapshot()
         })
       })
+    })
 
-      describe('user is disabled', () => {
-        beforeEach(() => {
-          propsData.user.disabled = true
-        })
+    describe('user is disabled', () => {
+      it('renders anonymous user', () => {
+        const wrapper = Wrapper({ user: { ...userTilda, disabled: true } })
+        expect(wrapper.container).toMatchSnapshot()
+      })
 
-        it('renders anonymous user', () => {
-          const wrapper = Wrapper()
-          expect(wrapper.text()).not.toMatch('Tilda Swinton')
-          expect(mocks.$t).toHaveBeenCalledWith('profile.userAnonym')
-        })
-
-        describe('current user is a moderator', () => {
-          beforeEach(() => {
-            getters['auth/isModerator'] = () => true
-          })
-
-          it('renders user name', () => {
-            const wrapper = Wrapper()
-            expect(wrapper.text()).not.toMatch('Anonymous')
-            expect(wrapper.text()).toMatch('Tilda Swinton')
-          })
-
-          it('has "disabled-content" class', () => {
-            const wrapper = Wrapper()
-            expect(wrapper.classes()).toContain('disabled-content')
-          })
+      describe('current user is a moderator', () => {
+        it('renders user name', () => {
+          const wrapper = Wrapper({ user: { ...userTilda, disabled: true }, isModerator: true })
+          expect(wrapper.container).toMatchSnapshot()
         })
       })
     })
