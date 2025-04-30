33965ffb8f3a33690cd81572f737c01e817858a2
Maximilian Harz <maxharz@gmail.com>
2025-04-30 15:09:33 +0200

    Refactor location info into own component

---
+++ webapp/components/UserTeaser/LocationInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="location-info">
+    <div class="location">
+      <base-icon name="map-marker" />
+      {{ location.name }}
+    </div>
+    <div v-if="distance" class="distance">{{ distance }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LocationInfo',
+  props: {
+    location: { type: Object, default: null },
+  },
+  computed: {
+    distance() {
+      if (this.location /* .distance, when backend is ready */) {
+        return this.$t('location.distance', {
+          distance: this.location.distance ?? 5000 /* todo remove when backend is ready */,
+        })
+      }
+      return null
+    },
+  },
+}
+</script>
+
+<style scoped>
+.location-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .location {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .distance {
+    margin-top: 8px;
+  }
+}
+</style>
--- webapp/components/UserTeaser/UserTeaserPopover.vue
+++ webapp/components/UserTeaser/UserTeaserPopover.vue
@@ -4,13 +4,7 @@
       v-if="$env.BADGES_ENABLED"
       :badges="[user.badgeVerification, ...user.badgeTrophiesSelected]"
     />
-    <div class="location-info">
-      <div class="location" v-if="user.location">
-        <base-icon name="map-marker" />
-        {{ user.location.name }}
-      </div>
-      <div v-if="distanceToMe" class="distance">{{ distanceToMe }}</div>
-    </div>
+    <location-info v-if="user.location" :location="user.location" class="location-info" />
     <ul class="statistics">
       <li>
         <ds-number :count="user.followedByCount" :label="$t('profile.followers')" />
@@ -36,12 +30,14 @@
 
 <script>
 import Badges from '~/components/Badges.vue'
+import LocationInfo from '~/components/UserTeaser/LocationInfo.vue'
 import { isTouchDevice } from '~/components/utils/isTouchDevice'
 
 export default {
   name: 'UserTeaserPopover',
   components: {
     Badges,
+    LocationInfo,
   },
   props: {
     user: { type: Object, default: null },
@@ -50,14 +46,6 @@ export default {
   computed: {
     isTouchDevice() {
       return isTouchDevice()
-    },
-    distanceToMe() {
-      if (this.user.location /* && this.user.location.distanceToMe */) {
-        return this.$t('location.distance', {
-          distance: this.user.location.distanceToMe ?? 5000 /* todo remove when backend is ready */,
-        })
-      }
-      return null
     },
   },
 }
@@ -69,25 +57,12 @@ export default {
   flex-direction: column;
   align-items: center;
   padding: 16px;
+  gap: 16px;
   min-width: 200px;
 }
 
 .location-info {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-block: 16px;
-
-  .location {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .distance {
-    margin-top: 8px;
-  }
+  margin-bottom: 12px;
 }
 
 .link {
