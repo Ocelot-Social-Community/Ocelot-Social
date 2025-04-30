845131d3996d5203e040960adf203da532d38ec9
Moriz Wahl <moriz.wahl@gmx.de>
2025-04-30 08:06:37 +0200

    user db properties works for seed and real data

--- backend/src/db/types/User.ts
+++ backend/src/db/types/User.ts
@@ -2,20 +2,23 @@ import { Integer, Node } from 'neo4j-driver'
 
 export interface UserDbProperties {
   allowEmbedIframes: boolean
+  awaySince?: string
   createdAt: string
   deleted: boolean
   disabled: boolean
-  emailNotificationsChatMessage: boolean
-  emailNotificationsCommentOnObservedPost: boolean
-  emailNotificationsFollowingUsers: boolean
-  emailNotificationsGroupMemberJoined: boolean
-  emailNotificationsGroupMemberLeft: boolean
-  emailNotificationsGroupMemberRemoved: boolean
-  emailNotificationsGroupMemberRoleChanged: boolean
-  emailNotificationsMention: boolean
-  emailNotificationsPostInGroup: boolean
+  emailNotificationsChatMessage?: boolean
+  emailNotificationsCommentOnObservedPost?: boolean
+  emailNotificationsFollowingUsers?: boolean
+  emailNotificationsGroupMemberJoined?: boolean
+  emailNotificationsGroupMemberLeft?: boolean
+  emailNotificationsGroupMemberRemoved?: boolean
+  emailNotificationsGroupMemberRoleChanged?: boolean
+  emailNotificationsMention?: boolean
+  emailNotificationsPostInGroup?: boolean
   encryptedPassword: string
   id: string
+  lastActiveAt?: string
+  lastOnlineStatus?: string
   locale: string
   name: string
   role: string
