e7f7b55347393ac2198436e3d8798bbc70426e40
mahula <lenzmath@posteo.de>
2025-05-04 14:43:14 +0200

    cypress: reintroduce accidentally removed semicolons

--- cypress/support/step_definitions/Post.Comment/I_type_in_a_comment_with_{int}_characters.js
+++ cypress/support/step_definitions/Post.Comment/I_type_in_a_comment_with_{int}_characters.js
@@ -1,7 +1,7 @@
 
 defineStep('I type in a comment with {int} characters', size => {
   var c = ''
-  for (var i = 0 i < size i++) {
+  for (var i = 0; i < size; i++) {
     c += 'c'
   }
   cy.get('.editor .ProseMirror').type(c)
