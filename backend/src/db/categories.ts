9a76a075883905641260baf0d09843be48fe82e6
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-07 23:42:56 +0200

    define ids and slugs in categories, check for existing ids, only seed the new ids

--- backend/src/db/categories.ts
+++ backend/src/db/categories.ts
@@ -1,27 +1,52 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 
 import { categories } from '@constants/categories'
 
 import { getDriver } from './neo4j'
 
+interface Category {
+  icon: string
+  id: string
+  slug: string
+  name: string
+}
+
 const createCategories = async () => {
   const driver = getDriver()
   const session = driver.session()
+  let existingCategories: Category[] = []
+  const existingCategoriesTxResultPromise = session.readTransaction(async (txc) => {
+    const transactionResponse = await txc.run(`
+      MATCH (category:Category)
+      RETURN category { .* }
+    `)
+    return transactionResponse.records.map((record) => record.get('category'))
+  })
+  try {
+    const result = await existingCategoriesTxResultPromise
+    existingCategories = existingCategories.concat(result)
+    // eslint-disable-next-line no-catch-all/no-catch-all
+  } catch (error) {
+    console.log(`Error creating categories: ${error}`) // eslint-disable-line no-console
+  }
+  const existingCategoryIds = existingCategories.map((c) => c.id)
   const createCategoriesTxResultPromise = session.writeTransaction(async (txc) => {
-    categories.forEach(({ icon, name }, index) => {
-      const id = `cat${index + 1}`
-      txc.run(
-        `MERGE (c:Category {
+    categories
+      .filter((c) => !existingCategoryIds.includes(c.id))
+      .forEach(({ icon, name, slug, id }) => {
+        txc.run(
+          `CREATE (c:Category {
           icon: "${icon}",
-          slug: "${name}",
+          slug: "${slug}",
           name: "${name}",
           id: "${id}",
           createdAt: toString(datetime())
         })`,
-      )
-    })
+        )
+      })
   })
   try {
     await createCategoriesTxResultPromise
