265c9493cd16d5c1d717ff07e67221ea97001d69
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-08 21:56:49 +0200

    linting

--- backend/src/db/categories.ts
+++ backend/src/db/categories.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { categories } from '@constants/categories'
 import databaseContext from '@context/database'
 
