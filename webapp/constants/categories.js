41eeb229c6149b0a7a300dac7e2148691b7a2a81
Wolfgang Huß <wolle.huss@pjannto.com>
2025-05-09 20:42:07 +0200

    Brand 'categories.*' files

--- webapp/constants/categories.js
+++ webapp/constants/categories.js
@@ -1,3 +1,90 @@
-// this file is duplicated in `backend/src/constants/metadata.js` and `webapp/constants/metadata.js`
+// this file is duplicated in `backend/src/constants/metadata` and `webapp/constants/metadata.js`
 export const CATEGORIES_MIN = 1
 export const CATEGORIES_MAX = 3
+
+export const categories = [
+  {
+    id: 'corona',
+    slug: 'corona',
+    name: 'corona',
+    icon: '/static/img/categories/corona.svg',
+  },
+  {
+    id: 'digital_euro',
+    slug: 'digital_euro',
+    name: 'digital_euro',
+    icon: '/static/img/categories/digital_euro.svg',
+  },
+  {
+    id: 'who',
+    slug: 'who',
+    name: 'who',
+    icon: '/static/img/categories/who.svg',
+  },
+  {
+    id: 'monetary_system',
+    slug: 'monetary_system',
+    name: 'monetary_system',
+    icon: '/static/img/categories/monetary_system.svg',
+  },
+  {
+    id: 'basic_rights',
+    slug: 'basic_rights',
+    name: 'basic_rights',
+    icon: '/static/img/categories/basic_rights.svg',
+  },
+  {
+    id: 'justice',
+    slug: 'justice',
+    name: 'justice',
+    icon: '/static/img/categories/justice.svg',
+  },
+  {
+    id: 'demonstrations',
+    slug: 'demonstrations',
+    name: 'demonstrations',
+    icon: '/static/img/categories/demonstrations.svg',
+  },
+  {
+    id: 'child_protection',
+    slug: 'child_protection',
+    name: 'child_protection',
+    icon: '/static/img/categories/child_protection.svg',
+  },
+  {
+    id: 'transhumanism',
+    slug: 'transhumanism',
+    name: 'transhumanism',
+    icon: '/static/img/categories/transhumanism.svg',
+  },
+  {
+    id: 'surveillance',
+    slug: 'surveillance',
+    name: 'surveillance',
+    icon: '/static/img/categories/surveillance.svg',
+  },
+  {
+    id: 'education',
+    slug: 'education',
+    name: 'education',
+    icon: '/static/img/categories/education.svg',
+  },
+  {
+    id: 'media',
+    slug: 'media',
+    name: 'media',
+    icon: '/static/img/categories/media.svg',
+  },
+  {
+    id: 'ngos',
+    slug: 'ngos',
+    name: 'ngos',
+    icon: '/static/img/categories/ngos.svg',
+  },
+  {
+    id: 'miscellaneous',
+    slug: 'miscellaneous',
+    name: 'miscellaneous',
+    icon: '/static/img/categories/miscellaneous.svg',
+  },
+]
