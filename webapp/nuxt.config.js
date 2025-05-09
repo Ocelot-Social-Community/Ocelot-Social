abc393fd74c357f238a3ccc7f258c55ffb13eabf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-05-09 03:24:37 +0200

    use build args

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -302,7 +302,6 @@ export default {
         exclude: /(node_modules)/,
       })
 
-      /*
       const tagAttributesForTesting = ['data-test', ':data-test', 'v-bind:data-test']
       ctx.loaders.vue.compilerOptions = {
         modules: [
@@ -322,7 +321,7 @@ export default {
             },
           },
         ],
-      } */
+      }
     },
   },
 }
