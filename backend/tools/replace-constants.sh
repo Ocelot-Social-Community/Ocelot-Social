ba9da00a7f6dc64b297500257d66d7c60ead1ad3
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-10 07:08:39 +0200

    correct copy path in dockerfile  (#8519)

--- backend/tools/replace-constants.sh
+++ backend/tools/replace-constants.sh
@@ -4,4 +4,5 @@
 [ -f src/config/tmp/emails.js ] && mv src/config/tmp/emails.js src/config/emails.ts
 [ -f src/config/tmp/logos.js ] && mv src/config/tmp/logos.js src/config/logos.ts
 [ -f src/config/tmp/metadata.js ] && mv src/config/tmp/metadata.js src/config/metadata.ts
+[ -f src/constants/categories.js ] && mv src/constants/categories.js src/constants/categories.ts
 exit 0
