c0b389a34ca5359fc60bfa52d1d04469141dd8cb
Robert Schäfer <git@roschaefer.de>
2025-05-15 00:30:12 +0800

    improve db migration
    e.g. let the migration crash if some files cannot be found on disk

--- backend/src/db/migrations/20250502230521-migrate-to-s3.ts
+++ backend/src/db/migrations/20250502230521-migrate-to-s3.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 
-import { existsSync, createReadStream } from 'node:fs'
+import { open } from 'node:fs/promises'
 import path from 'node:path'
 
 import { S3Client, ObjectCannedACL } from '@aws-sdk/client-s3'
@@ -13,10 +13,11 @@ import { getDriver } from '@db/neo4j'
 export const description =
   'Upload all image files to a S3 compatible object storage in order to reduce load on our backend.'
 
-export async function up() {
+export async function up(_next) {
   if (CONFIG.NODE_ENV === 'test') {
     // Let's skip this migration for simplicity.
     // There is nothing to migrate in test environment and setting up the S3 services seems overkill.
+    return
   }
   const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_ENDPOINT, AWS_BUCKET } = CONFIG
   if (!(AWS_ACCESS_KEY_ID && AWS_SECRET_ACCESS_KEY && AWS_ENDPOINT && AWS_BUCKET)) {
@@ -39,44 +40,40 @@ export async function up() {
     const { records } = await transaction.run('MATCH (image:Image) RETURN image.url as url')
     let urls: string[] = records.map((r) => r.get('url') as string)
     urls = urls.filter((url) => url.startsWith('/uploads'))
-    const locations = await Promise.all(
+    // eslint-disable-next-line no-console
+    console.log('URLS uploaded:')
+    await Promise.all(
       urls
-        .map((url) => {
-          return async () => {
-            const { pathname } = new URL(url, 'http://example.org')
-            const fileLocation = path.join(__dirname, `../../../public/${pathname}`)
-            const s3Location = `original${pathname}`
-            // eslint-disable-next-line n/no-sync, security/detect-non-literal-fs-filename
-            if (existsSync(fileLocation)) {
-              const mimeType = lookup(fileLocation)
-              const params = {
-                Bucket: AWS_BUCKET,
-                Key: s3Location,
-                ACL: ObjectCannedACL.public_read,
-                ContentType: mimeType || 'image/jpeg',
-                // eslint-disable-next-line security/detect-non-literal-fs-filename
-                Body: createReadStream(fileLocation),
-              }
-
-              const command = new Upload({ client: s3, params })
-              const data = await command.done()
-              const { Location: spacesUrl } = data
-
-              const updatedRecord = await transaction.run(
-                'MATCH (image:Image {url: $url}) SET image.url = $spacesUrl RETURN image.url as url',
-                { url, spacesUrl },
-              )
-              const [updatedUrl] = updatedRecord.records.map(
-                (record) => record.get('url') as string,
-              )
-              return updatedUrl
-            }
+        .map((url) => async () => {
+          const { pathname } = new URL(url, 'http://example.org')
+          const fileLocation = path.join(__dirname, `../../../public/${pathname}`)
+          const s3Location = `original${pathname}`
+          const mimeType = lookup(fileLocation)
+          // eslint-disable-next-line security/detect-non-literal-fs-filename
+          const fileHandle = await open(fileLocation)
+          const params = {
+            Bucket: AWS_BUCKET,
+            Key: s3Location,
+            ACL: ObjectCannedACL.public_read,
+            ContentType: mimeType || 'image/jpeg',
+            Body: fileHandle.createReadStream(),
           }
+          const command = new Upload({ client: s3, params })
+
+          const data = await command.done()
+          const { Location: spacesUrl } = data
+
+          const updatedRecord = await transaction.run(
+            'MATCH (image:Image {url: $url}) SET image.url = $spacesUrl RETURN image.url as url',
+            { url, spacesUrl },
+          )
+          const [updatedUrl] = updatedRecord.records.map((record) => record.get('url') as string)
+          // eslint-disable-next-line no-console
+          console.log(updatedUrl)
+          return updatedUrl
         })
         .map((p) => p()),
     )
-    // eslint-disable-next-line no-console
-    console.log('these are the locations', locations)
     await transaction.commit()
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -90,7 +87,7 @@ export async function up() {
   }
 }
 
-export async function down() {
+export async function down(_next) {
   const driver = getDriver()
   const session = driver.session()
   const transaction = session.beginTransaction()
