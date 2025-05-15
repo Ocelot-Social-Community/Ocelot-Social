7ff5334331aacf5aac413b76a081b3f4d98af574
Robert Schäfer <git@roschaefer.de>
2025-05-15 17:16:44 +0800

    DRY sanitizeRelationshipTypes

---
+++ backend/src/graphql/resolvers/images/sanitizeRelationshipTypes.ts
@@ -0,0 +1,7 @@
+export const sanitizeRelationshipType = (relationshipType: string) => {
+  // Cypher query language does not allow to parameterize relationship types
+  // See: https://github.com/neo4j/neo4j/issues/340
+  if (!['HERO_IMAGE', 'AVATAR_IMAGE'].includes(relationshipType)) {
+    throw new Error(`Unknown relationship type ${relationshipType}`)
+  }
+}
