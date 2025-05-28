ba2e086b4b5799713835773ca1e849cf3c3d08b7
Moriz Wahl <moriz.wahl@gmx.de>
2025-05-28 20:41:10 +0200

    feat(webapp): ask for real name (#8605)
    * feat(webapp): ask for real name
    
    * fix locales
    
    * function to create name from given and surname
    
    * mock env
    
    * use x2004 to create name from given and surname

--- webapp/components/Registration/RegistrationSlideCreate.vue
+++ webapp/components/Registration/RegistrationSlideCreate.vue
@@ -35,7 +35,25 @@
       <!-- leave this here in case the scoped variable is needed in the future nobody would remember this -->
       <!-- <template v-slot="{ errors }"> -->
       <template>
+        <div v-if="askForRealName" class="full-name">
+          <!-- <p>{{ $t('settings.data.realNamePlease') }}</p>-->
+          <ds-input
+            id="givenName"
+            model="givenName"
+            icon="user"
+            :label="$t('settings.data.givenName')"
+            :placeholder="$t('settings.data.givenNamePlaceholder')"
+          />
+          <ds-input
+            id="surName"
+            model="surName"
+            icon="user"
+            :label="$t('settings.data.surName')"
+            :placeholder="$t('settings.data.surNamePlaceholder')"
+          />
+        </div>
         <ds-input
+          v-else
           id="name"
           model="name"
           icon="user"
@@ -102,13 +120,13 @@
           <input
             id="checkbox1"
             type="checkbox"
-            v-model="recieveCommunicationAsEmailsEtcConfirmed"
-            :checked="recieveCommunicationAsEmailsEtcConfirmed"
+            v-model="receiveCommunicationAsEmailsEtcConfirmed"
+            :checked="receiveCommunicationAsEmailsEtcConfirmed"
           />
           <label for="checkbox1">
             {{
               $t(
-                'components.registration.create-user-account.recieveCommunicationAsEmailsEtcConfirmed',
+                'components.registration.create-user-account.receiveCommunicationAsEmailsEtcConfirmed',
               )
             }}
           </label>
@@ -131,6 +149,8 @@ import PageParamsLink from '~/components/_new/features/PageParamsLink/PageParams
 import PasswordForm from '~/components/utils/PasswordFormHelper'
 import ShowPassword from '../ShowPassword/ShowPassword.vue'
 
+const threePerEmSpace = ' ' // unicode u+2004;
+
 export default {
   name: 'RegistrationSlideCreate',
   components: {
@@ -149,13 +169,25 @@ export default {
       links,
       supportEmail: emails.SUPPORT_EMAIL,
       formData: {
+        givenName: '',
+        surName: '',
         name: '',
         ...passwordForm.formData,
       },
       formSchema: {
+        givenName: {
+          type: 'string',
+          required: this.$env.ASK_FOR_REAL_NAME,
+          min: 2,
+        },
+        surName: {
+          type: 'string',
+          required: this.$env.ASK_FOR_REAL_NAME,
+          min: 2,
+        },
         name: {
           type: 'string',
-          required: true,
+          required: !this.$env.ASK_FOR_REAL_NAME,
           min: 3,
         },
         ...passwordForm.formSchema,
@@ -165,15 +197,26 @@ export default {
       // Integrate termsAndConditionsConfirmed into `this.formData` once we
       // have checkmarks available.
       termsAndConditionsConfirmed: false,
-      recieveCommunicationAsEmailsEtcConfirmed: false,
+      receiveCommunicationAsEmailsEtcConfirmed: false,
       showPassword: false,
       showPasswordConfirm: false,
     }
   },
   mounted: function () {
-    this.formData.name = this.sliderData.collectedInputData.name
-      ? this.sliderData.collectedInputData.name
-      : ''
+    if (this.askForRealName) {
+      if (this.sliderData.collectedInputData.name) {
+        const split = this.sliderData.collectedInputData.name.split(threePerEmSpace)
+        this.formData.givenName = split[0]
+        this.formData.surName = split[1] || ''
+      } else {
+        this.formData.surName = ''
+        this.formData.givenName = ''
+      }
+    } else {
+      this.formData.name = this.sliderData.collectedInputData.name
+        ? this.sliderData.collectedInputData.name
+        : ''
+    }
     this.formData.password = this.sliderData.collectedInputData.password
       ? this.sliderData.collectedInputData.password
       : ''
@@ -184,9 +227,9 @@ export default {
       .termsAndConditionsConfirmed
       ? this.sliderData.collectedInputData.termsAndConditionsConfirmed
       : false
-    this.recieveCommunicationAsEmailsEtcConfirmed = this.sliderData.collectedInputData
-      .recieveCommunicationAsEmailsEtcConfirmed
-      ? this.sliderData.collectedInputData.recieveCommunicationAsEmailsEtcConfirmed
+    this.receiveCommunicationAsEmailsEtcConfirmed = this.sliderData.collectedInputData
+      .receiveCommunicationAsEmailsEtcConfirmed
+      ? this.sliderData.collectedInputData.receiveCommunicationAsEmailsEtcConfirmed
       : false
     this.sendValidation()
 
@@ -195,13 +238,18 @@ export default {
     })
   },
   computed: {
+    askForRealName() {
+      return this.$env.ASK_FOR_REAL_NAME
+    },
     validInput() {
       return (
-        this.formData.name.length >= 3 &&
+        (this.askForRealName
+          ? this.formData.givenName.length >= 2 && this.formData.surName.length >= 2
+          : this.formData.name.length >= 3) &&
         this.formData.password.length >= 1 &&
         this.formData.password === this.formData.passwordConfirmation &&
         this.termsAndConditionsConfirmed &&
-        this.recieveCommunicationAsEmailsEtcConfirmed
+        this.receiveCommunicationAsEmailsEtcConfirmed
       )
     },
     iconNamePassword() {
@@ -215,14 +263,19 @@ export default {
     termsAndConditionsConfirmed() {
       this.sendValidation()
     },
-    recieveCommunicationAsEmailsEtcConfirmed() {
+    receiveCommunicationAsEmailsEtcConfirmed() {
       this.sendValidation()
     },
   },
   methods: {
+    buildName(data) {
+      if (this.askForRealName) return `${data.givenName}${threePerEmSpace}${data.surName}`
+      return data.name
+    },
     sendValidation() {
-      const { name, password, passwordConfirmation } = this.formData
-      const { termsAndConditionsConfirmed, recieveCommunicationAsEmailsEtcConfirmed } = this
+      const { password, passwordConfirmation } = this.formData
+      const name = this.buildName(this.formData)
+      const { termsAndConditionsConfirmed, receiveCommunicationAsEmailsEtcConfirmed } = this
 
       this.sliderData.setSliderValuesCallback(this.validInput, {
         collectedInputData: {
@@ -230,7 +283,7 @@ export default {
           password,
           passwordConfirmation,
           termsAndConditionsConfirmed,
-          recieveCommunicationAsEmailsEtcConfirmed,
+          receiveCommunicationAsEmailsEtcConfirmed,
         },
       })
     },
@@ -241,7 +294,8 @@ export default {
       this.sendValidation()
     },
     async submit() {
-      const { name, password } = this.formData
+      const { password } = this.formData
+      const name = this.buildName(this.formData).replace(threePerEmSpace, ' ')
       const { email, inviteCode = null, nonce } = this.sliderData.collectedInputData
       const termsAndConditionsAgreedVersion = VERSION
       const locale = this.$i18n.locale()
@@ -353,5 +407,9 @@ export default {
     margin-left: 0;
     width: 100%;
   }
+}
+
+.full-name {
+  padding-bottom: 16px;
 }
 </style>
