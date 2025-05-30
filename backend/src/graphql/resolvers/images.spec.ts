3f27c7e8a9d4eb9a7707a8954a4fd272ec45ad13
Robert Schäfer <git@roschaefer.de>
2025-05-31 00:10:08 +0800

    ugly fix to keep the path segment on kubernetes
    TODO: refactor me

--- backend/src/graphql/resolvers/images.spec.ts
+++ backend/src/graphql/resolvers/images.spec.ts
@@ -7,7 +7,7 @@ describe('Image', () => {
   const args = {}
   const Location =
     'https://fsn1.your-objectstorage.com/ocelot-social-staging/original/f965ea15-1f6b-43aa-a535-927410e2585e-dsc02586.jpg'
-  const config = {
+  const defaultConfig = {
     ...TEST_CONFIG,
     AWS_ENDPOINT: 'https://fsn1.your-objectstorage.com',
     S3_PUBLIC_GATEWAY: 'https://your-public-gateway.com',
@@ -15,15 +15,30 @@ describe('Image', () => {
   }
 
   describe('.url', () => {
+    const config = { ...defaultConfig }
     it('replaces the internal domain of the S3 `Location` with a public domain', () => {
       const expectedUrl =
         'https://your-public-gateway.com/f_qz7PlAWIQx-IrMOZfikzDFM6I=/ocelot-social-staging/original/f965ea15-1f6b-43aa-a535-927410e2585e-dsc02586.jpg'
       expect(Image.url({ url: Location }, args, { config })).toEqual(expectedUrl)
     })
+
+    describe('given `S3_PUBLIC_GATEWAY` has a path segment', () => {
+      const config = {
+        ...defaultConfig,
+        S3_PUBLIC_GATEWAY: 'https://your-public-gateway.com/imagor',
+      }
+
+      it('keeps the path segment', () => {
+        const expectedUrl =
+          'https://your-public-gateway.com/imagor/f_qz7PlAWIQx-IrMOZfikzDFM6I=/ocelot-social-staging/original/f965ea15-1f6b-43aa-a535-927410e2585e-dsc02586.jpg'
+        expect(Image.url({ url: Location }, args, { config })).toEqual(expectedUrl)
+      })
+    })
   })
 
   describe('.transform', () => {
     describe('resize transformations', () => {
+      const config = { ...defaultConfig }
       const args = { width: 320 }
 
       it('encodes `fit-in` imagor transformations in the URL', () => {
