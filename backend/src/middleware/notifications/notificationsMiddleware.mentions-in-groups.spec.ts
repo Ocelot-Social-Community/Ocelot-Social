f4a77b82fda97b18a142d3242ebb5a4630122e4a
Robert Schäfer <git@roschaefer.de>
2025-06-04 02:04:27 +0800

    delete whitespaces

--- backend/src/middleware/notifications/notificationsMiddleware.mentions-in-groups.spec.ts
+++ backend/src/middleware/notifications/notificationsMiddleware.mentions-in-groups.spec.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import gql from 'graphql-tag'
