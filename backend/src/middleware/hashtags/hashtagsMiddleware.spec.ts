81e9d6def4b6e8bc7af8dce7d9aeba630fae44aa
Robert Schäfer <git@roschaefer.de>
2025-06-04 02:04:27 +0800

    delete whitespaces

--- backend/src/middleware/hashtags/hashtagsMiddleware.spec.ts
+++ backend/src/middleware/hashtags/hashtagsMiddleware.spec.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import gql from 'graphql-tag'
 
