3216b50d1aae7e0a39fd88ff74bb265be8b7ad98
Moriz Wahl <moriz.wahl@gmx.de>
2025-06-19 14:59:15 +0200

    feat(backend): logger (#8655)
    * add logger to context
    
    * add apollo logger
    
    * apollo logger plugin must be set on createServer to avoid spam in unit tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 
 import CONFIG from './config'
+import { loggerPlugin } from './plugins/apolloLogger'
 import createServer from './server'
 
-const { server, httpServer } = createServer()
+const { server, httpServer } = createServer({
+  plugins: [loggerPlugin],
+})
 const url = new URL(CONFIG.GRAPHQL_URI)
 httpServer.listen({ port: url.port }, () => {
   /* eslint-disable-next-line no-console */
