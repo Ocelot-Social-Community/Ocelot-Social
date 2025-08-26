f91e02d71b49b60ee0ee4a458c4c1c32363aa003
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-08-26 02:15:05 +0200

    more tests & updated snapshots

--- webapp/components/LocationInfo/LocationInfo.spec.js
+++ webapp/components/LocationInfo/LocationInfo.spec.js
@@ -19,13 +19,27 @@ describe('LocationInfo', () => {
     })
   }
 
-  it('renders with distance', () => {
-    const wrapper = Wrapper({ withDistance: true })
-    expect(wrapper.container).toMatchSnapshot()
+  describe('distance', () => {
+    it('renders with distance', () => {
+      const wrapper = Wrapper({ withDistance: true })
+      expect(wrapper.container).toMatchSnapshot()
+    })
+
+    it('renders without distance', () => {
+      const wrapper = Wrapper({ withDistance: false })
+      expect(wrapper.container).toMatchSnapshot()
+    })
   })
 
-  it('renders without distance', () => {
-    const wrapper = Wrapper({ withDistance: false })
-    expect(wrapper.container).toMatchSnapshot()
+  describe('size', () => {
+    it('renders in base size', () => {
+      const wrapper = Wrapper({ size: 'base' })
+      expect(wrapper.container).toMatchSnapshot()
+    })
+
+    it('renders in small size', () => {
+      const wrapper = Wrapper({ size: 'small' })
+      expect(wrapper.container).toMatchSnapshot()
+    })
   })
 })
