7ad4bf8df31889313a15baae0ad17ad005c6f283
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-09-04 20:48:52 +0200

    remove frontend and all related services, scripts and code

--- frontend/src/pages/index/+title.ts
+++
@@ -1,3 +0,0 @@
-import { META } from '#src/env'
-
-export const title = META.DEFAULT_TITLE
--- frontend/types/SBComp.ts
+++
@@ -1,4 +0,0 @@
-import { ConcreteComponent } from 'vue'
-
-// Storybook Component Type
-export type SBComp = Omit<ConcreteComponent<unknown>, 'props'>
