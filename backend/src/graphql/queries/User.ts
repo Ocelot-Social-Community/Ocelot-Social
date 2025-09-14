fa6ab6bc15e4984c9c243fa15bbe1e3bdf50bb34
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-09-14 20:08:47 +0200

    User with different queries due to permissions

--- backend/src/graphql/queries/User.ts
+++ backend/src/graphql/queries/User.ts
@@ -5,7 +5,58 @@ export const User = gql`
     User(id: $id, name: $name, email: $email) {
       id
       name
-      email
+      badgeTrophiesCount
+      badgeTrophies {
+        id
+      }
+      badgeVerification {
+        id
+        isDefault
+      }
+      badgeTrophiesSelected {
+        id
+        isDefault
+      }
+      followedBy {
+        id
+      }
+      followedByCurrentUser
+      following {
+        name
+        slug
+        about
+        avatar {
+          url
+        }
+        comments {
+          content
+          contentExcerpt
+        }
+        contributions {
+          title
+          slug
+          image {
+            url
+          }
+          content
+          contentExcerpt
+        }
+      }
+      isMuted
+      isBlocked
+      location {
+        distanceToMe
+      }
+      activeCategories
+    }
+  }
+`
+
+export const UserEmailNotificationSettings = gql`
+  query ($id: ID, $name: String, $email: String) {
+    User(id: $id, name: $name, email: $email) {
+      id
+      name
       badgeTrophiesCount
       badgeTrophies {
         id
@@ -54,6 +105,59 @@ export const User = gql`
           name
           value
         }
+      }
+      activeCategories
+    }
+  }
+`
+
+export const UserEmail = gql`
+  query ($id: ID, $name: String, $email: String) {
+    User(id: $id, name: $name, email: $email) {
+      id
+      name
+      email
+      badgeTrophiesCount
+      badgeTrophies {
+        id
+      }
+      badgeVerification {
+        id
+        isDefault
+      }
+      badgeTrophiesSelected {
+        id
+        isDefault
+      }
+      followedBy {
+        id
+      }
+      followedByCurrentUser
+      following {
+        name
+        slug
+        about
+        avatar {
+          url
+        }
+        comments {
+          content
+          contentExcerpt
+        }
+        contributions {
+          title
+          slug
+          image {
+            url
+          }
+          content
+          contentExcerpt
+        }
+      }
+      isMuted
+      isBlocked
+      location {
+        distanceToMe
       }
       activeCategories
     }
