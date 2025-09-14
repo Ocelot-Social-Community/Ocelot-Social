de887c038be271ecc5fd5cfa9e174fbc28c85d55
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-09-15 00:02:21 +0200

    fix userInteractions.spec

--- backend/src/middleware/userInteractions.spec.ts
+++ backend/src/middleware/userInteractions.spec.ts
@@ -30,6 +30,9 @@ beforeAll(async () => {
         driver,
         neode,
         user: authenticatedUser,
+        cypherParams: {
+          currentUserId: authenticatedUser ? authenticatedUser.id : null,
+        },
       }
     },
   })
