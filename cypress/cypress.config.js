0e12a13d7fcc7555f5669e5367413da494a28b64
mahula <lenzmath@posteo.de>
2025-12-13 20:18:57 +0100

    fix(e2e): update cypress.config.js for esbuild bundler
    Migrate from browserify to esbuild following
    @badeball/cypress-cucumber-preprocessor v24.0.0 breaking change.

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,22 +1,44 @@
 const dotenv = require('dotenv')
 const { defineConfig } = require('cypress');
-const browserify = require('@cypress/browserify-preprocessor');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const {
   addCucumberPreprocessorPlugin,
 } = require('@badeball/cypress-cucumber-preprocessor');
 const {
-  preprendTransformerToOptions,
-} = require('@badeball/cypress-cucumber-preprocessor/browserify');
+  createEsbuildPlugin,
+} = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const {
+  NodeModulesPolyfillPlugin,
+} = require('@esbuild-plugins/node-modules-polyfill');
+const {
+  NodeGlobalsPolyfillPlugin,
+} = require('@esbuild-plugins/node-globals-polyfill');
 
 // Test persistent(between commands) store
 const testStore = {}
 
 async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
     'file:preprocessor',
-    browserify(preprendTransformerToOptions(config, browserify.defaultOptions)),
+    createBundler({
+      // NodeGlobalsPolyfillPlugin provides browser-compatible polyfills for Node.js globals (Buffer, process)
+      // NodeModulesPolyfillPlugin provides browser-compatible polyfills for Node.js built-in modules
+      // (fs, path, crypto, etc.) required when importing backend code
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+        createEsbuildPlugin(config),
+      ],
+      define: {
+        global: 'globalThis',
+      },
+    }),
   );
 
   on('task', {
