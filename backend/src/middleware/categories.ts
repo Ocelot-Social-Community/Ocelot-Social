eeaf1e74cfd6bd70278ffb7aed1fa6c2bf3233b1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-01-21 15:21:08 +0100

    use dynamic before/after defintion for middleares as suggested by @roschaefer

--- backend/src/middleware/categories.ts
+++ backend/src/middleware/categories.ts
@@ -1,18 +1,20 @@
+import { GraphQLResolveInfo } from 'graphql'
+
 import type { Context } from '@src/context'
 
 type Resolver = (
   root: unknown,
   args: unknown,
   context: Context,
-  resolveInfo: unknown,
+  resolveInfo: GraphQLResolveInfo,
 ) => Promise<unknown>
-const checkCategoriesActive = (
+const checkCategoriesActive = async (
   resolve: Resolver,
   root: unknown,
   args: unknown,
   context: Context,
-  resolveInfo: unknown,
-) => {
+  resolveInfo: GraphQLResolveInfo,
+): Promise<unknown> => {
   if (context.config.CATEGORIES_ACTIVE) {
     return resolve(root, args, context, resolveInfo)
   }
