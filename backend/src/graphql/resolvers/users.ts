1d3096cc39fdab7fdecdd53afdc41476be4f1884
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-01-23 23:55:53 +0100

    lint fix

--- backend/src/graphql/resolvers/users.ts
+++ backend/src/graphql/resolvers/users.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
