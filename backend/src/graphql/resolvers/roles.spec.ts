fdef3d3da104e73d801f794d2c0611e815cba070
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-03 05:29:51 +0100

    working tests

--- backend/src/graphql/resolvers/roles.spec.ts
+++ backend/src/graphql/resolvers/roles.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ApolloServerTestClient, createTestClient } from 'apollo-server-testing'
@@ -48,19 +49,55 @@ describe('availableRoles', () => {
   })
 
   describe('authenticated', () => {
-    beforeEach(async () => {
-      const currentUser = await Factory.build(
-        'user',
-        { id: 'current-user-id', role: 'user' },
-        { email: 'test@example.org', password: '1234' },
-      )
-      authenticatedUser = await currentUser.toJson()
+    describe('as user', () => {
+      beforeEach(async () => {
+        const user = await Factory.build(
+          'user',
+          { id: 'user-id', role: 'user' },
+          { email: 'user@example.org', password: '1234' },
+        )
+        authenticatedUser = await user.toJson()
+      })
+
+      it('throws authorization error', async () => {
+        const { data, errors } = await query({ query: availableRoles })
+        expect(data).toEqual(null)
+        expect(errors).toEqual([expect.objectContaining({ message: 'Not Authorized!' })])
+      })
     })
 
-    it('returns available roles', async () => {
-      const { data, errors } = await query({ query: availableRoles })
-      expect(errors).toBeUndefined()
-      expect(data?.availableRoles).toEqual(['admin', 'moderator', 'user'])
+    describe('as moderator', () => {
+      beforeEach(async () => {
+        const moderator = await Factory.build(
+          'user',
+          { id: 'moderator-id', role: 'moderator' },
+          { email: 'moderator@example.org', password: '1234' },
+        )
+        authenticatedUser = await moderator.toJson()
+      })
+
+      it('throws authorization error', async () => {
+        const { data, errors } = await query({ query: availableRoles })
+        expect(data).toEqual(null)
+        expect(errors).toEqual([expect.objectContaining({ message: 'Not Authorized!' })])
+      })
+    })
+
+    describe('as admin', () => {
+      beforeEach(async () => {
+        const admin = await Factory.build(
+          'user',
+          { id: 'admin-id', role: 'admin' },
+          { email: 'admin@example.org', password: '1234' },
+        )
+        authenticatedUser = await admin.toJson()
+      })
+
+      it('returns available roles', async () => {
+        const { data, errors } = await query({ query: availableRoles })
+        expect(errors).toBeUndefined()
+        expect(data?.availableRoles).toEqual(['admin', 'moderator', 'user'])
+      })
     })
   })
 })
