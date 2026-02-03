753a300c3fdc998b5136f052edfca8ec08823b62
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-03 14:20:19 +0100

    refactor(backend): middleware before/after (#9128)

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { applyMiddleware, IMiddleware } from 'graphql-middleware'
+import { applyMiddleware, IMiddleware, IMiddlewareGenerator } from 'graphql-middleware'
 
 import CONFIG from '@config/index'
 
-// eslint-disable-next-line import/no-cycle
 import brandingMiddlewares from './branding/brandingMiddlewares'
 import categories from './categories'
 import chatMiddleware from './chatMiddleware'
@@ -25,41 +24,79 @@ import validation from './validation/validationMiddleware'
 import xss from './xssMiddleware'
 
 export interface MiddlewareOrder {
-  order: number
+  position: 'prepend' | 'append' | { before: string } | { after: string }
   name: string
-  middleware: IMiddleware
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  middleware: IMiddleware | IMiddlewareGenerator<any, any, any>
 }
 
-const ocelotMiddlewares: MiddlewareOrder[] = [
-  { order: -200, name: 'sentry', middleware: sentry },
-  { order: -190, name: 'permissions', middleware: permissions },
-  { order: -180, name: 'xss', middleware: xss },
-  { order: -170, name: 'validation', middleware: validation },
-  { order: -160, name: 'userInteractions', middleware: userInteractions },
-  { order: -150, name: 'sluggify', middleware: sluggify },
-  { order: -140, name: 'languages', middleware: languages },
-  { order: -130, name: 'excerpt', middleware: excerpt },
-  { order: -120, name: 'login', middleware: login },
-  { order: -110, name: 'notifications', middleware: notifications },
-  { order: -100, name: 'hashtags', middleware: hashtags },
-  { order: -90, name: 'softDelete', middleware: softDelete },
-  { order: -80, name: 'includedFields', middleware: includedFields },
-  { order: -70, name: 'orderBy', middleware: orderBy },
-  { order: -60, name: 'chatMiddleware', middleware: chatMiddleware },
-  { order: -50, name: 'categories', middleware: categories },
-]
+const ocelotMiddlewares: MiddlewareOrder[] = []
+
+export const addMiddleware = (middleware: MiddlewareOrder) => {
+  switch (middleware.position) {
+    case 'append':
+      ocelotMiddlewares.push(middleware)
+      break
+    case 'prepend':
+      ocelotMiddlewares.unshift(middleware)
+      break
+    default: {
+      const anchor =
+        'before' in middleware.position ? middleware.position.before : middleware.position.after
+      const appendMiddlewareAt = ocelotMiddlewares.findIndex((m) => m.name === anchor)
+      if (appendMiddlewareAt === -1) {
+        throw new Error(
+          `Could not find middleware "${anchor}" to append the middleware "${middleware.name}"`,
+        )
+      }
+      ocelotMiddlewares.splice(
+        appendMiddlewareAt + ('before' in middleware.position ? 0 : 1),
+        0,
+        middleware,
+      )
+    }
+  }
+}
+
+addMiddleware({ name: 'sentry', middleware: sentry, position: 'append' })
+addMiddleware({ name: 'permissions', middleware: permissions, position: { after: 'sentry' } })
+addMiddleware({ name: 'xss', middleware: xss, position: { after: 'permissions' } })
+addMiddleware({ name: 'validation', middleware: validation, position: { after: 'xss' } })
+addMiddleware({
+  name: 'userInteractions',
+  middleware: userInteractions,
+  position: { after: 'validation' },
+})
+addMiddleware({ name: 'sluggify', middleware: sluggify, position: { after: 'userInteractions' } })
+addMiddleware({ name: 'languages', middleware: languages, position: { after: 'sluggify' } })
+addMiddleware({ name: 'excerpt', middleware: excerpt, position: { after: 'languages' } })
+addMiddleware({ name: 'login', middleware: login, position: { after: 'excerpt' } })
+addMiddleware({ name: 'notifications', middleware: notifications, position: { after: 'login' } })
+addMiddleware({ name: 'hashtags', middleware: hashtags, position: { after: 'notifications' } })
+addMiddleware({ name: 'softDelete', middleware: softDelete, position: { after: 'hashtags' } })
+addMiddleware({
+  name: 'includedFields',
+  middleware: includedFields,
+  position: { after: 'softDelete' },
+})
+addMiddleware({ name: 'orderBy', middleware: orderBy, position: { after: 'includedFields' } })
+addMiddleware({
+  name: 'chatMiddleware',
+  middleware: chatMiddleware,
+  position: { after: 'orderBy' },
+})
+addMiddleware({ name: 'categories', middleware: categories, position: { after: 'chatMiddleware' } })
 
 export default (schema) => {
-  const middlewares = ocelotMiddlewares
-    .concat(brandingMiddlewares())
-    .sort((a, b) => a.order - b.order)
+  // execute branding middleware function
+  brandingMiddlewares()
 
-  const filteredMiddlewares = middlewares.filter(
+  const filteredMiddlewares = ocelotMiddlewares.filter(
     (middleware) => !CONFIG.DISABLED_MIDDLEWARES.includes(middleware.name),
   )
 
   // Warn if we filtered
-  if (middlewares.length < filteredMiddlewares.length) {
+  if (ocelotMiddlewares.length !== filteredMiddlewares.length) {
     // eslint-disable-next-line no-console
     console.log(`Warning: Disabled "${CONFIG.DISABLED_MIDDLEWARES.join(', ')}" middleware.`)
   }
