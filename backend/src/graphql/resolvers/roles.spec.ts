6370f5eea28a0b2830cd883ef8cbba9371cfd52f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-03 05:24:27 +0100

    fix unauthorized

--- backend/src/graphql/resolvers/roles.spec.ts
+++ backend/src/graphql/resolvers/roles.spec.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { ApolloServerTestClient, createTestClient } from 'apollo-server-testing'
 
-import { cleanDatabase } from '@db/factories'
+import Factory, { cleanDatabase } from '@db/factories'
 import { getDriver, getNeode } from '@db/neo4j'
 import { availableRoles } from '@graphql/queries/availableRoles'
 import createServer from '@src/server'
@@ -11,6 +11,7 @@ const instance = getNeode()
 const driver = getDriver()
 
 describe('availableRoles', () => {
+  let authenticatedUser
   let query: ApolloServerTestClient['query']
 
   beforeAll(async () => {
@@ -21,7 +22,7 @@ describe('availableRoles', () => {
         return {
           driver,
           neode: instance,
-          user: null,
+          user: authenticatedUser,
         }
       },
     })
@@ -33,9 +34,33 @@ describe('availableRoles', () => {
     await driver.close()
   })
 
-  it('returns available roles', async () => {
-    const { data, errors } = await query({ query: availableRoles })
-    expect(errors).toBeUndefined()
-    expect(data?.availableRoles).toEqual(['admin', 'moderator', 'user'])
+  afterEach(async () => {
+    await cleanDatabase()
+  })
+
+  describe('unauthenticated', () => {
+    it('throws authorization error', async () => {
+      authenticatedUser = null
+      const { data, errors } = await query({ query: availableRoles })
+      expect(data).toEqual({ availableRoles: null })
+      expect(errors).toEqual([expect.objectContaining({ message: 'Not Authorized!' })])
+    })
+  })
+
+  describe('authenticated', () => {
+    beforeEach(async () => {
+      const currentUser = await Factory.build(
+        'user',
+        { id: 'current-user-id', role: 'user' },
+        { email: 'test@example.org', password: '1234' },
+      )
+      authenticatedUser = await currentUser.toJson()
+    })
+
+    it('returns available roles', async () => {
+      const { data, errors } = await query({ query: availableRoles })
+      expect(errors).toBeUndefined()
+      expect(data?.availableRoles).toEqual(['admin', 'moderator', 'user'])
+    })
   })
 })
