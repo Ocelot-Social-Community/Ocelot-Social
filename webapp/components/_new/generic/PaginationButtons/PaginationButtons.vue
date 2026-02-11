bf0a3a26a695e9302de39fe5052406e7e4d0b51e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-11 03:58:28 +0100

    PaginationButtons

--- webapp/components/_new/generic/PaginationButtons/PaginationButtons.vue
+++ webapp/components/_new/generic/PaginationButtons/PaginationButtons.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="pagination-buttons">
-    <base-button
+    <os-button
       class="previous-button"
       :disabled="!hasPrevious"
-      icon="arrow-left"
+      appearance="outline"
+      variant="primary"
       circle
       data-test="previous-button"
+      aria-label="Previous"
       @click="$emit('back')"
-    />
+    >
+      <template #icon><base-icon name="arrow-left" /></template>
+    </os-button>
 
     <span v-if="showPageCounter" class="pagination-pageCount" data-test="pagination-pageCount">
       {{ $t('search.page') }} {{ activePage + 1 }} /
       {{ Math.floor((activeResourceCount - 1) / pageSize) + 1 }}
     </span>
 
-    <base-button
+    <os-button
       class="next-button"
       :disabled="!hasNext"
-      icon="arrow-right"
+      appearance="outline"
+      variant="primary"
       circle
       data-test="next-button"
+      aria-label="Next"
       @click="$emit('next')"
-    />
+    >
+      <template #icon><base-icon name="arrow-right" /></template>
+    </os-button>
   </div>
 </template>
 
 <script>
+import { OsButton } from '@ocelot-social/ui'
+
 export default {
+  components: {
+    OsButton,
+  },
   props: {
     pageSize: {
       type: Number,
