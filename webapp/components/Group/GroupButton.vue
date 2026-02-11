2666b9ca0732d4273ad86c1c4757bfa4f03a17bf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-11 20:46:46 +0100

    GroupButton

--- webapp/components/Group/GroupButton.vue
+++ webapp/components/Group/GroupButton.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
     <nuxt-link to="/groups">
-      <base-button
-        icon="users"
+      <os-button
+        variant="primary"
+        appearance="ghost"
         circle
-        ghost
         v-tooltip="{
           content: $t('header.groups.tooltip'),
           placement: 'bottom-start',
         }"
-      />
+      >
+        <template #icon>
+          <base-icon name="users" />
+        </template>
+      </os-button>
     </nuxt-link>
   </div>
 </template>
 
 <script>
-export default {}
+import { OsButton } from '@ocelot-social/ui'
+
+export default {
+  components: { OsButton },
+}
 </script>
