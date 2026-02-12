bd8570a77f5e39357654cdc2e039e59270889907
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-13 00:18:47 +0100

    selector specified

--- cypress/support/step_definitions/User.Block/I_{string}_see_{string}_from_the_content_menu_in_the_user_info_box.js
+++ cypress/support/step_definitions/User.Block/I_{string}_see_{string}_from_the_content_menu_in_the_user_info_box.js
@@ -1,7 +1,7 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I {string} see {string} from the content menu in the user info box', (condition, link) => {
-  cy.get('.user-content-menu button').click()
+  cy.get('.user-content-menu [data-test="content-menu-button"]').click()
   cy.get('.popover .ds-menu-item-link')
     .should(condition === 'should' ? 'contain' : 'not.contain', link)
 })
