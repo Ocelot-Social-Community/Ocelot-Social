bd61e49a3ef2b891de71bd62980b5cb1d55071ed
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-13 12:07:11 +0100

    emotions button aria

--- webapp/components/EmotionButton/EmotionButton.vue
+++ webapp/components/EmotionButton/EmotionButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="emotion-button">
-    <os-button :id="emotion" appearance="ghost" circle @click="$emit('toggleEmotion', emotion)">
+    <os-button :id="emotion" appearance="ghost" circle :aria-label="$t(`contribution.emotions-label.${emotion}`)" @click="$emit('toggleEmotion', emotion)">
       <img class="image" :src="emojiPath" />
     </os-button>
     <label class="label" :for="emotion">{{ $t(`contribution.emotions-label.${emotion}`) }}</label>
