46c2fee5057882da971dc39a9780086a454d054f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-13 12:35:15 +0100

    editor menu aria

--- webapp/components/Editor/MenuBar.vue
+++ webapp/components/Editor/MenuBar.vue
@@ -6,6 +6,7 @@
         circle
         variant="primary"
         :appearance="isActive.bold() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.bold')"
         @click="commands.bold"
       >
         <template #icon>
@@ -18,6 +19,7 @@
         circle
         variant="primary"
         :appearance="isActive.italic() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.italic')"
         @click="commands.italic"
       >
         <template #icon>
@@ -30,6 +32,7 @@
         circle
         variant="primary"
         :appearance="isActive.underline() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.underline')"
         @click="commands.underline"
       >
         <template #icon>
@@ -42,6 +45,7 @@
         circle
         variant="primary"
         :appearance="isActive.link() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.link')"
         @click="(event) => toggleLinkInput(getMarkAttrs('link'), event.target.closest('button'))"
       >
         <template #icon>
@@ -54,6 +58,7 @@
         circle
         variant="primary"
         :appearance="isActive.paragraph() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.paragraph')"
         @click="commands.paragraph"
       >
         <template #icon>
@@ -66,6 +71,7 @@
         circle
         variant="primary"
         :appearance="isActive.heading({ level: 3 }) ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.heading3')"
         @click="() => commands.heading({ level: 3 })"
       >
         H3
@@ -76,6 +82,7 @@
         circle
         variant="primary"
         :appearance="isActive.heading({ level: 4 }) ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.heading4')"
         @click="() => commands.heading({ level: 4 })"
       >
         H4
@@ -86,6 +93,7 @@
         circle
         variant="primary"
         :appearance="isActive.bullet_list() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.unorderedList')"
         @click="commands.bullet_list"
       >
         <template #icon>
@@ -98,6 +106,7 @@
         circle
         variant="primary"
         :appearance="isActive.ordered_list() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.orderedList')"
         @click="commands.ordered_list"
       >
         <template #icon>
@@ -110,6 +119,7 @@
         circle
         variant="primary"
         :appearance="isActive.blockquote() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.quote')"
         @click="commands.blockquote"
       >
         <template #icon>
@@ -122,6 +132,7 @@
         circle
         variant="primary"
         :appearance="isActive.horizontal_rule() ? 'outline' : 'ghost'"
+        :aria-label="$t('editor.legend.ruler')"
         @click="commands.horizontal_rule"
       >
         <template #icon>
