b496ef50bc81ec07e1b7ebadd4db7906f56cfea8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-14 03:13:41 +0100

    fix e2e test

--- cypress/support/step_definitions/Notification.Mention/the_unread_counter_is_removed.js
+++ cypress/support/step_definitions/Notification.Mention/the_unread_counter_is_removed.js
@@ -1,6 +1,6 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('the unread counter is removed', () => {
-  cy.get('.notifications-menu .counter-icon')
+  cy.get('.notifications-menu .counter-icon .count')
     .should('not.exist')
 })
