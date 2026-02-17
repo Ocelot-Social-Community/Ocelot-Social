9f2fccd5b471ec4add4d2a1be9c93240441956a5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-17 22:09:35 +0100

    more linebreaks for svgs

--- packages/ui/src/plugins/vite-svg-icon.ts
+++ packages/ui/src/plugins/vite-svg-icon.ts
@@ -7,7 +7,13 @@ const SUFFIX = '?icon'
 
 /** Escape a string for safe embedding in a single-quoted JS literal */
 function escapeJS(str: string): string {
-  return str.replace(/\\/g, '\\\\').replace(/'/g, "\\'").replace(/\n/g, '\\n').replace(/\r/g, '\\r')
+  return str
+    .replace(/\\/g, '\\\\')
+    .replace(/'/g, "\\'")
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029')
 }
 
 const SUPPORTED_ELEMENTS = ['path', 'circle', 'rect', 'polygon', 'polyline', 'ellipse', 'line']
