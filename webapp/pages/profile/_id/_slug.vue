b49dbc106b3bf05df86921532a608d39cfd79967
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-18 23:28:53 +0100

    fix spinner

--- webapp/pages/profile/_id/_slug.vue
+++ webapp/pages/profile/_id/_slug.vue
@@ -202,7 +202,7 @@
           </template>
         </masonry-grid>
         <client-only>
-          <infinite-loading v-if="hasMore && !$apollo.loading" @infinite="showMoreContributions">
+          <infinite-loading v-if="hasMore" @infinite="showMoreContributions">
             <os-spinner slot="spinner" size="lg" />
           </infinite-loading>
         </client-only>
