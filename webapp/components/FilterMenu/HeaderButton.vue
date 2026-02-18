033d55b933f9fa5c1e8628cc5025fb1c5d0fcd3a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-18 01:11:00 +0100

    implement filter button with suffix

--- webapp/components/FilterMenu/HeaderButton.vue
+++ webapp/components/FilterMenu/HeaderButton.vue
@@ -1,23 +1,28 @@
 <template>
-  <span class="header-button-wrapper">
-    <os-button class="my-filter-button" variant="primary" appearance="filled" @click="clickButton">
-      {{ title }}
-    </os-button>
-    <os-button
-      class="filter-remove"
-      variant="primary"
-      appearance="filled"
-      circle
-      size="sm"
-      :title="titleRemove"
-      :aria-label="titleRemove"
-      @click.stop="clickRemove"
-    >
-      <template #icon>
-        <os-icon :icon="icons.close" />
-      </template>
-    </os-button>
-  </span>
+  <os-button
+    class="my-filter-button"
+    variant="primary"
+    appearance="filled"
+    @click="clickButton"
+  >
+    {{ title }}
+    <template #suffix>
+      <os-button
+        class="filter-remove"
+        variant="primary"
+        appearance="filled"
+        circle
+        size="sm"
+        :title="titleRemove"
+        :aria-label="titleRemove"
+        @click.stop="clickRemove"
+      >
+        <template #icon>
+          <os-icon :icon="icons.close" />
+        </template>
+      </os-button>
+    </template>
+  </os-button>
 </template>
 <script>
 import { OsButton, OsIcon } from '@ocelot-social/ui'
@@ -49,22 +54,3 @@ export default {
   },
 }
 </script>
-<style lang="scss">
-.header-button-wrapper {
-  display: inline-flex;
-  align-items: center;
-  position: relative;
-  margin-right: 8px;
-
-  > .my-filter-button {
-    padding-right: 36px !important;
-  }
-
-  > .filter-remove {
-    position: absolute !important;
-    right: 4px !important;
-    top: 50% !important;
-    transform: translateY(-50%);
-  }
-}
-</style>
