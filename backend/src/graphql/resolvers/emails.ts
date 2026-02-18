37c5a1651b5913a661fae8b024d944d057960b6e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-18 20:40:19 +0100

    cleanup

--- backend/src/graphql/resolvers/emails.ts
+++ backend/src/graphql/resolvers/emails.ts
@@ -17,18 +17,16 @@ export default {
     VerifyNonce: async (_parent, args, context, _resolveInfo) => {
       args.email = normalizeEmail(args.email)
       const session = context.driver.session()
-      const readTxResultPromise = session.readTransaction(async (txc) => {
-        const result = await txc.run(
-          `
-            MATCH (email:EmailAddress {email: $email, nonce: $nonce})
-            RETURN count(email) > 0 AS result
-          `,
-          { email: args.email, nonce: args.nonce },
-        )
-        return result
-      })
       try {
-        const txResult = await readTxResultPromise
+        const txResult = await session.readTransaction(async (txc) => {
+          return await txc.run(
+            `
+              MATCH (email:EmailAddress {email: $email, nonce: $nonce})
+              RETURN count(email) > 0 AS result
+            `,
+            { email: args.email, nonce: args.nonce },
+          )
+        })
         return txResult.records[0].get('result')
       } finally {
         await session.close()
@@ -37,7 +35,6 @@ export default {
   },
   Mutation: {
     AddEmailAddress: async (_parent, args, context, _resolveInfo) => {
-      let response
       args.email = normalizeEmail(args.email)
       try {
         const { neode } = context
@@ -57,68 +54,64 @@ export default {
       } = context
 
       const session = context.driver.session()
-      const writeTxResultPromise = session.writeTransaction(async (txc) => {
-        const result = await txc.run(
-          `
-            MATCH (user:User {id: $userId})
-            MERGE (user)<-[:BELONGS_TO]-(email:UnverifiedEmailAddress {email: $email, nonce: $nonce})
-            SET email.createdAt = toString(datetime())
-            RETURN email, user
-          `,
-          { userId, email: args.email, nonce },
-        )
-        return result.records.map((record) => ({
-          name: record.get('user').properties.name,
-          locale: record.get('user').properties.locale,
-          ...record.get('email').properties,
-        }))
-      })
       try {
-        const txResult = await writeTxResultPromise
-        response = txResult[0]
+        const txResult = await session.writeTransaction(async (txc) => {
+          const result = await txc.run(
+            `
+              MATCH (user:User {id: $userId})
+              MERGE (user)<-[:BELONGS_TO]-(email:UnverifiedEmailAddress {email: $email, nonce: $nonce})
+              SET email.createdAt = toString(datetime())
+              RETURN email, user
+            `,
+            { userId, email: args.email, nonce },
+          )
+          return result.records.map((record) => ({
+            name: record.get('user').properties.name,
+            locale: record.get('user').properties.locale,
+            ...record.get('email').properties,
+          }))
+        })
+        return txResult[0]
       } finally {
         await session.close()
       }
-      return response
     },
     VerifyEmailAddress: async (_parent, args, context, _resolveInfo) => {
-      let response
       const {
         user: { id: userId },
       } = context
       args.email = normalizeEmail(args.email)
       const { nonce, email } = args
       const session = context.driver.session()
-      const writeTxResultPromise = session.writeTransaction(async (txc) => {
-        const result = await txc.run(
-          `
-            MATCH (user:User {id: $userId})-[:PRIMARY_EMAIL]->(previous:EmailAddress)
-            MATCH (user)<-[:BELONGS_TO]-(email:UnverifiedEmailAddress {email: $email, nonce: $nonce})
-            OPTIONAL MATCH (abandonedEmail:EmailAddress{email: $email}) WHERE NOT EXISTS ((abandonedEmail)<-[]-())
-            DELETE abandonedEmail
-            MERGE (user)-[:PRIMARY_EMAIL]->(email)
-            SET email:EmailAddress
-            SET email.verifiedAt = toString(datetime())
-            REMOVE email:UnverifiedEmailAddress
-            DETACH DELETE previous
-            RETURN email
-          `,
-          { userId, email, nonce },
-        )
-        return result.records.map((record) => record.get('email').properties)
-      })
       try {
-        const txResult = await writeTxResultPromise
-        response = txResult[0]
+        const txResult = await session.writeTransaction(async (txc) => {
+          const result = await txc.run(
+            `
+              MATCH (user:User {id: $userId})-[:PRIMARY_EMAIL]->(previous:EmailAddress)
+              MATCH (user)<-[:BELONGS_TO]-(email:UnverifiedEmailAddress {email: $email, nonce: $nonce})
+              OPTIONAL MATCH (abandonedEmail:EmailAddress{email: $email}) WHERE NOT EXISTS ((abandonedEmail)<-[]-())
+              DELETE abandonedEmail
+              MERGE (user)-[:PRIMARY_EMAIL]->(email)
+              SET email:EmailAddress
+              SET email.verifiedAt = toString(datetime())
+              REMOVE email:UnverifiedEmailAddress
+              DETACH DELETE previous
+              RETURN email
+            `,
+            { userId, email, nonce },
+          )
+          return result.records.map((record) => record.get('email').properties)
+        })
+        const response = txResult[0]
+        if (!response) throw new UserInputError('Invalid nonce or no email address found.')
+        return response
       } catch (e) {
         if (e.code === 'Neo.ClientError.Schema.ConstraintValidationFailed')
           throw new UserInputError('A user account with this email already exists.')
-        throw new Error(e)
+        throw e
       } finally {
         await session.close()
       }
-      if (!response) throw new UserInputError('Invalid nonce or no email address found.')
-      return response
     },
   },
   EmailAddress: {
