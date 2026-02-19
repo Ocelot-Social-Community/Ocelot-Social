090023fe53aa2217381a708e440af9a7f48fb4d1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-19 21:15:14 +0100

    resend email

--- webapp/components/Registration/RegistrationSlideEmail.vue
+++ webapp/components/Registration/RegistrationSlideEmail.vue
@@ -12,8 +12,8 @@
     <ds-input :placeholder="$t('login.email')" type="email" id="email" model="email" name="email" />
     <slot></slot>
     <p class="ds-text" v-if="sliderData.collectedInputData.emailSend">
-      <input id="checkbox" type="checkbox" v-model="sendEmailAgain" :checked="sendEmailAgain" />
-      <label for="checkbox0">
+      <input id="sendEmailAgain" type="checkbox" v-model="sendEmailAgain" :checked="sendEmailAgain" />
+      <label for="sendEmailAgain">
         {{ $t('components.registration.email.form.sendEmailAgain') }}
       </label>
     </p>
