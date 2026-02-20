c382dcbfd9d1382bad55509333a417ff9a87e3d7
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-20 19:57:03 +0100

    show empty warning when there is no categories

--- webapp/pages/admin/categories.vue
+++ webapp/pages/admin/categories.vue
@@ -1,29 +1,35 @@
 <template>
-  <os-card>
-    <h2 class="title">{{ $t('admin.categories.name') }}</h2>
-    <div class="ds-table-wrap">
-      <table class="ds-table ds-table-condensed ds-table-bordered">
-        <thead>
-          <tr>
-            <th class="ds-table-head-col" aria-hidden="true"></th>
-            <th scope="col" class="ds-table-head-col">{{ $t('admin.categories.categoryName') }}</th>
-            <th scope="col" class="ds-table-head-col ds-table-head-col-right">
-              {{ $t('admin.categories.postCount') }}
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="category in Category" :key="category.id">
-            <td class="ds-table-col">
-              <os-icon :icon="resolveIcon(category.icon)" />
-            </td>
-            <td class="ds-table-col">{{ category.name }}</td>
-            <td class="ds-table-col ds-table-col-right">{{ category.postCount }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </os-card>
+  <div>
+    <os-card v-if="Category && Category.length">
+      <h2 class="title">{{ $t('admin.categories.name') }}</h2>
+      <div class="ds-table-wrap">
+        <table class="ds-table ds-table-condensed ds-table-bordered">
+          <thead>
+            <tr>
+              <th class="ds-table-head-col" aria-hidden="true"></th>
+              <th scope="col" class="ds-table-head-col">{{ $t('admin.categories.categoryName') }}</th>
+              <th scope="col" class="ds-table-head-col ds-table-head-col-right">
+                {{ $t('admin.categories.postCount') }}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="category in Category" :key="category.id">
+              <td class="ds-table-col">
+                <os-icon :icon="resolveIcon(category.icon)" />
+              </td>
+              <td class="ds-table-col">{{ category.name }}</td>
+              <td class="ds-table-col ds-table-col-right">{{ category.postCount }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </os-card>
+    <os-card v-else>
+      <h2 class="title">{{ $t('admin.categories.name') }}</h2>
+      <div class="ds-placeholder">{{ $t('admin.categories.empty') }}</div>
+    </os-card>
+  </div>
 </template>
 
 <script>
