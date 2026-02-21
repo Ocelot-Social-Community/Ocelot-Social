65367cb91101ee70b3216afe2afffaefa235bcd1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-21 12:54:41 +0100

    emit & scoped scss

--- webapp/components/ResponsiveImage/ResponsiveImage.vue
+++ webapp/components/ResponsiveImage/ResponsiveImage.vue
@@ -14,6 +14,7 @@
 <script>
 export default {
   name: 'ResponsiveImage',
+  emits: ['loaded'],
   props: {
     image: {
       type: Object,
@@ -47,7 +48,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .responsive-image {
   opacity: 0;
   transition: opacity 0.3s ease;
