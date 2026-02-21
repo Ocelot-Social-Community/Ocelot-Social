92633d808ea8992472615932d57d8a95a88795ee
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-21 10:09:29 +0100

    fix masonrygrid tests

--- webapp/components/MasonryGrid/MasonryGridItem.spec.js
+++ webapp/components/MasonryGrid/MasonryGridItem.spec.js
@@ -7,37 +7,37 @@ describe('MasonryGridItem', () => {
   let wrapper
 
   describe('given an imageAspectRatio', () => {
-    it('sets the initial rowSpan to 13 when the ratio is higher than 1.3', () => {
+    it('sets the initial rowSpan to 114 when the ratio is higher than 1.3', () => {
       const propsData = { imageAspectRatio: 2 }
       wrapper = mount(MasonryGridItem, { localVue, propsData })
 
-      expect(wrapper.vm.rowSpan).toBe(13)
+      expect(wrapper.vm.rowSpan).toBe(114)
     })
 
-    it('sets the initial rowSpan to 15 when the ratio is between 1.3 and 1', () => {
+    it('sets the initial rowSpan to 132 when the ratio is between 1.3 and 1', () => {
       const propsData = { imageAspectRatio: 1.1 }
       wrapper = mount(MasonryGridItem, { localVue, propsData })
 
-      expect(wrapper.vm.rowSpan).toBe(15)
+      expect(wrapper.vm.rowSpan).toBe(132)
     })
 
-    it('sets the initial rowSpan to 18 when the ratio is between 1 and 0.7', () => {
+    it('sets the initial rowSpan to 159 when the ratio is between 1 and 0.7', () => {
       const propsData = { imageAspectRatio: 0.7 }
       wrapper = mount(MasonryGridItem, { localVue, propsData })
 
-      expect(wrapper.vm.rowSpan).toBe(18)
+      expect(wrapper.vm.rowSpan).toBe(159)
     })
 
-    it('sets the initial rowSpan to 25 when the ratio is lower than 0.7', () => {
+    it('sets the initial rowSpan to 222 when the ratio is lower than 0.7', () => {
       const propsData = { imageAspectRatio: 0.3 }
       wrapper = mount(MasonryGridItem, { localVue, propsData })
-      expect(wrapper.vm.rowSpan).toBe(25)
+      expect(wrapper.vm.rowSpan).toBe(222)
     })
 
     describe('given no aspect ratio', () => {
-      it('sets the initial rowSpan to 8 when not given an imageAspectRatio', () => {
+      it('sets the initial rowSpan to 69 when not given an imageAspectRatio', () => {
         wrapper = mount(MasonryGridItem, { localVue })
-        expect(wrapper.vm.rowSpan).toBe(8)
+        expect(wrapper.vm.rowSpan).toBe(69)
       })
     })
   })
