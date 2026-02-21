159b6f36f20e09230ce073a87339280090e64077
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-21 05:48:00 +0100

    equal gaps

--- webapp/components/MasonryGrid/MasonryGrid.vue
+++ webapp/components/MasonryGrid/MasonryGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="ds-grid"
-    :style="{ gridAutoRows: '20px', rowGap: '16px', columnGap: '16px' }"
+    :style="{ gridAutoRows: '2px', rowGap: '2px', columnGap: '16px' }"
     :class="[itemsCalculating ? 'reset-grid-height' : '']"
   >
     <slot></slot>
