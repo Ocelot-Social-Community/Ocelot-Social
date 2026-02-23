f436242d40aaf09cdbd31cb712fe32d874ba1d9c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-23 05:48:58 +0100

    switch to tsx

--- backend/src/db/compiler.ts
+++ backend/src/db/compiler.ts
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable import-x/no-commonjs */
 // eslint-disable-next-line n/no-unpublished-require
-const tsNode = require('ts-node')
-// eslint-disable-next-line import-x/no-unassigned-import, n/no-unpublished-require
-require('tsconfig-paths/register')
+const tsx = require('tsx/cjs/api')
 
-module.exports = tsNode.register
+module.exports = tsx.register
