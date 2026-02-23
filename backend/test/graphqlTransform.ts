d9e6b10af7f663b1ba010499df5b842d78d98524
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-23 05:51:01 +0100

    debug info

--- backend/test/graphqlTransform.ts
+++ backend/test/graphqlTransform.ts
@@ -1,9 +1,13 @@
 import { parse } from 'graphql'
 
 export default {
-  process(sourceText: string) {
-    return {
-      code: `module.exports = ${JSON.stringify(parse(sourceText))};`,
+  process(sourceText: string, sourcePath: string) {
+    try {
+      return {
+        code: `module.exports = ${JSON.stringify(parse(sourceText))};`,
+      }
+    } catch (error) {
+      throw new Error(`Failed to parse ${sourcePath}: ${error instanceof Error ? error.message : error}`)
     }
   },
 }
