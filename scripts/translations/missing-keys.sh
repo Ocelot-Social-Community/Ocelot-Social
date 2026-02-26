1de379276f429c6f7e9fe22236830f36c611c124
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-26 22:38:36 +0100

    ensure all langauges are complete

--- scripts/translations/missing-keys.sh
+++ scripts/translations/missing-keys.sh
@@ -1,17 +1,34 @@
 #! /usr/bin/env bash
 
 ROOT_DIR=$(dirname "$0")/../..
+LOCALES_DIR="$ROOT_DIR/webapp/locales"
 
 sorting="jq -f $ROOT_DIR/scripts/translations/sort_filter.jq"
-english="$sorting $ROOT_DIR/webapp/locales/en.json"
-german="$sorting $ROOT_DIR/webapp/locales/de.json"
+english="$sorting $LOCALES_DIR/en.json"
 listPaths="jq -c 'path(..)|[.[]|tostring]|join(\".\")'"
-diffString="<( $english | $listPaths ) <( $german | $listPaths )"
-if eval "diff -q $diffString";
-then
-  : # all good
-else
-  eval "diff -y $diffString | grep '[|<>]'";
-  printf "\nEnglish and German translation keys do not match, see diff above.\n"
+
+has_error=0
+
+for file in "$LOCALES_DIR"/*.json; do
+  lang=$(basename "$file" .json)
+
+  # skip english itself
+  if [ "$lang" = "en" ]; then
+    continue
+  fi
+
+  other="$sorting $file"
+  diffString="<( $english | $listPaths ) <( $other | $listPaths )"
+
+  if eval "diff -q $diffString" > /dev/null 2>&1; then
+    : # all good
+  else
+    eval "diff -y $diffString | grep '[|<>]'"
+    printf "\nEnglish and %s (%s) translation keys do not match, see diff above.\n\n" "$lang" "$file"
+    has_error=1
+  fi
+done
+
+if [ "$has_error" -eq 1 ]; then
   exit 1
 fi
