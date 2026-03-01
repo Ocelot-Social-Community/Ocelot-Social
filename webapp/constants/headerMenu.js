704440b00c1cd0fb0d38091d2e452409f1f6b9be
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-01 13:09:43 +0100

    update branding branch

--- webapp/constants/headerMenu.js
+++ webapp/constants/headerMenu.js
@@ -1,1 +1,13 @@
-export default {}
+export default {
+  MENU: [
+    // {
+    //   nameIdent: 'nameIdent',
+    //   path: '/',
+    // },
+    // {
+    //   nameIdent: 'nameIdent',
+    //   url: 'https://ocelot.social',
+    //   target: '_blank',
+    // },
+  ],
+}
