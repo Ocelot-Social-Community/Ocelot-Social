821bd34a360f72a2f799f45e0bd335a66a1991fc
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-01 09:32:08 +0100

    lint fix

--- backend/src/graphql/resolvers/locations.spec.ts
+++ backend/src/graphql/resolvers/locations.spec.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { parse } from 'graphql'
+
 import Factory, { cleanDatabase } from '@db/factories'
 import User from '@graphql/queries/users/User.gql'
-import { parse } from 'graphql'
 import { createApolloTestSetup } from '@root/test/helpers'
 
 import type { ApolloTestSetup } from '@root/test/helpers'
