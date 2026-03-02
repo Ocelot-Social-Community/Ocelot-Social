dbf11056e87652605206e2f3202e2a1dc8f2919b
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-02 12:52:22 +0100

    lint fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import-x/no-named-as-default-member */
