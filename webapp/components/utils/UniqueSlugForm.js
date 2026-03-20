142e5f5aa059259cf42ba02bba53142c914bdfe0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-20 17:16:25 +0100

    fix debounce

--- webapp/components/utils/UniqueSlugForm.js
+++ webapp/components/utils/UniqueSlugForm.js
@@ -2,6 +2,22 @@ import { debounce } from 'lodash'
 import { checkSlugAvailableQuery } from '~/graphql/User.js'
 
 export default function UniqueSlugForm({ translate, apollo, currentUser }) {
+  const debouncedSlugCheck = debounce((value, callback) => {
+    const variables = { slug: value }
+    apollo.query({ query: checkSlugAvailableQuery, variables }).then((response) => {
+      const {
+        data: { User },
+      } = response
+      const existingSlug = User && User[0] && User[0].slug
+      const available = !existingSlug || existingSlug === currentUser.slug
+      if (!available) {
+        callback(new Error(translate('settings.validation.slug.alreadyTaken')))
+      } else {
+        callback()
+      }
+    })
+  }, 500)
+
   return {
     formSchema: {
       slug: [
@@ -13,21 +29,7 @@ export default function UniqueSlugForm({ translate, apollo, currentUser }) {
         },
         {
           asyncValidator(rule, value, callback) {
-            debounce(() => {
-              const variables = { slug: value }
-              apollo.query({ query: checkSlugAvailableQuery, variables }).then((response) => {
-                const {
-                  data: { User },
-                } = response
-                const existingSlug = User && User[0] && User[0].slug
-                const available = !existingSlug || existingSlug === currentUser.slug
-                if (!available) {
-                  callback(new Error(translate('settings.validation.slug.alreadyTaken')))
-                } else {
-                  callback()
-                }
-              })
-            }, 500)()
+            debouncedSlugCheck(value, callback)
           },
         },
       ],
