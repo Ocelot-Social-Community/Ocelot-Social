df8c57c83ac80e23b04bd13b7badeaa1d7839f1a
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-23 19:55:24 +0100

    fix missing styles in storybook

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import-x/no-unassigned-import
 import '@fontsource-variable/inter'
 // eslint-disable-next-line import-x/no-unassigned-import
+import '../src/styles/index.css'
+// eslint-disable-next-line import-x/no-unassigned-import
 import './storybook.css'
 
 export const parameters = {
