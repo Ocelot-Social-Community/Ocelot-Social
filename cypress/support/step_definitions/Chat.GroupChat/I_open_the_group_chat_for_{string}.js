4502ebf9ea09fe702068ecbb77fbfade398e7e93
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-26 13:09:05 +0100

    better e2e target selection

--- cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
+++ cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
@@ -1,8 +1,7 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I open the group chat for {string}', (groupId) => {
-  // Click the add-room button to open the search panel
   cy.get('[id="add-room"]', { timeout: 10000 }).click({ force: true })
-  // Wait for the group list to appear and click the group
-  cy.get('.group-list-item', { timeout: 10000 }).first().click()
+  cy.get('#chat-search-combined input', { timeout: 10000 }).type(groupId)
+  cy.get('.chat-search-result-detail', { timeout: 10000 }).contains(`&${groupId}`).click()
 })
