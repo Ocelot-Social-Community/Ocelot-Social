0af89cff7362d258746e5a10ea189c3960e656b0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-26 11:11:22 +0100

    simplify e2e step

--- cypress/support/step_definitions/Chat.GroupChat/I_send_the_message_{string}_in_the_chat.js
+++ cypress/support/step_definitions/Chat.GroupChat/I_send_the_message_{string}_in_the_chat.js
@@ -1,6 +1,5 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I send the message {string} in the chat', (message) => {
-  cy.get('.vac-textarea', { timeout: 15000 }).should('be.visible').type(message)
-  cy.get('.vac-textarea').type('{enter}')
+  cy.get('.vac-textarea', { timeout: 15000 }).should('be.visible').type(`${message}{enter}`)
 })
