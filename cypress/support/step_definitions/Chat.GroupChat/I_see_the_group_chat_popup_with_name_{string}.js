be01917fdedf7451a9eccc1fcdabf8ab4affd1b0
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-26 11:12:02 +0100

    more e2e simplification

--- cypress/support/step_definitions/Chat.GroupChat/I_see_the_group_chat_popup_with_name_{string}.js
+++ cypress/support/step_definitions/Chat.GroupChat/I_see_the_group_chat_popup_with_name_{string}.js
@@ -1,6 +1,5 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I see the group chat popup with name {string}', (groupName) => {
-  cy.get('.chat-modul', { timeout: 15000 }).should('be.visible')
-  cy.get('.chat-modul').should('contain', groupName)
+  cy.get('.chat-modul', { timeout: 15000 }).should('be.visible').and('contain', groupName)
 })
