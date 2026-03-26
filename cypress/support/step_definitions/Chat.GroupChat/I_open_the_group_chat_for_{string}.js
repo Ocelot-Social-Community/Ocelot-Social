8ab1c44dae0a98a8efa4bb267f8f5bb968f76875
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-26 18:17:17 +0100

    use groupSlug in e2e selector

--- cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
+++ cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
@@ -1,7 +1,7 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
-defineStep('I open the group chat for {string}', (groupId) => {
+defineStep('I open the group chat for {string}', (groupSlug) => {
   cy.get('[id="add-room"]', { timeout: 10000 }).click({ force: true })
-  cy.get('#chat-search-combined input', { timeout: 10000 }).type(groupId)
-  cy.get('.chat-search-result-detail', { timeout: 10000 }).contains(`&${groupId}`).click()
+  cy.get('#chat-search-combined input', { timeout: 10000 }).type(groupSlug)
+  cy.get('.chat-search-result-detail', { timeout: 10000 }).contains(`&${groupSlug}`).click()
 })
