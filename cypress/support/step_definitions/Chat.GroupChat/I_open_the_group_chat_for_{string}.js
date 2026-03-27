624986a05d7800240a35162b1136fb92f1bde31d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-27 15:18:54 +0100

    fix remaining selectors

--- cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
+++ cypress/support/step_definitions/Chat.GroupChat/I_open_the_group_chat_for_{string}.js
@@ -3,8 +3,8 @@ import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 defineStep('I open the group chat for {string}', (groupSlug) => {
   cy.get('vue-advanced-chat', { timeout: 10000 })
     .shadow()
-    .find('[id="add-room"]')
+    .find('.vac-add-icon')
     .click({ force: true })
-  cy.get('#chat-search-combined input', { timeout: 10000 }).type(groupSlug)
+  cy.get('input#chat-search-combined', { timeout: 10000 }).type(groupSlug)
   cy.get('.chat-search-result-detail', { timeout: 10000 }).contains(`&${groupSlug}`).click()
 })
