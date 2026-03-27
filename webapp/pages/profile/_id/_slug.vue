765078613a19091a3c70137d88623a66067895ae
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-27 18:37:45 +0100

    remove unnecessary await

--- webapp/pages/profile/_id/_slug.vue
+++ webapp/pages/profile/_id/_slug.vue
@@ -577,11 +577,11 @@ export default {
       if (type === 'following') this.followingCount = count
       if (type === 'followedBy') this.followedByCount = count
     },
-    async showOrChangeChat(userId) {
+    showOrChangeChat(userId) {
       if (this.getShowChat.showChat) {
-        await this.showChat({ showChat: false, chatUserId: null })
+        this.showChat({ showChat: false, chatUserId: null })
       }
-      await this.showChat({ showChat: true, chatUserId: userId })
+      this.showChat({ showChat: true, chatUserId: userId })
     },
   },
   apollo: {
