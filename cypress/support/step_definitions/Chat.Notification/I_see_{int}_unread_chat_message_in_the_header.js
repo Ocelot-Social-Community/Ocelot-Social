1fe915421118c328d22c057247c915aca809769f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-03-30 03:15:23 +0200

    fix e2e selectors

--- cypress/support/step_definitions/Chat.Notification/I_see_{int}_unread_chat_message_in_the_header.js
+++ cypress/support/step_definitions/Chat.Notification/I_see_{int}_unread_chat_message_in_the_header.js
@@ -2,11 +2,11 @@ import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I see no unread chat messages in the header', () => {
   cy.get('.chat-notification-menu:visible', { timeout: 15000 }).should('exist')
-  cy.get('.chat-notification-menu:visible .count.--danger').should('not.exist')
+  cy.get('.chat-notification-menu:visible .os-counter-icon__count.os-counter-icon__count--danger').should('not.exist')
 })
 
 defineStep('I see {int} unread chat message in the header', (count) => {
-  cy.get('.chat-notification-menu:visible .count.--danger', { timeout: 15000 }).should(
+  cy.get('.chat-notification-menu:visible .os-counter-icon__count.os-counter-icon__count--danger', { timeout: 15000 }).should(
     'contain',
     count,
   )
--- cypress/support/step_definitions/Notification.Mention/the_unread_counter_is_removed.js
+++ cypress/support/step_definitions/Notification.Mention/the_unread_counter_is_removed.js
@@ -1,6 +1,6 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('the unread counter is removed', () => {
-  cy.get('.notifications-menu .counter-icon .count')
+  cy.get('.notifications-menu .os-counter-icon .os-counter-icon__count')
     .should('not.exist')
 })
