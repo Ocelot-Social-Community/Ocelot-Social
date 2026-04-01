1915d35707f8a2e05f15970e38578c544cefe667
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-04-01 23:05:37 +0200

    settings style

--- webapp/layouts/default.vue
+++ webapp/layouts/default.vue
@@ -84,6 +84,11 @@ export default {
 }
 
 @media (max-width: 810px) {
+  .main-container {
+    padding-top: 4rem;
+    padding-bottom: 0;
+  }
+
   .desktop-footer {
     display: none;
   }
--- webapp/pages/settings.vue
+++ webapp/pages/settings.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <div class="ds-my-small">
+  <div class="settings-page">
+    <div class="settings-header">
       <h1 class="ds-heading ds-heading-h1">{{ $t('settings.name') }}</h1>
     </div>
-    <div class="ds-my-large"></div>
-    <div class="ds-flex ds-flex-gap-small">
-      <div class="menu-container">
+    <div class="settings-layout">
+      <div class="settings-menu-desktop">
         <os-menu :routes="routes" :is-exact="() => true" link-tag="router-link" />
+      </div>
+      <div class="settings-menu-mobile">
+        <select
+          class="settings-select"
+          :value="$route.path"
+          @change="$router.push($event.target.value)"
+        >
+          <option v-for="route in routes" :key="route.path" :value="route.path">
+            {{ route.name }}
+          </option>
+        </select>
       </div>
       <div class="settings-content" id="settings-content">
         <transition name="slide-up" appear>
@@ -47,6 +57,9 @@ export default {
           name: this.$t('settings.social-media.name'),
           path: `/settings/my-social-media`,
         },
+        ...(this.$env.INVITE_REGISTRATION === true
+          ? [{ name: this.$t('settings.invites.name'), path: `/settings/invites` }]
+          : []),
         {
           name: this.$t('settings.muted-users.name'),
           path: `/settings/muted-users`,
@@ -91,36 +104,84 @@ export default {
         })
       }
 
-      if (this.$env.INVITE_REGISTRATION === true) {
-        routes.push({
-          name: this.$t('settings.invites.name'),
-          path: `/settings/invites`,
-        })
-      }
-
       return routes
     },
   },
 }
 </script>
 
-<style scoped>
-.menu-container {
-  width: 100%;
+<style scoped lang="scss">
+.settings-page {
+  padding-bottom: $space-x-small;
 }
+
+.settings-header {
+  margin-top: $space-base;
+  margin-bottom: $space-x-small;
+}
+
+.settings-layout {
+  display: flex;
+  flex-direction: column;
+  gap: $space-xx-small;
+}
+
+.settings-menu-desktop {
+  display: none;
+  width: 200px;
+  flex-shrink: 0;
+}
+
+.settings-menu-mobile {
+  display: block;
+  margin-bottom: $space-small;
+}
+
+.settings-select {
+  width: 100%;
+  padding: $space-x-small $space-large $space-x-small $space-small;
+  font-size: $font-size-base;
+  border: 1px solid $color-neutral-80;
+  border-radius: $border-radius-base;
+  background-color: white;
+  color: $text-color-base;
+  appearance: none;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='8' viewBox='0 0 12 8'%3E%3Cpath fill='%234b4554' d='M1.41 0L6 4.58 10.59 0 12 1.41l-6 6-6-6z'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: right $space-small center;
+  cursor: pointer;
+
+  &:focus {
+    outline: 1px dashed $color-primary;
+    outline-offset: -1px;
+  }
+}
+
 .settings-content {
   flex: 1;
-  padding: 0 24px;
-  margin-top: 32px;
+  min-width: 0;
 }
 
 @media screen and (min-width: 600px) {
-  .settings-content {
-    margin-top: 0;
+  .settings-header {
+    margin-bottom: $space-small;
   }
 
-  .menu-container {
-    width: 200px;
+  .settings-layout {
+    flex-direction: row;
+    gap: $space-small;
+  }
+
+  .settings-menu-desktop {
+    display: block;
+  }
+
+  .settings-menu-mobile {
+    display: none;
+  }
+
+  .settings-content {
+    padding: 0 $space-base;
   }
 }
 </style>
