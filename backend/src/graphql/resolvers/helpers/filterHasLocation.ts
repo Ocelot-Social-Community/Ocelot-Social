b569c97196dc878e43600e88b71f1183ff08a8d6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-04-01 14:22:31 +0200

    fix some lint

--- backend/src/graphql/resolvers/helpers/filterHasLocation.ts
+++ backend/src/graphql/resolvers/helpers/filterHasLocation.ts
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { mergeWith, isArray } from 'lodash'
 
-const getIdsWithLocation = async (context, label: string) => {
+import type { Context } from '@src/context'
+
+const getIdsWithLocation = async (context: Context, label: string): Promise<string[]> => {
   const session = context.driver.session()
   try {
     const result = await session.readTransaction(async (transaction) => {
@@ -11,7 +10,7 @@ const getIdsWithLocation = async (context, label: string) => {
         MATCH (n:${label})-[:IS_IN]->(l:Location)
         RETURN collect(n.id) AS ids`
       const response = await transaction.run(cypher)
-      return response.records.map((record) => record.get('ids'))
+      return response.records.map((record) => record.get('ids') as string[])
     })
     const [ids] = result
     return ids
@@ -20,34 +19,26 @@ const getIdsWithLocation = async (context, label: string) => {
   }
 }
 
-export const filterUsersHasLocation = async (params, context) => {
+export const filterUsersHasLocation = async (params, context: Context) => {
   if (!params.filter?.hasLocation) return params
   delete params.filter.hasLocation
   const userIds = await getIdsWithLocation(context, 'User')
-  params.filter = mergeWith(
-    params.filter,
-    { id_in: userIds },
-    (objValue, srcValue) => {
-      if (isArray(objValue)) {
-        return objValue.concat(srcValue)
-      }
-    },
-  )
+  params.filter = mergeWith(params.filter, { id_in: userIds }, (objValue, srcValue) => {
+    if (isArray(objValue)) {
+      return objValue.concat(srcValue)
+    }
+  })
   return params
 }
 
-export const filterPostsHasLocation = async (params, context) => {
+export const filterPostsHasLocation = async (params, context: Context) => {
   if (!params.filter?.hasLocation) return params
   delete params.filter.hasLocation
   const postIds = await getIdsWithLocation(context, 'Post')
-  params.filter = mergeWith(
-    params.filter,
-    { id_in: postIds },
-    (objValue, srcValue) => {
-      if (isArray(objValue)) {
-        return objValue.concat(srcValue)
-      }
-    },
-  )
+  params.filter = mergeWith(params.filter, { id_in: postIds }, (objValue, srcValue) => {
+    if (isArray(objValue)) {
+      return objValue.concat(srcValue)
+    }
+  })
   return params
 }
