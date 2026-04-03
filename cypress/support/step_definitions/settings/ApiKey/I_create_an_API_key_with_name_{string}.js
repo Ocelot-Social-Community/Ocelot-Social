81a72421cc20130251540aabefbc729ab41413c2
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-04-03 13:16:19 +0200

    fix e2e

--- cypress/support/step_definitions/settings/ApiKey/I_create_an_API_key_with_name_{string}.js
+++ cypress/support/step_definitions/settings/ApiKey/I_create_an_API_key_with_name_{string}.js
@@ -2,5 +2,5 @@ import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep('I create an API key with name {string}', (name) => {
   cy.get('#api-key-name').clear().type(name)
-  cy.get('[data-test="create-api-key-submit"]').click()
+  cy.get('#api-key-name').closest('form').submit()
 })
