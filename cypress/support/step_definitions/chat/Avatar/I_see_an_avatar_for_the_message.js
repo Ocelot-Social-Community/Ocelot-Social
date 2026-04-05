0a745e22e7c5bd05f660c8e0e8f6d52f7a711022
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-04-05 13:12:33 +0200

    stabilize e2e

--- cypress/support/step_definitions/chat/Avatar/I_see_an_avatar_for_the_message.js
+++ cypress/support/step_definitions/chat/Avatar/I_see_an_avatar_for_the_message.js
@@ -2,32 +2,20 @@ import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
 
 defineStep("I see an avatar for the other user's message", () => {
   // Other user's messages are on the left side (no vac-offset-current class)
-  // The avatar may be in light DOM (our ProfileAvatar slot) or shadow DOM (library default)
-  cy.get('vue-advanced-chat', { timeout: 15000 }).then(($el) => {
-    const shadow = $el[0].shadowRoot
-    const messageWrappers = shadow.querySelectorAll('.vac-message-wrapper')
-    const otherUserMsg = Array.from(messageWrappers).find(
-      (el) => !el.querySelector('.vac-offset-current'),
-    )
-    expect(otherUserMsg).to.exist
-    // Check for avatar in shadow DOM or light DOM slot content
-    const hasAvatar =
-      otherUserMsg.querySelector('.vac-avatar') ||
-      $el.find('.profile-avatar.vac-message-avatar').length > 0
-    expect(hasAvatar).to.be.ok
-  })
+  cy.get('vue-advanced-chat', { timeout: 15000 })
+    .shadow()
+    .find('.vac-message-wrapper:not(:has(.vac-offset-current))', { timeout: 10000 })
+    .first()
+    .find('.vac-avatar, .profile-avatar.vac-message-avatar')
+    .should('exist')
 })
 
 defineStep('I see an avatar for my own message', () => {
   // Own messages are on the right side (vac-offset-current class)
-  cy.get('vue-advanced-chat', { timeout: 15000 }).then(($el) => {
-    const shadow = $el[0].shadowRoot
-    const ownMsg = shadow.querySelector('.vac-offset-current')
-    expect(ownMsg).to.exist
-    // Check for avatar in shadow DOM or light DOM slot content
-    const hasAvatar =
-      shadow.querySelector('.vac-avatar-current') ||
-      $el.find('.profile-avatar.vac-message-avatar').length > 0
-    expect(hasAvatar).to.be.ok
-  })
+  cy.get('vue-advanced-chat', { timeout: 15000 })
+    .shadow()
+    .find('.vac-message-wrapper:has(.vac-offset-current)', { timeout: 10000 })
+    .first()
+    .find('.vac-avatar-current, .profile-avatar.vac-message-avatar')
+    .should('exist')
 })
