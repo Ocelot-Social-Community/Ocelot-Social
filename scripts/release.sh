e9f4701cf0348a8f18c34aec12ae79054cf46ae5
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-04-28 16:07:53 +0200

    fix(other): fix chart version to be in lockstep with app-version (#9604)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
+set -eu
 
 # base setup
-SCRIPT_PATH=$(realpath $0)
-SCRIPT_DIR=$(dirname $SCRIPT_PATH)
-ROOT_DIR=$SCRIPT_DIR/..
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+ROOT_DIR="$SCRIPT_DIR/.."
 
 # Update Version
-cd $ROOT_DIR
+cd "$ROOT_DIR"
 yarn version --no-git-tag-version --no-commit-hooks --no-commit
 VERSION_NEW=$(node -p -e "require('$ROOT_DIR/package.json').version")
 
 ## packages
-cd backend
-yarn version --no-git-tag-version --no-commit-hooks --no-commit --new-version $VERSION_NEW
-cd $ROOT_DIR/webapp
-yarn version --no-git-tag-version --no-commit-hooks --no-commit --new-version $VERSION_NEW
-cd $ROOT_DIR/maintenance
-npm version --no-git-tag-version $VERSION_NEW
+cd "$ROOT_DIR/backend"
+yarn version --no-git-tag-version --no-commit-hooks --no-commit --new-version "$VERSION_NEW"
+cd "$ROOT_DIR/webapp"
+yarn version --no-git-tag-version --no-commit-hooks --no-commit --new-version "$VERSION_NEW"
+cd "$ROOT_DIR/maintenance"
+npm version --no-git-tag-version "$VERSION_NEW"
 
 ## helm
-sed -i -e 's/appVersion: ".*"/appVersion: "'"$VERSION_NEW"'"/g' $ROOT_DIR/deployment/helm/charts/ocelot-neo4j/Chart.yaml
-sed -i -e 's/appVersion: ".*"/appVersion: "'"$VERSION_NEW"'"/g' $ROOT_DIR/deployment/helm/charts/ocelot-social/Chart.yaml
+sed -i -e 's/appVersion: ".*"/appVersion: "'"$VERSION_NEW"'"/g' "$ROOT_DIR/deployment/helm/charts/ocelot-neo4j/Chart.yaml"
+sed -i -e 's/appVersion: ".*"/appVersion: "'"$VERSION_NEW"'"/g' "$ROOT_DIR/deployment/helm/charts/ocelot-social/Chart.yaml"
+sed -i -e 's/^version: .*/version: '"$VERSION_NEW"'/' "$ROOT_DIR/deployment/helm/charts/ocelot-neo4j/Chart.yaml"
+sed -i -e 's/^version: .*/version: '"$VERSION_NEW"'/' "$ROOT_DIR/deployment/helm/charts/ocelot-social/Chart.yaml"
 
 # generate changelog
-cd $ROOT_DIR
-yarn run auto-changelog --commit-limit 0 --latest-version $VERSION_NEW
+cd "$ROOT_DIR"
+yarn run auto-changelog --commit-limit 0 --latest-version "$VERSION_NEW"
