688364c505f497e4677382ad0f7d6a94377f1ae2
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-06-17 11:17:44 +0200

    simplify e2e steps

--- cypress/support/step_definitions/moderation/ReportContent/I_click_on_the_avatar_menu_in_the_top_right_corner.js
+++ cypress/support/step_definitions/moderation/ReportContent/I_click_on_the_avatar_menu_in_the_top_right_corner.js
@@ -1,7 +1,8 @@
 import { defineStep } from '@badeball/cypress-cucumber-preprocessor'
-import 'cypress-network-idle'
 
 defineStep('I click on the avatar menu in the top right corner', () => {
   cy.get('.avatar-menu').click()
-  cy.waitForNetworkIdle(2000)
+  // Opening the dropdown is a client-side action (no network request), so sync on the
+  // popover becoming visible rather than on network idle.
+  cy.get('.avatar-menu-popover').should('be.visible')
 })
