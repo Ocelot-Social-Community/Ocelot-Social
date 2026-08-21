7550adbf30087f8d03a7db0be1ae60b225988648
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-08-21 13:03:56 +0200

    fix cypress

--- cypress/support/step_definitions/common/I_am_logged_in_as_{string}.js
+++ cypress/support/step_definitions/common/I_am_logged_in_as_{string}.js
@@ -3,16 +3,11 @@ import CONFIG from '../../../../backend/build/src/config/index'
 import { encode } from '../../../../backend/build/src/jwt/encode'
 
 defineStep('I am logged in as {string}', slug => {
-  cy.neode()
+  cy.fixtures()
     .firstOf('User', { slug })
-    .then(user => {
-      return new Cypress.Promise((resolve, reject) => {
-        if(!user) {
-          return reject(`User ${email} not found in database`)
-        }
-        return user.toJson().then((user) => resolve(user))
-      })
-    })
+    // No missing-user branch: the fixture API throws with the slug it looked for when
+    // nothing matches, which is the message this used to build by hand.
+    .then(user => user.toJson())
     .then(user => {
       cy.setCookie('ocelot-social-token', encode({ config: CONFIG })(user))
     })
